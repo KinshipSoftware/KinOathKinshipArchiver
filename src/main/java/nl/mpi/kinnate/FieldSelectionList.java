a449ce05510993f86127dfc0de3845ae515f48be
Peter Withers <Peter.Withers@mpi.nl>
2011-02-17 10:47:30 +0000

    Added delete buttons to the field selection list items and handled the diagram updates required.

--- src/main/java/nl/mpi/kinnate/FieldSelectionList.java
+++ src/main/java/nl/mpi/kinnate/FieldSelectionList.java
@@ -1,9 +1,11 @@
 package nl.mpi.kinnate;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import nl.mpi.kinnate.EntityIndexer.IndexerParameters.IndexerParam;
 
 /**
  *  Document   : FieldSelectionList
@@ -12,28 +14,48 @@ import javax.swing.JPanel;
  */
 public class FieldSelectionList extends JPanel {
 
-    public FieldSelectionList() {
+    KinTypeEgoSelectionTestPanel egoSelectionTestPanel;
+    private IndexerParam indexerParam;
+
+    public FieldSelectionList(KinTypeEgoSelectionTestPanel egoSelectionTestPanelLocal, IndexerParam indexerParamLocal) {
+        egoSelectionTestPanel = egoSelectionTestPanelLocal;
+        indexerParam = indexerParamLocal;
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        populateSelectionList();
     }
 
-    public FieldSelectionList(String[] fieldListArray) {
+    private void populateSelectionList() {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.removeAll();
-        for (String fieldArray : fieldListArray) {
+        for (String fieldArray : indexerParam.getValues()) {
             JLabel fieldPathLabel = new JLabel(fieldArray);
             JPanel fieldPanel = new JPanel();
             fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.LINE_AXIS));
             fieldPanel.add(fieldPathLabel);
+            JButton removeButton = new JButton("x");
+            removeButton.setActionCommand(fieldArray);
+            removeButton.addActionListener(new java.awt.event.ActionListener() {
+
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    indexerParam.removeValue(evt.getActionCommand());
+                    populateSelectionList();
+                    revalidate();
+                    egoSelectionTestPanel.drawGraph();
+                }
+            });
+            fieldPanel.add(new JPanel());
+            fieldPanel.add(removeButton);
             this.add(fieldPanel);
         }
+        this.add(new JPanel());
     }
 
-    public FieldSelectionList(String[][] fieldListArray) {
+    private void populateSelectionList(String[][] fieldListArray, String[] availableValues) {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.removeAll();
         for (String[] fieldArray : fieldListArray) {
             JLabel fieldPathLabel = new JLabel(fieldArray[0]);
-            JComboBox valueSelect = new JComboBox(new String[]{"circle", "triangle", "square", "resource", "union"}); // todo: remove the strings and get from the svg
+            JComboBox valueSelect = new JComboBox(availableValues); // todo: get the string list from the svg: new String[]{"circle", "triangle", "square", "resource", "union"}
             valueSelect.setSelectedItem(fieldArray[1]);
             JPanel fieldPanel = new JPanel();
             fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.LINE_AXIS));
