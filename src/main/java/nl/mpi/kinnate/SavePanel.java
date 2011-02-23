878895b38ca0eb03f1ebe1964cddfd8ca359fb63
Peter Withers <Peter.Withers@mpi.nl>
2011-02-23 09:16:38 +0000

    Added interface for saveable panels.

---
+++ src/main/java/nl/mpi/kinnate/SavePanel.java
@@ -0,0 +1,19 @@
+package nl.mpi.kinnate;
+
+import java.io.File;
+
+/**
+ *  Document   : SavePanel
+ *  Created on : Feb 17, 2011, 1:37:48 PM
+ *  Author     : Peter Withers
+ */
+public interface SavePanel {
+
+    public boolean requiresSave();
+
+    public boolean hasSaveFileName();
+
+    public void saveToFile();
+
+    public void saveToFile(File saveFile);
+}
