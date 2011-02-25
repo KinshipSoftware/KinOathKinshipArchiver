c33b6d55f95b545e44961038989c9edc75359f0b
Peter Withers <Peter.Withers@mpi.nl>
2011-02-25 14:29:52 +0000

    Added configurable symbol selection based on metadata of the entities.

--- src/main/java/nl/mpi/kinnate/FieldSelectionList.java
+++ src/main/java/nl/mpi/kinnate/FieldSelectionList.java
@@ -5,6 +5,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import nl.mpi.kinnate.EntityIndexer.IndexerParameters.IndexerParam;
 
 /**
@@ -27,13 +28,24 @@ public class FieldSelectionList extends JPanel {
     private void populateSelectionList() {
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         this.removeAll();
-        for (String fieldArray : indexerParam.getValues()) {
-            JLabel fieldPathLabel = new JLabel(fieldArray);
+        for (String fieldArray[] : indexerParam.getValues()) {
+            JLabel fieldPathLabel = new JLabel(fieldArray[0]);
             JPanel fieldPanel = new JPanel();
             fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.LINE_AXIS));
             fieldPanel.add(fieldPathLabel);
+            if (fieldArray.length > 1) {
+                String[] availableValues = indexerParam.getAvailableValues();
+                if (availableValues != null) {
+                    JComboBox valueSelect = new JComboBox(availableValues);
+                    valueSelect.setSelectedItem(fieldArray[1]);
+                    fieldPanel.add(valueSelect);
+                } else {
+                    JTextField valueField = new JTextField(fieldArray[1]);
+                    fieldPanel.add(valueField);
+                }
+            }
             JButton removeButton = new JButton("x");
-            removeButton.setActionCommand(fieldArray);
+            removeButton.setActionCommand(fieldArray[0]);
             removeButton.addActionListener(new java.awt.event.ActionListener() {
 
                 public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -50,18 +62,18 @@ public class FieldSelectionList extends JPanel {
         this.add(new JPanel());
     }
 
-    private void populateSelectionList(String[][] fieldListArray, String[] availableValues) {
-        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        this.removeAll();
-        for (String[] fieldArray : fieldListArray) {
-            JLabel fieldPathLabel = new JLabel(fieldArray[0]);
-            JComboBox valueSelect = new JComboBox(availableValues); // todo: get the string list from the svg: new String[]{"circle", "triangle", "square", "resource", "union"}
-            valueSelect.setSelectedItem(fieldArray[1]);
-            JPanel fieldPanel = new JPanel();
-            fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.LINE_AXIS));
-            fieldPanel.add(fieldPathLabel);
-            fieldPanel.add(valueSelect);
-            this.add(fieldPanel);
-        }
-    }
+//    private void populateSelectionList(String[][] fieldListArray, String[] availableValues) {
+//        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+//        this.removeAll();
+//        for (String[] fieldArray : fieldListArray) {
+//            JLabel fieldPathLabel = new JLabel(fieldArray[0]);
+//            JComboBox valueSelect = new JComboBox(availableValues); // todo: get the string list from the svg: new String[]{"circle", "triangle", "square", "resource", "union"}
+//            valueSelect.setSelectedItem(fieldArray[1]);
+//            JPanel fieldPanel = new JPanel();
+//            fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.LINE_AXIS));
+//            fieldPanel.add(fieldPathLabel);
+//            fieldPanel.add(valueSelect);
+//            this.add(fieldPanel);
+//        }
+//    }
 }
