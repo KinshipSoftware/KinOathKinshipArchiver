6ab2bd080b506b55366906aef61cc990895496de
Peter Withers <Peter.Withers@mpi.nl>
2011-03-08 10:42:26 +0000

    Refactored

---
+++ src/main/java/nl/mpi/kinnate/svg/GraphDataNode.java
@@ -0,0 +1,210 @@
+package nl.mpi.kinnate.svg;
+
+import java.util.ArrayList;
+import nl.mpi.kinnate.KinTypeStringConverter.KinType;
+
+/**
+ *  Document   : GraphDataNode
+ *  Created on : Sep 11, 2010, 4:30:41 PM
+ *  Author     : Peter Withers
+ */
+public class GraphDataNode {
+
+    public enum SymbolType {
+        // symbol terms are used here to try to keep things agnostic
+
+        square, triangle, circle, union, resource, ego, none
+    }
+
+    public enum RelationLineType {
+
+        square, horizontalCurve, verticalCurve, none
+    }
+
+    public enum RelationType {
+        // the term sibling is too specific and needs to encompas anything on the same generation such as union
+
+        sibling, ancestor, descendant, union, none
+    }
+
+    public static RelationType getOpposingRelationType(RelationType relationType) {
+        switch (relationType) {
+            case ancestor:
+                return GraphDataNode.RelationType.descendant;
+            case descendant:
+                return GraphDataNode.RelationType.ancestor;
+            case sibling:
+                return GraphDataNode.RelationType.sibling;
+            case union:
+                return GraphDataNode.RelationType.union;
+        }
+        return GraphDataNode.RelationType.sibling;
+    }
+    private String entityPath;
+    private SymbolType symbolType;
+    private String symbolTypeString;
+    boolean isEgo = false;
+    private String[] labelString;
+    private ArrayList<NodeRelation> relatedNodes = new ArrayList<NodeRelation>();
+    int xPos;
+    int yPos;
+
+    public class NodeRelation {
+
+        // todo: private String[] relationPath;
+        public GraphDataNode sourceNode;
+        public GraphDataNode linkedNode;
+        public int generationalDistance;
+        RelationType relationType;
+        RelationLineType relationLineType;
+        String labelString;
+    }
+
+    public GraphDataNode(String entityPathLocal, String symbolTypeLocal, String[] labelStringLocal, boolean isEgoLocal) {
+        entityPath = entityPathLocal;
+        symbolType = null;
+        symbolTypeString = symbolTypeLocal;
+        labelString = labelStringLocal;
+        isEgo = isEgoLocal;
+    }
+
+    public GraphDataNode(String entityPathLocal, SymbolType symbolIndex, String[] labelStringLocal, boolean isEgoLocal) {
+        entityPath = entityPathLocal;
+        symbolType = symbolIndex;
+        labelString = labelStringLocal;
+        isEgo = isEgoLocal;
+    }
+//    public GraphDataNode(SymbolType symbolIndex, String[] labelStringLocal) {
+//        symbolType = symbolIndex;
+//        labelString = labelStringLocal;
+//    }
+
+//    public GraphDataNode(String[] labelStringLocal) {
+//        labelString = labelStringLocal;
+//    }
+    public String getSymbolType() {
+        if (symbolType != null) {
+            switch (symbolType) {
+                case circle:
+                    return "circle";
+                case ego:
+                    return "square";
+                case none:
+                    return null;
+                case resource:
+                    return "resource";
+                case square:
+                    return "square";
+                case triangle:
+                    return "triangle";
+                case union:
+                    return "union";
+            }
+        }
+        return symbolTypeString;
+    }
+
+    public String getEntityPath() {
+        return entityPath;
+    }
+
+    public String[] getLabel() {
+        return labelString;
+    }
+    ArrayList<String> unhandledLinkTypesArray = new ArrayList<String>();
+
+//    protected void calculateLinks(HashMap<String, GraphDataNode> graphDataNodeList) {
+//        if (this.imdiTreeObject != null) {
+//            this.imdiTreeObject.waitTillLoaded();
+//
+//
+//            for (ImdiTreeObject childNode : this.imdiTreeObject.getAllChildren()) {
+//                ImdiField[] currentField = childNode.getFields().get("Link");
+//
+//
+//                if (currentField != null && currentField.length > 0) {
+//                    GraphDataNode.RelationType relationType = GraphDataNode.RelationType.sibling;
+//                    ImdiField[] relationTypeField = childNode.getFields().get("Type"); //todo: this RELA field might not be the best nor the only one to gather relation types from
+//
+//
+//                    if (relationTypeField != null && relationTypeField.length > 0) {
+//                        String typeString = relationTypeField[0].getFieldValue();
+//                        System.out.println("link type field: " + relationTypeField[0].getFieldValue());
+//                        List<String> ancestorTerms = Arrays.asList(new String[]{"SUBN", "_HME", "WIFE", "CHIL", "HUSB", "REPO", "OBJE", "NOTE", "FAMC", "FAMS", "SOUR", "ASSO", "SUBM", "ANCI", "DESI", "ALIA"});
+//
+//
+//                        if (("Kinnate.Gedcom.Entity." + ancestorTerms).contains(typeString)) {
+//                            relationType = GraphDataNode.RelationType.ancestor;
+//
+//
+//                        } else {
+//                            unhandledLinkTypesArray.add(typeString);
+//
+//
+//                        }
+//
+////                        if ("Father".equals(typeString)) {
+////                            relationType = GraphDataNode.RelationType.ancestor;
+////                        } else if ("Mother".equals(typeString)) {
+////                            relationType = GraphDataNode.RelationType.ancestor;
+////                        }
+//                    }
+//                    System.out.println("link field: " + currentField[0].getFieldValue());
+////                    linkArray.add(currentField[0].getFieldValue());
+//                    GraphDataNode linkedNode = graphDataNodeList.get(currentField[0].getFieldValue());
+//
+//
+//                    if (linkedNode != null) {
+//                        this.addRelatedNode(linkedNode, 0, relationType);
+//
+//
+//                    }
+//                }
+//            }
+//        }
+//        if (unhandledLinkTypesArray.size() > 0) {
+//            System.err.println("unhandledLinkTypes: " + unhandledLinkTypesArray.toString());
+//
+//
+//        }
+//    }
+//    public GraphDataNode[] getLinks() {
+//        if (imdiTreeObject == null) {
+//            return linkStringsArray;
+//        } else {
+//            ArrayList<String> linkArray = new ArrayList<String>();
+//            imdiTreeObject.waitTillLoaded();
+//            for (ImdiTreeObject childNode : imdiTreeObject.getAllChildren()) {
+////            System.out.println("getAllChildren: " + childNode.getUrlString());
+//                ImdiField[] currentField = childNode.getFields().get("Link");
+//                if (currentField != null && currentField.length > 0) {
+//                    System.out.println("link field: " + currentField[0].getFieldValue());
+//                    linkArray.add(currentField[0].getFieldValue());
+//                }
+//            }
+//            return linkArray.toArray(new String[]{});
+//        }
+//    }
+    public void addRelatedNode(GraphDataNode relatedNode, int generationalDistance, RelationType relationType, RelationLineType relationLineType, String labelString) {
+        // note that the test gedcom file has multiple links for a given pair so in might be necessary to filter incoming links on a preferential basis
+        NodeRelation nodeRelation = new NodeRelation();
+        nodeRelation.sourceNode = this;
+        nodeRelation.linkedNode = relatedNode;
+        nodeRelation.generationalDistance = generationalDistance;
+        nodeRelation.relationType = relationType;
+        nodeRelation.relationLineType = relationLineType;
+        nodeRelation.labelString = labelString;
+        relatedNodes.add(nodeRelation);
+        relatedNode.relatedNodes.add(nodeRelation);
+    }
+
+    public boolean relationMatchesType(String alterPath, KinType kinType) {
+        // todo: compare the relation data
+//        return kinType.symbolType == symbolType;
+        return true;
+    }
+
+    public NodeRelation[] getNodeRelations() {
+        return relatedNodes.toArray(new NodeRelation[]{});
+    }
+}
---
+++ src/main/java/nl/mpi/kinnate/svg/GraphPanel0.java
@@ -0,0 +1,229 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.mpi.kinnate.svg;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import nl.mpi.arbil.LinorgSessionStorage;
+import nl.mpi.arbil.clarin.CmdiComponentBuilder;
+import org.apache.batik.dom.svg.SVGDOMImplementation;
+import org.apache.batik.swing.JSVGCanvas;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Element;
+import nl.mpi.arbil.data.ImdiLoader;
+import nl.mpi.arbil.data.ImdiTreeObject;
+import org.w3c.dom.Text;
+import org.w3c.dom.svg.SVGDocument;
+
+/**
+ *  Document   : GraphPanel0
+ *  Created on : Aug 16, 2010, 5:31:33 PM
+ *  Author     : Peter Withers
+ */
+public class GraphPanel0 extends JPanel {
+
+    protected JSVGCanvas svgCanvas = new JSVGCanvas();
+
+    public GraphPanel0() {
+        this.setLayout(new BorderLayout());
+        //doTest();
+        drawNodes();
+
+//        Interactor zoomInteractor = new AbstractZoomInteractor(){
+//            public boolean startInteraction(InputEvent ie) {
+//                int mods = ie.getModifiers();
+//                return ie.getID() == MouseEvent.MOUSE_PRESSED
+//                        && (mods & InputEvent.BUTTON3_MASK) != 0;
+//            }
+//        };
+//        Interactor panInteractor = new AbstractPanInteractor() {
+//
+//            public boolean startInteraction(InputEvent ie) {
+//                int mods = ie.getModifiers();
+//                return ie.getID() == MouseEvent.MOUSE_PRESSED
+//                        && (mods & InputEvent.BUTTON1_MASK) != 0;
+//            }
+//        };
+//        svgCanvas.getInteractors().add(panInteractor);
+//        svgCanvas.getInteractors().add(zoomInteractor);
+
+        //Ctrl+LeftButton - Zoom Box
+        //Shift+RightButton - Zoom (with instant feedback)
+        //Shift+LeftButton - Pan
+        //Ctrl+RightButton - Rotate
+        //Ctrl+Shift+RightButton - Reset transform (also known as "Original View")
+        
+        svgCanvas.setEnableImageZoomInteractor(true);
+        svgCanvas.setEnablePanInteractor(true);
+        svgCanvas.setEnableRotateInteractor(true);
+        svgCanvas.setEnableZoomInteractor(true);
+        svgCanvas.setEnableResetTransformInteractor(true);
+        
+    }
+
+    public void drawNodes() {
+        String[] treeNodesArray = LinorgSessionStorage.getSingleInstance().loadStringArray("KinGraphTree");
+        ArrayList<ImdiTreeObject> tempArray = new ArrayList<ImdiTreeObject>();
+        if (treeNodesArray != null) {
+            for (String currentNodeString : treeNodesArray) {
+                try {
+                    tempArray.add(ImdiLoader.getSingleInstance().getImdiObject(null, new URI(currentNodeString)));
+                } catch (URISyntaxException exception) {
+                    System.err.println(exception.getMessage());
+                    exception.printStackTrace();
+                }
+            }
+        }
+        DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
+        String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
+        // Document doc = impl.createDocument(svgNS, "svg", null);
+        SVGDocument doc = (SVGDocument)impl.createDocument(svgNS, "svg", null);
+// Get the root element (the 'svg' element).
+        Element svgRoot = doc.getDocumentElement();
+
+
+//        svgRoot.removeAttribute("version");
+
+        int stepNumber = 300;
+       int preferedWidth = (int)Math.sqrt(tempArray.size()) * stepNumber + stepNumber;
+
+
+        // Set the width and height attributes on the root 'svg' element.
+        svgRoot.setAttributeNS(null, "width", Integer.toString(preferedWidth));
+        svgRoot.setAttributeNS(null, "height", Integer.toString(preferedWidth));
+
+        this.setPreferredSize(new Dimension(preferedWidth, preferedWidth));
+
+        svgCanvas.setSVGDocument(doc);
+        int xPos = stepNumber;
+        int yPos = stepNumber;
+        for (ImdiTreeObject currentChild : tempArray) {
+            currentChild.waitTillLoaded();
+            if (!currentChild.isEmptyMetaNode()) {
+                // Create the rectangle.
+                Element rectangle = doc.createElementNS(svgNS, "rect");
+                rectangle.setAttributeNS(null, "x", Integer.toString(xPos));
+                rectangle.setAttributeNS(null, "y", Integer.toString(yPos));
+                rectangle.setAttributeNS(null, "width", "10");
+                rectangle.setAttributeNS(null, "height", "10");
+                rectangle.setAttributeNS(null, "fill", "red");
+// Attach the rectangle to the root 'svg' element.
+                svgRoot.appendChild(rectangle);
+                // <text id="_7" x="39.0" y="140.0" fill="black" stroke="black" stroke-width="0" font-size="15">Sample Text</text>
+                Element labelText = doc.createElementNS(svgNS, "text");
+                labelText.setAttributeNS(null, "x", Integer.toString(xPos));
+                labelText.setAttributeNS(null, "y", Integer.toString(yPos));
+                labelText.setAttributeNS(null, "fill", "black");
+                labelText.setAttributeNS(null, "stroke-width", "0");
+                labelText.setAttributeNS(null, "font-size", "14");
+                //labelText.setNodeValue(currentChild.toString());
+
+                //String textWithUni = "\u0041";
+                Text textNode = doc.createTextNode(currentChild.toString());
+                labelText.appendChild(textNode);
+                svgRoot.appendChild(labelText);
+
+                xPos += stepNumber;
+                if (xPos > preferedWidth) {
+                    xPos = stepNumber;
+                    yPos += stepNumber;
+                }
+            }
+        }
+
+
+        this.add(BorderLayout.CENTER, svgCanvas);
+        new CmdiComponentBuilder().savePrettyFormatting(doc, new File("/Users/petwit/Documents/SharedInVirtualBox/KinGraph/KinGraph2/src/main/resources/output.svg"));
+    }
+
+    public void doTest() {
+        ImdiTreeObject imdiNode = null;
+        try {
+            imdiNode = ImdiLoader.getSingleInstance().getImdiObject(null, new URI("file:/Users/petwit/.arbil/ArbilWorkingFiles/20100817152236/20100817152236.cmdi"));
+            //"file:/Users/petwit/.arbil/ArbilWorkingFiles/http/corpus1.mpi.nl/qfs1/media-archive/echo_data/sign_language/SSL/SSL_Poetry/Metadata/SSL_JM_poem_cayak_2.imdi"
+            imdiNode.waitTillLoaded();
+        } catch (URISyntaxException exception) {
+            System.err.println(exception.getMessage());
+        }
+
+        DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
+        String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
+        SVGDocument doc = (SVGDocument)impl.createDocument(svgNS, "svg", null);
+//        SVGDocument doc = svgCanvas.getSVGDocument();
+
+// Get the root element (the 'svg' element).
+        Element svgRoot = doc.getDocumentElement();
+
+// Set the width and height attributes on the root 'svg' element.
+        svgRoot.setAttributeNS(null, "width", "400");
+        svgRoot.setAttributeNS(null, "height", "450");
+
+        this.setPreferredSize(new Dimension(450, 400));
+
+        svgRoot.removeAttribute("version");
+
+        int xPos = 10;
+        int yPos = 10;
+        if (imdiNode != null) {
+            for (ImdiTreeObject currentChild : imdiNode.getAllChildren()) {
+                if (!currentChild.isEmptyMetaNode()) {
+                    // Create the rectangle.
+                    Element rectangle = doc.createElementNS(svgNS, "rect");
+                    rectangle.setAttributeNS(null, "x", Integer.toString(xPos));
+                    rectangle.setAttributeNS(null, "y", Integer.toString(yPos));
+                    rectangle.setAttributeNS(null, "width", "100");
+                    rectangle.setAttributeNS(null, "height", "50");
+                    rectangle.setAttributeNS(null, "fill", "red");
+// Attach the rectangle to the root 'svg' element.
+                    svgRoot.appendChild(rectangle);
+                    // <text id="_7" x="39.0" y="140.0" fill="black" stroke="black" stroke-width="0" font-size="15">Sample Text</text>
+                    Element labelText = doc.createElementNS(svgNS, "text");
+                    labelText.setAttributeNS(null, "x", Integer.toString(xPos));
+                    labelText.setAttributeNS(null, "y", Integer.toString(yPos));
+                    labelText.setAttributeNS(null, "fill", "black");
+                    labelText.setAttributeNS(null, "stroke-width", "0");
+                    labelText.setAttributeNS(null, "font-size", "14");
+                    //labelText.setNodeValue(currentChild.toString());
+
+                    //String textWithUni = "\u0041";
+                    Text textNode = doc.createTextNode(currentChild.getURI().getFragment());
+                    labelText.appendChild(textNode);
+                    svgRoot.appendChild(labelText);
+
+                    xPos += 150;
+                    if (xPos > 380) {
+                        xPos = 10;
+                        yPos += 70;
+                    }
+                }
+            }
+        }
+
+//// Create the rectangle.
+//        Element rectangle = doc.createElementNS(svgNS, "rect");
+//        rectangle.setAttributeNS(null, "x", "10");
+//        rectangle.setAttributeNS(null, "y", "20");
+//        rectangle.setAttributeNS(null, "width", "100");
+//        rectangle.setAttributeNS(null, "height", "50");
+//        rectangle.setAttributeNS(null, "fill", "red");
+//
+//// Attach the rectangle to the root 'svg' element.
+//        svgRoot.appendChild(rectangle);
+//        this.add(BorderLayout.NORTH, new JTextArea(doc.getTextContent()));
+//        try {
+        svgCanvas.setDocument(doc);
+        //svgCanvas.setURI(f.toURL().toString());
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+        this.add(BorderLayout.CENTER, svgCanvas);
+        new CmdiComponentBuilder().savePrettyFormatting(doc, new File("/Users/petwit/Documents/SharedInVirtualBox/mpi-co-svn-mpi-nl/LAT/Kinnate/trunk/src/main/resources/output0.svg"));
+    }
+}
