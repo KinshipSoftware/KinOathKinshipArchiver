fae879c23518a4fcb2ff531e4258c9d8340ec1a6
Peter Withers <Peter.Withers@mpi.nl>
2011-03-08 14:00:13 +0000

    Added the kin term panel where the user can define kin terms and their kin type counterparts.

--- src/main/java/nl/mpi/kinnate/ui/IndexerParametersPanel.java
+++ src/main/java/nl/mpi/kinnate/ui/IndexerParametersPanel.java
@@ -1,8 +1,8 @@
 package nl.mpi.kinnate.ui;
 
-import nl.mpi.kinnate.ui.KinTypeEgoSelectionTestPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import nl.mpi.kinnate.SavePanel;
 import nl.mpi.kinnate.entityindexer.EntityIndex;
 import nl.mpi.kinnate.svg.GraphPanel;
 
@@ -13,13 +13,13 @@ import nl.mpi.kinnate.svg.GraphPanel;
  */
 public class IndexerParametersPanel extends JTabbedPane {
 
-    public IndexerParametersPanel(KinTypeEgoSelectionTestPanel egoSelectionTestPanel, GraphPanel graphPanel, EntityIndex entityIndex) {
+    public IndexerParametersPanel(SavePanel savePanel, GraphPanel graphPanel, EntityIndex entityIndex) {
 
         // todo: add drag drop of field to these lists and initially populate them from the SVG data
-        this.add("Symbol Fields", new JScrollPane(new FieldSelectionList(egoSelectionTestPanel, graphPanel.getIndexParameters().symbolFieldsFields)));
+        this.add("Symbol Fields", new JScrollPane(new FieldSelectionList(savePanel, graphPanel.getIndexParameters().symbolFieldsFields)));
 //        this.add("Relation Fields", new JScrollPane(new FieldSelectionList(graphPanel.getIndexParameters().relevantLinkData)));
-        this.add("Ancestor Fields", new JScrollPane(new FieldSelectionList(egoSelectionTestPanel, graphPanel.getIndexParameters().ancestorFields)));
-        this.add("Decendant Fields", new JScrollPane(new FieldSelectionList(egoSelectionTestPanel, graphPanel.getIndexParameters().decendantFields)));
-        this.add("Label Fields", new JScrollPane(new FieldSelectionList(egoSelectionTestPanel, graphPanel.getIndexParameters().labelFields)));
+        this.add("Ancestor Fields", new JScrollPane(new FieldSelectionList(savePanel, graphPanel.getIndexParameters().ancestorFields)));
+        this.add("Decendant Fields", new JScrollPane(new FieldSelectionList(savePanel, graphPanel.getIndexParameters().decendantFields)));
+        this.add("Label Fields", new JScrollPane(new FieldSelectionList(savePanel, graphPanel.getIndexParameters().labelFields)));
     }
 }
