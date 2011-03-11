024794c8dfaaf4e2058abaea241da671c5c0d105
Peter Withers <Peter.Withers@mpi.nl>
2011-03-11 09:55:35 +0000

    Added a hide pane to make the ui more viewable for the user.

--- src/main/java/nl/mpi/kinnate/ui/HidePane.java
+++ src/main/java/nl/mpi/kinnate/ui/HidePane.java
@@ -3,6 +3,8 @@ package nl.mpi.kinnate.ui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -19,6 +21,7 @@ public class HidePane extends JPanel {
     int shownWidth = 300;
     String openLabel;
     String closeLabel;
+    boolean blockNextMouseUp = false;
 
     public HidePane(Component contentComponentLocal, String labelStringLocal, final String borderPosition) {
         if (borderPosition.equals(BorderLayout.LINE_END)) {
@@ -31,25 +34,58 @@ public class HidePane extends JPanel {
         contentComponent = contentComponentLocal;
         this.setLayout(new BorderLayout());
         removeButton = new JButton(openLabel);
+        removeButton.addMouseMotionListener(new MouseMotionAdapter() {
+
+            private int lastXpos;
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (!hiddenState) {
+                    if (borderPosition.equals(BorderLayout.LINE_END)) {
+                        shownWidth = shownWidth - lastXpos + e.getX();
+                    } else {
+                        shownWidth = shownWidth - lastXpos - e.getX();
+                    }
+                    if (shownWidth < removeButton.getPreferredSize().width * 2) {
+                        shownWidth = removeButton.getPreferredSize().width * 2;
+                    } else if (shownWidth > HidePane.this.getParent().getWidth()) {
+                        shownWidth = HidePane.this.getParent().getWidth() - removeButton.getPreferredSize().width;
+                    }
+                    HidePane.this.setPreferredSize(new Dimension(shownWidth, HidePane.this.getPreferredSize().height));
+                    HidePane.this.revalidate();
+                    HidePane.this.repaint();
+                    blockNextMouseUp = true;
+                }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                lastXpos = e.getX();
+            }
+        });
         removeButton.setToolTipText("hide/show");
         removeButton.setPreferredSize(new Dimension(24, 24));
         removeButton.addActionListener(new java.awt.event.ActionListener() {
 
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                if (!hiddenState) {
-                    HidePane.this.remove(contentComponent);
-                    removeButton.setText(openLabel);
-                    HidePane.this.add(removeButton, BorderLayout.CENTER);
-                    HidePane.this.setPreferredSize(new Dimension(removeButton.getPreferredSize().width, HidePane.this.getPreferredSize().height));
+                if (blockNextMouseUp) {
+                    blockNextMouseUp = false;
                 } else {
-                    HidePane.this.add(removeButton, borderPosition);
-                    HidePane.this.add(contentComponent, BorderLayout.CENTER);
-                    removeButton.setText(closeLabel);
-                    HidePane.this.setPreferredSize(new Dimension(shownWidth, HidePane.this.getPreferredSize().height));
+                    if (!hiddenState) {
+                        HidePane.this.remove(contentComponent);
+                        removeButton.setText(openLabel);
+                        HidePane.this.add(removeButton, BorderLayout.CENTER);
+                        HidePane.this.setPreferredSize(new Dimension(removeButton.getPreferredSize().width, HidePane.this.getPreferredSize().height));
+                    } else {
+                        HidePane.this.add(removeButton, borderPosition);
+                        HidePane.this.add(contentComponent, BorderLayout.CENTER);
+                        removeButton.setText(closeLabel);
+                        HidePane.this.setPreferredSize(new Dimension(shownWidth, HidePane.this.getPreferredSize().height));
+                    }
+                    hiddenState = !hiddenState;
+                    HidePane.this.revalidate();
+                    HidePane.this.repaint();
                 }
-                hiddenState = !hiddenState;
-                HidePane.this.revalidate();
-                HidePane.this.repaint();
             }
         });
         this.add(removeButton, BorderLayout.CENTER);
