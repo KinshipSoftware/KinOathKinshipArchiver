7936c08c3419d660e5e3cf63bff0369321ac118e
Peter Withers <Peter.Withers@mpi.nl>
2011-03-14 18:06:26 +0000

    Replaced the all entity tree with an entity search panel to make access to the potentially large number of entity records possible.

--- src/main/java/nl/mpi/kinnate/entityindexer/EntityCollection.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/EntityCollection.java
@@ -40,10 +40,11 @@ public class EntityCollection {
         ArrayList<String> resultPaths = new ArrayList<String>();
         try {
             //for $doc in collection('nl-mpi-kinnate')  where $doc//NAME="Bob /Cox/" return base-uri($doc)
-            String query = "for $doc in collection('nl-mpi-kinnate')  where $doc//NAME=\"" + namePartString + "\" return base-uri($doc)";
-            QueryProcessor proc = new QueryProcessor(query, context);
+            String query = "for $doc in collection('nl-mpi-kinnate') where contains($doc//NAME/text(), \"" + namePartString + "\") return base-uri($doc)";
+            QueryProcessor proc = new QueryProcessor(query, context);//Emp[contains(Ename,"AR")]
             Iter iter = proc.iter();
             Item item;
+            resultPaths.add(query);
             while ((item = iter.next()) != null) {
 //                System.out.println(item.toJava());
                 resultPaths.add(item.toJava().toString());
