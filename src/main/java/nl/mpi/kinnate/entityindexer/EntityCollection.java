70b4db3603c59d6cb160c661f6c844f886077333
Peter Withers <Peter.Withers@mpi.nl>
2011-03-18 15:34:35 +0000

    Replaced the all entity tree with an entity search panel to make access to the potentially large number of entity records possible.

--- src/main/java/nl/mpi/kinnate/entityindexer/EntityCollection.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/EntityCollection.java
@@ -1,7 +1,16 @@
 package nl.mpi.kinnate.entityindexer;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import nl.mpi.arbil.GuiHelper;
 import nl.mpi.arbil.LinorgBugCatcher;
 import nl.mpi.arbil.LinorgSessionStorage;
@@ -43,13 +52,55 @@ public class EntityCollection {
         }
     }
 
+    public String[] getRelatedNodes(String uniqueIdentifier) {
+        // todo: it would seem that entityPath is not going to be adequate because of resolved vs unresolved paths, it would seem best at this point to implement an ID or even persistent identifier if posible
+        // there are two parts required to get all relations of an ego: check the ego entity for relations to others and then check the relations of all other entities for references to the ego entity
+        // for now maybe use an md5 sum the full path url or something and put it into both the entity and linking entities
+        String displayLinks = "(\"Kinnate.Gedcom.Entity.HUSB\","
+                + "\"Kinnate.Gedcom.Entity.WIFE\","
+                + "\"Kinnate.Gedcom.Entity.CHIL\","
+                + "\"Kinnate.Gedcom.Entity.FAMS\","
+                + "\"Kinnate.Gedcom.Entity.FAMC\","
+                + "\"Kinnate.Gedcom.Entity.BIRT.FAMC\","
+                + "\"Kinnate.Gedcom.Entity.CHR.FAMC\","
+                + "\"Kinnate.Gedcom.Entity.ADOP.FAMC\","
+                + "\"Kinnate.Gedcom.Entity.SLGC.FAMC\")";
+
+        String query1String = "for $doc in collection('nl-mpi-kinnate')\n"
+                + "where /Kinnate/Relation/UniqueIdentifier/* = \"" + uniqueIdentifier + "\"\n"
+                + "and /Kinnate/Relation/Type = " + displayLinks + "\n"
+                + "return base-uri($doc)\n";
+        String query2String = "for $docOuter in collection('nl-mpi-kinnate')\n"
+                + "where $docOuter/Kinnate/(Gedcom, Entity)/UniqueIdentifier/* = \"" + uniqueIdentifier + "\"\n"
+                + "return\n"
+                + "     for $docInner in collection('nl-mpi-kinnate')\n"
+                + "     where $docInner/Kinnate/(Gedcom, Entity)/UniqueIdentifier/* = $docOuter/Kinnate/Relation/UniqueIdentifier/*\n"
+                + "     and $docInner/Kinnate/Relation/Type = " + displayLinks + "\n"
+                + "     return base-uri($docInner)\n";
+        System.out.println("query1String: " + query1String);
+        System.out.println("query2String: " + query2String);
+        ArrayList<String> resultsArray = new ArrayList<String>();
+        resultsArray.addAll(Arrays.asList(performQuery(query1String).resultsPathArray));
+        resultsArray.addAll(Arrays.asList(performQuery(query2String).resultsPathArray));
+        return resultsArray.toArray(new String[]{});
+    }
+
+    public SearchResults listAllRelationTypes() {
+        // todo: use this to populate the InderParametersFormUI
+        String queryString = "distinct-values(collection('nl-mpi-kinnate')/Kinnate/Relation/Type/text())";
+        return performQuery(queryString);
+    }
+
     public SearchResults searchByName(String namePartString) {
-        String queryString = "";
+        String queryString = "for $doc in collection('nl-mpi-kinnate') where contains(string-join($doc//text()), \"" + namePartString + "\") return base-uri($doc)";
+        return performQuery(queryString);
+    }
+
+    private SearchResults performQuery(String queryString) {
         SearchResults searchResults = new SearchResults();
         ArrayList<String> resultPaths = new ArrayList<String>();
         try {
             //for $doc in collection('nl-mpi-kinnate')  where $doc//NAME="Bob /Cox/" return base-uri($doc)
-            queryString = "for $doc in collection('nl-mpi-kinnate') where contains(string-join($doc//text()), \"" + namePartString + "\") return base-uri($doc)";
 //            String query = "for $doc in collection('nl-mpi-kinnate') where $doc//NAME = \"" + namePartString + "\" return base-uri($doc)";
             QueryProcessor proc = new QueryProcessor(queryString, context);//Emp[contains(Ename,"AR")]
             Iter iter = proc.iter();
@@ -72,5 +123,33 @@ public class EntityCollection {
         searchResults.resultsPathArray = resultPaths.toArray(new String[]{});
         searchResults.statusMessage = searchResults.statusMessage + "\n query: " + queryString;
         return searchResults;
+    }
+
+    static public void main(String[] args) {
+        final EntityCollection entityCollection = new EntityCollection();
+        JFrame jFrame = new JFrame("Test Query Window");
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JTextArea queryText = new JTextArea();
+        final JTextArea resultsText = new JTextArea();
+        resultsText.setVisible(false);
+        JButton jButton = new JButton("run query");
+        jButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                SearchResults results = entityCollection.performQuery(queryText.getText());
+                resultsText.setText(results.statusMessage + "\n");
+                for (String resultLine : results.resultsPathArray) {
+                    resultsText.append(resultLine + "\n");
+                }
+                resultsText.setVisible(true);
+            }
+        });
+        JPanel jPanel = new JPanel(new BorderLayout());
+        jPanel.add(queryText, BorderLayout.PAGE_START);
+        jPanel.add(resultsText, BorderLayout.CENTER);
+        jPanel.add(jButton, BorderLayout.PAGE_END);
+        jFrame.setContentPane(new JScrollPane(jPanel));
+        jFrame.pack();
+        jFrame.setVisible(true);
     }
 }
