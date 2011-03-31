3dafd227270d3d988d3c1681276d307b2d43ba66
Peter Withers <Peter.Withers@mpi.nl>
2011-03-31 16:38:56 +0000

    Added text search to the kin type string processing.

--- src/main/java/nl/mpi/kinnate/entityindexer/QueryParser.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/QueryParser.java
@@ -10,19 +10,27 @@ import java.util.ArrayList;
 public class QueryParser {
 
     public String[][] getQueryStrings(String kinTypeString) {
+        String kinType = null;
         ArrayList<String[]> queryTerms = new ArrayList<String[]>();
-        //String[] queryParts = kinTypeString.split("=\\[");
-        String[] queryParts = kinTypeString.split("\n");
-        for (String queryText : queryParts) {
-            if (!queryText.contains("=")) {
-                if (queryText.length() > 2) {
-                    queryTerms.add(new String[]{"*", queryText});
-                }
-            } else {
-                String[] queryTerm = queryText.split("=");
-                if (queryTerm.length == 2) {
-                    if (queryTerm[0].length() > 2 && queryTerm[1].length() > 2) {
-                        queryTerms.add(new String[]{queryTerm[0], queryTerm[1]});
+//        String[] queryParts = kinTypeString.split("(=\\[)|([\\]])");
+        String[] queryParts = kinTypeString.split("[\\]]");
+//        String[] queryParts = kinTypeString.split("\n");
+        for (String querySection : queryParts) {
+            String[] subParts = querySection.split("=\\[");
+            if (subParts.length == 2) {
+                String queryText = subParts[1];
+                kinType = subParts[0];
+                //queryText = queryText.split("\\]")[0];
+                if (!queryText.contains("=")) {
+                    if (queryText.length() > 2) {
+                        queryTerms.add(new String[]{"*", queryText});
+                    }
+                } else {
+                    String[] queryTerm = queryText.split("=");
+                    if (queryTerm.length == 2) {
+                        if (queryTerm[0].length() > 2 && queryTerm[1].length() > 2) {
+                            queryTerms.add(new String[]{queryTerm[0], queryTerm[1]});
+                        }
                     }
                 }
             }
