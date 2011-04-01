35b741fd5a26c9989723a66f08e9d9f745500e54
Peter Withers <Peter.Withers@mpi.nl>
2011-04-01 08:49:17 +0000

    Added kin term import and export and menu items for the kin terms.

---
+++ src/main/java/nl/mpi/kinnate/KinTermSavePanel.java
@@ -0,0 +1,17 @@
+package nl.mpi.kinnate;
+
+/**
+ *  Document   : KinTermPanel
+ *  Created on : Apr 1, 2011, 9:41:11 AM
+ *  Author     : Peter Withers
+ */
+public interface KinTermSavePanel {
+
+    public void hideShow();
+
+    public void importKinTerms();
+
+    public void exportKinTerms();
+
+    public boolean isHidden();
+}
