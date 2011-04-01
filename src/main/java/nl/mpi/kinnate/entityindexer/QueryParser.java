e59e164093df0bdde7ca0869987b21b029ef0229
Peter Withers <Peter.Withers@mpi.nl>
2011-04-01 10:34:44 +0000

    Added text search to the kin type string processing.

--- src/main/java/nl/mpi/kinnate/entityindexer/QueryParser.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/QueryParser.java
@@ -1,13 +1,27 @@
 package nl.mpi.kinnate.entityindexer;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
+import nl.mpi.kinnate.kindata.EntityData;
+import nl.mpi.kinnate.kindata.EntityRelation;
+import nl.mpi.kinnate.kintypestrings.KinTypeStringConverter;
+import nl.mpi.kinnate.kintypestrings.KinTypeStringConverter.KinType;
 
 /**
  *  Document   : QueryParser
  *  Created on : Mar 31, 2011, 2:38:20 PM
  *  Author     : Peter Withers
  */
-public class QueryParser {
+public class QueryParser implements EntityService {
+
+    HashMap<String, EntityData> loadedGraphNodes;
+    EntityCollection entityCollection;
+
+    public QueryParser() {
+        entityCollection = new EntityCollection();
+        loadedGraphNodes = new HashMap<String, EntityData>();
+    }
 
     public String[][] getQueryStrings(String kinTypeString) {
         String kinType = null;
@@ -36,5 +50,86 @@ public class QueryParser {
             }
         }
         return queryTerms.toArray(new String[][]{});
+    }
+
+    private void getNextRelations(HashMap<String, EntityData> createdGraphNodes, EntityData egoNode, ArrayList<KinType> remainingKinTypes, IndexerParameters indexParameters) {
+        KinType currentKinType = remainingKinTypes.remove(0);
+        for (EntityRelation entityRelation : egoNode.getDistinctRelateNodes()) {
+            EntityData alterNode;
+            if (createdGraphNodes.containsKey(entityRelation.alterUniqueIdentifier)) {
+                alterNode = createdGraphNodes.get(entityRelation.alterUniqueIdentifier);
+            } else {
+                alterNode = entityCollection.getEntity(entityRelation.alterUniqueIdentifier, indexParameters);
+                createdGraphNodes.put(entityRelation.alterUniqueIdentifier, alterNode);
+            }
+            alterNode.isVisible = true;
+
+//            if (egoNode.relationMatchesType(entityRelation, currentKinType)) {
+            // only traverse if the type matches
+            if (remainingKinTypes.size() > 0) {
+                getNextRelations(createdGraphNodes, alterNode, remainingKinTypes, indexParameters);
+            }
+//            }
+        }
+    }
+
+    public EntityData[] getRelationsOfEgo(URI[] egoNodes, String[] uniqueIdentifiers, String[] kinTypeStrings, IndexerParameters indexParameters) throws EntityServiceException {
+        if (indexParameters.valuesChanged) {
+            indexParameters.valuesChanged = false;
+            loadedGraphNodes = new HashMap<String, EntityData>();
+        }
+        KinTypeStringConverter kinTypeStringConverter = new KinTypeStringConverter();
+        QueryParser queryParser = new QueryParser();
+        for (EntityData graphDataNode : loadedGraphNodes.values()) {
+            graphDataNode.clearVisibility();
+        }
+        for (String currentKinString : kinTypeStrings) {
+            for (String currentFoundId : entityCollection.getEntityIdByTerm(queryParser.getQueryStrings(currentKinString))) {
+                EntityData queryNode;
+                currentFoundId = currentFoundId.trim();
+                if (currentFoundId.length() > 0 /* make sure that non results do not get mistaken for an identifier */) {
+                    if (loadedGraphNodes.containsKey(currentFoundId)) {
+                        queryNode = loadedGraphNodes.get(currentFoundId);
+                    } else {
+                        queryNode = entityCollection.getEntity(currentFoundId, indexParameters);
+                        loadedGraphNodes.put(currentFoundId, queryNode);
+                    }
+                    queryNode.isVisible = true;
+                }
+            }
+        }
+        for (String currentEgoId : uniqueIdentifiers) {
+            EntityData egoNode;
+            if (loadedGraphNodes.containsKey(currentEgoId)) {
+                egoNode = loadedGraphNodes.get(currentEgoId);
+            } else {
+                egoNode = entityCollection.getEntity(currentEgoId, indexParameters);
+                loadedGraphNodes.put(currentEgoId, egoNode);
+            }
+            egoNode.isEgo = true;
+            egoNode.isVisible = true;
+            if (kinTypeStrings != null) {
+                for (String currentKinString : kinTypeStrings) {
+                    ArrayList<KinType> kinTypes = kinTypeStringConverter.getKinTypes(currentKinString);
+                    getNextRelations(loadedGraphNodes, egoNode, kinTypes, indexParameters);
+                }
+            }
+            // set the alter node object from the unique identifier
+            for (EntityData graphDataNode : loadedGraphNodes.values()) {
+                for (EntityRelation nodeRelation : graphDataNode.getDistinctRelateNodes()) {
+                    nodeRelation.setAlterNode(loadedGraphNodes.get(nodeRelation.alterUniqueIdentifier));
+                }
+            }
+        }
+        return loadedGraphNodes.values().toArray(new EntityData[]{});
+
+
+
+//        ArrayList<GraphDataNode> graphDataNodes = new ArrayList<GraphDataNode>();
+//        for (String entityIdentifier : uniqueIdentifiers) {
+//            graphDataNodes.add(getEntity(entityIdentifier, indexParameters));
+//        }
+//        // todo: process the kin type strings
+//        return graphDataNodes.toArray(new GraphDataNode[]{});
     }
 }
