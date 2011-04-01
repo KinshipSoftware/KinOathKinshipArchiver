99c6dfc4cefd348695547793af9a1327cff375e0
Peter Withers <Peter.Withers@mpi.nl>
2011-04-01 08:50:16 +0000

    Added kin term import and export and menu items for the kin terms.

---
+++ src/main/java/nl/mpi/kinnate/ui/KinTermsMenu.java
@@ -0,0 +1,83 @@
+package nl.mpi.kinnate.ui;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import nl.mpi.kinnate.KinTermSavePanel;
+
+/**
+ *  Document   : KinTermsMenu
+ *  Created on : Apr 1, 2011, 9:32:40 AM
+ *  Author     : Peter Withers
+ */
+public class KinTermsMenu extends JMenu {
+
+    static private MainFrame mainFrame;
+    KinTermSavePanel currentKinTermSavePanel;
+    JMenuItem hideShowMenu;
+    JMenuItem exportMenu;
+    JMenuItem importMenu;
+
+    public KinTermsMenu(MainFrame mainFrameLocal) {
+        mainFrame = mainFrameLocal;
+        initMenu();
+    }
+
+    public KinTermsMenu() {
+        initMenu();
+    }
+
+    private void initMenu() {
+        hideShowMenu = new JMenuItem("Hide/Show");
+        exportMenu = new JMenuItem("Export");
+        importMenu = new JMenuItem("Import");
+        this.setText("Kin Terms");
+        this.add(hideShowMenu);
+        this.add(importMenu);
+        this.add(exportMenu);
+        this.addMenuListener(new javax.swing.event.MenuListener() {
+
+            public void menuSelected(javax.swing.event.MenuEvent evt) {
+                // todo: check if a kin term pane is showing or associated with the diagram and that a diagam is showing
+                currentKinTermSavePanel = mainFrame.getKinTermPanel();
+                if (currentKinTermSavePanel != null) {
+                    hideShowMenu.setEnabled(true);
+                    if (currentKinTermSavePanel.isHidden()) {
+                        exportMenu.setEnabled(false);
+                        importMenu.setEnabled(false);
+                        hideShowMenu.setText("Show");
+                    } else {
+                        exportMenu.setEnabled(true);
+                        importMenu.setEnabled(true);
+                        hideShowMenu.setText("Hide");
+                    }
+                } else {
+                    exportMenu.setEnabled(false);
+                    importMenu.setEnabled(false);
+                    hideShowMenu.setEnabled(false);
+                }
+            }
+
+            public void menuDeselected(javax.swing.event.MenuEvent evt) {
+            }
+
+            public void menuCanceled(javax.swing.event.MenuEvent evt) {
+            }
+        });
+        hideShowMenu.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                currentKinTermSavePanel.hideShow();
+            }
+        });
+        exportMenu.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            }
+        });
+        importMenu.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            }
+        });
+    }
+}
