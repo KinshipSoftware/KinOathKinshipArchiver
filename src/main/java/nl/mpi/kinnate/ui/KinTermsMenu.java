504235476340a3ccb129a0fbd5281be779c8abca
Peter Withers <Peter.Withers@mpi.nl>
2011-04-11 09:41:02 +0000

    Added ability to have multiple kin term groups in one diagram.

--- src/main/java/nl/mpi/kinnate/ui/KinTermsMenu.java
+++ src/main/java/nl/mpi/kinnate/ui/KinTermsMenu.java
@@ -14,6 +14,7 @@ public class KinTermsMenu extends JMenu {
     static private MainFrame mainFrame;
     KinTermSavePanel currentKinTermSavePanel;
     JMenuItem hideShowMenu;
+    JMenuItem newMenu;
     JMenuItem exportMenu;
     JMenuItem importMenu;
 
@@ -28,10 +29,12 @@ public class KinTermsMenu extends JMenu {
 
     private void initMenu() {
         hideShowMenu = new JMenuItem("Hide/Show");
+        newMenu = new JMenuItem("New Kin Term Group");
         exportMenu = new JMenuItem("Export");
         importMenu = new JMenuItem("Import");
         this.setText("Kin Terms");
         this.add(hideShowMenu);
+        this.add(newMenu);
         this.add(importMenu);
         this.add(exportMenu);
         this.addMenuListener(new javax.swing.event.MenuListener() {
@@ -44,16 +47,19 @@ public class KinTermsMenu extends JMenu {
                     if (currentKinTermSavePanel.isHidden()) {
                         exportMenu.setEnabled(false);
                         importMenu.setEnabled(false);
+                        newMenu.setEnabled(false);
                         hideShowMenu.setText("Show");
                     } else {
                         exportMenu.setEnabled(true);
                         importMenu.setEnabled(true);
+                        newMenu.setEnabled(true);
                         hideShowMenu.setText("Hide");
                     }
                 } else {
                     exportMenu.setEnabled(false);
                     importMenu.setEnabled(false);
                     hideShowMenu.setEnabled(false);
+                    newMenu.setEnabled(false);
                 }
             }
 
@@ -79,6 +85,12 @@ public class KinTermsMenu extends JMenu {
 
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 currentKinTermSavePanel.importKinTerms();
+            }
+        });
+        newMenu.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                currentKinTermSavePanel.addKinTermGroup();
             }
         });
     }
