6e44788827ddd7c4982b81753f47f71ea501f60c
Peter Withers <Peter.Withers@mpi.nl>
2011-04-11 09:50:18 +0000

    Added ability to have multiple kin term groups in one diagram.

---
+++ src/main/java/nl/mpi/kinnate/ui/KinTermTabPane.java
@@ -0,0 +1,54 @@
+package nl.mpi.kinnate.ui;
+
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import nl.mpi.kinnate.SavePanel;
+import nl.mpi.kinnate.kintypestrings.KinTerms;
+
+/**
+ *  Document   : KinTermTabPane
+ *  Created on : Apr 8, 2011, 4:24:56 PM
+ *  Author     : Peter Withers
+ */
+public class KinTermTabPane extends JPanel {
+
+    JTabbedPane tabbedPane;
+    SavePanel savePanel;
+
+    public KinTermTabPane(SavePanel savePanelLocal, KinTerms[] kinTermsArray) {
+        savePanel = savePanelLocal;
+        tabbedPane = new JTabbedPane();
+        this.setLayout(new BorderLayout());
+        JMenuBar kintermMenuBar = new JMenuBar();
+        kintermMenuBar.add(new KinTermsMenu());
+        this.add(kintermMenuBar, BorderLayout.PAGE_START);
+        this.add(tabbedPane, BorderLayout.CENTER);
+        for (KinTerms kinTerms : kinTermsArray) {
+            tabbedPane.add(kinTerms.titleString, new KinTermPanel(savePanelLocal, kinTerms));
+        }
+    }
+
+    public void updateKinTerms(KinTerms[] kinTermsArray) {
+        int lastSelectedIndex = tabbedPane.getSelectedIndex();
+        int lastTabCount = tabbedPane.getTabCount();
+        tabbedPane.removeAll();
+        for (KinTerms kinTerms : kinTermsArray) {
+            tabbedPane.add(kinTerms.titleString, new KinTermPanel(savePanel, kinTerms));
+        }
+        if (lastTabCount != tabbedPane.getTabCount()) {
+            tabbedPane.setSelectedIndex(tabbedPane.getTabCount() - 1);
+        } else {
+            tabbedPane.setSelectedIndex(lastSelectedIndex);
+        }
+    }
+
+    public KinTerms getSelectedKinTerms() {
+        return ((KinTermPanel) tabbedPane.getSelectedComponent()).kinTerms;
+    }
+
+    public KinTermPanel getSelectedKinTermPanel() {
+        return (KinTermPanel) tabbedPane.getSelectedComponent();
+    }
+}
