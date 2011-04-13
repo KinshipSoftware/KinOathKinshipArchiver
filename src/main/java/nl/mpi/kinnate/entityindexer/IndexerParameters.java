90eca8aafdace900bf900aa74ce39395dc505d85
Peter Withers <Peter.Withers@mpi.nl>
2011-04-13 13:51:37 +0000

    Added the use of jaxb to save the current settings used to generate the graph in the graph svg file. Separated Index Parameters into a separate class.

--- src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
@@ -1,7 +1,7 @@
 package nl.mpi.kinnate.entityindexer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import javax.xml.bind.annotation.XmlElement;
 
 /**
  *  Document   : IndexParameters
@@ -9,69 +9,19 @@ import java.util.Arrays;
  *  Author     : Peter Withers
  */
 public class IndexerParameters {
+
     public boolean valuesChanged = false;
-
-    public class IndexerParam {
-
-        private String[][] valuesArray;
-        private String[] availableValuesArray = null;
-
-        public IndexerParam(String[][] valuesArrayLocal) {
-            valuesArray = valuesArrayLocal;
-        }
-
-        public void setValues(String[][] valuesArrayLocal) {
-            valuesArray = valuesArrayLocal;
-            IndexerParameters.this.relevantEntityData = null;
-            // cause the entity index and entity collection to update based on the new indexer values
-            valuesChanged = true;
-        }
-
-        public void setValue(String parameterString, String valueString) {
-            ArrayList<String[]> tempArrayList = new ArrayList<String[]>(Arrays.asList(valuesArray));
-            for (String[] currentEntry : valuesArray) {
-                if (currentEntry[0].equals(parameterString)) {
-                    currentEntry[1] = valueString;
-                }
-            }
-            setValues(tempArrayList.toArray(new String[][]{}));
-            // cause the entity index and entity collection to update based on the new indexer values
-            valuesChanged = true;
-        }
-
-        public void removeValue(String valueToRemove) {
-            ArrayList<String[]> tempArrayList = new ArrayList<String[]>(Arrays.asList(valuesArray));
-            for (String[] currentEntry : valuesArray) {
-                if (currentEntry[0].equals(valueToRemove)) {
-                    tempArrayList.remove(currentEntry); // todo: will array list remove use the value of the array or the pointer to the array?????
-                }
-            }
-            setValues(tempArrayList.toArray(new String[][]{}));
-            // cause the entity index and entity collection to update based on the new indexer values
-            valuesChanged = true;
-        }
-
-        public String[][] getValues() {
-            return valuesArray;
-        }
-
-        public void setAvailableValues(String[] availableValuesArrayLocal) {
-            availableValuesArray = availableValuesArrayLocal;
-        }
-
-        public String[] getAvailableValues() {
-            return availableValuesArray;
-        }
-    }
     public String linkPath = "/Kinnate/Relation/Link";
 //    public IndexerParam relevantEntityData = new IndexerParam(new String[][]{{"Kinnate/Gedcom/Entity/NoteText"}, {"Kinnate/Gedcom/Entity/SEX"}, {"Kinnate/Gedcom/Entity/GedcomType"}, {"Kinnate/Gedcom/Entity/NAME/NAME"}, {"Kinnate/Gedcom/Entity/NAME/NPFX"}}); // todo: the relevantData array comes from the user via the svg
-    public IndexerParam relevantLinkData = new IndexerParam(new String[][]{{"Type"}});
-    public IndexerParam labelFields = new IndexerParam(new String[][]{{"Kinnate/Gedcom/Entity/NAME/NAME"}, {"Kinnate/Gedcom/Entity[GedcomType='FAM']/GedcomType"}, {"Kinnate/Gedcom/Entity/Text"}, {"Kinnate/Gedcom/Entity/NAME/NPFX"}, {"Kinnate/Gedcom/Entity/NoteText"}});
-    public IndexerParam symbolFieldsFields = new IndexerParam(new String[][]{{"Kinnate/Gedcom/Entity[SEX='M']", "triangle"}, {"Kinnate/Gedcom/Entity[SEX='F']", "circle"}, {"Kinnate/Gedcom/Entity[GedcomType='FAM']", "union"}});
-    public IndexerParam ancestorFields = new IndexerParam(new String[][]{{"Kinnate.Gedcom.Entity.FAMC"}, {"Kinnate.Gedcom.Entity.BIRT.FAMC"}, {"Kinnate.Gedcom.Entity.CHR.FAMC"}, {"Kinnate.Gedcom.Entity.ADOP.FAMC"}, {"Kinnate.Gedcom.Entity.SLGC.FAMC"}, {"Kinnate.Gedcom.Entity.HUSB"}, {"Kinnate.Gedcom.Entity.WIFE"}});
+    public IndexerParam relevantLinkData = new IndexerParam(this, new String[][]{{"Type"}});
+    @XmlElement(name = "kin:LabelFields")
+    public IndexerParam labelFields = new IndexerParam(this, new String[][]{{"Kinnate/Gedcom/Entity/NAME/NAME"}, {"Kinnate/Gedcom/Entity[GedcomType='FAM']/GedcomType"}, {"Kinnate/Gedcom/Entity/Text"}, {"Kinnate/Gedcom/Entity/NAME/NPFX"}, {"Kinnate/Gedcom/Entity/NoteText"}});
+    public IndexerParam symbolFieldsFields = new IndexerParam(this, new String[][]{{"Kinnate/Gedcom/Entity[SEX='M']", "triangle"}, {"Kinnate/Gedcom/Entity[SEX='F']", "circle"}, {"Kinnate/Gedcom/Entity[GedcomType='FAM']", "union"}});
+    public IndexerParam ancestorFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.FAMC"}, {"Kinnate.Gedcom.Entity.BIRT.FAMC"}, {"Kinnate.Gedcom.Entity.CHR.FAMC"}, {"Kinnate.Gedcom.Entity.ADOP.FAMC"}, {"Kinnate.Gedcom.Entity.SLGC.FAMC"}, {"Kinnate.Gedcom.Entity.HUSB"}, {"Kinnate.Gedcom.Entity.WIFE"}});
 //    public IndexerParam siblingFields = new IndexerParam(new String[]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}});
-    public IndexerParam decendantFields = new IndexerParam(new String[][]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}});
+    public IndexerParam decendantFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}});
     //    public IndexerParam showEntityFields = new IndexerParam(new String[][]{{"Kinnate/Gedcom/Entity/GedcomType=INDI"}, {"Kinnate/Gedcom/Entity/GedcomType=FAM"}}); // todo: add fields that can be used to controll which nodes are shown
+    @Deprecated // I think this is no longer used or needed
     private String[][] relevantEntityData = null;
 
     @Deprecated // I think this is no longer used or needed
