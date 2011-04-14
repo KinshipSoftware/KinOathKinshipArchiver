7d874679125b23316554e4a6708d3a06fe97cc23
Peter Withers <Peter.Withers@mpi.nl>
2011-04-14 12:42:13 +0000

    Added the use of jaxb to save the current settings used to generate the graph in the graph svg file. Added the use of jaxb to load indexersettings and kin parameters from the svg. Separated Index Parameters into a separate class.

--- src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
@@ -1,7 +1,7 @@
 package nl.mpi.kinnate.entityindexer;
 
-import java.util.ArrayList;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *  Document   : IndexParameters
@@ -10,34 +10,37 @@ import javax.xml.bind.annotation.XmlElement;
  */
 public class IndexerParameters {
 
+    @XmlTransient
     public boolean valuesChanged = false;
+    @XmlTransient
     public String linkPath = "/Kinnate/Relation/Link";
 //    public IndexerParam relevantEntityData = new IndexerParam(new String[][]{{"Kinnate/Gedcom/Entity/NoteText"}, {"Kinnate/Gedcom/Entity/SEX"}, {"Kinnate/Gedcom/Entity/GedcomType"}, {"Kinnate/Gedcom/Entity/NAME/NAME"}, {"Kinnate/Gedcom/Entity/NAME/NPFX"}}); // todo: the relevantData array comes from the user via the svg
+    @XmlTransient
     public IndexerParam relevantLinkData = new IndexerParam(this, new String[][]{{"Type"}});
-    @XmlElement(name = "kin:LabelFields")
+    @XmlElement(name = "LabelFields", namespace = "http://mpi.nl/tla/kin")
     public IndexerParam labelFields = new IndexerParam(this, new String[][]{{"Kinnate/Gedcom/Entity/NAME/NAME"}, {"Kinnate/Gedcom/Entity[GedcomType='FAM']/GedcomType"}, {"Kinnate/Gedcom/Entity/Text"}, {"Kinnate/Gedcom/Entity/NAME/NPFX"}, {"Kinnate/Gedcom/Entity/NoteText"}});
-    @XmlElement(name = "kin:SymbolFieldsFields")
+    @XmlElement(name = "SymbolFieldsFields", namespace = "http://mpi.nl/tla/kin")
     public IndexerParam symbolFieldsFields = new IndexerParam(this, new String[][]{{"Kinnate/Gedcom/Entity[SEX='M']", "triangle"}, {"Kinnate/Gedcom/Entity[SEX='F']", "circle"}, {"Kinnate/Gedcom/Entity[GedcomType='FAM']", "union"}});
-    @XmlElement(name = "kin:AncestorFields")
+    @XmlElement(name = "AncestorFields", namespace = "http://mpi.nl/tla/kin")
     public IndexerParam ancestorFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.FAMC"}, {"Kinnate.Gedcom.Entity.BIRT.FAMC"}, {"Kinnate.Gedcom.Entity.CHR.FAMC"}, {"Kinnate.Gedcom.Entity.ADOP.FAMC"}, {"Kinnate.Gedcom.Entity.SLGC.FAMC"}, {"Kinnate.Gedcom.Entity.HUSB"}, {"Kinnate.Gedcom.Entity.WIFE"}});
 //    public IndexerParam siblingFields = new IndexerParam(new String[]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}});
-    @XmlElement(name = "kin:DecendantFields")
+    @XmlElement(name = "DecendantFields", namespace = "http://mpi.nl/tla/kin")
     public IndexerParam decendantFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}});
     //    public IndexerParam showEntityFields = new IndexerParam(new String[][]{{"Kinnate/Gedcom/Entity/GedcomType=INDI"}, {"Kinnate/Gedcom/Entity/GedcomType=FAM"}}); // todo: add fields that can be used to controll which nodes are shown
-    @Deprecated // I think this is no longer used or needed
-    private String[][] relevantEntityData = null;
-
-    @Deprecated // I think this is no longer used or needed
-    public String[][] getRelevantEntityData() {
-        if (relevantEntityData == null) {
-            ArrayList<String[]> relevantDataList = new ArrayList<String[]>();
-            for (IndexerParam currentIndexerParam : new IndexerParam[]{labelFields, symbolFieldsFields/*, showEntityFields*/}) {
-                for (String[] currentData : currentIndexerParam.getValues()) {
-                    relevantDataList.add(currentData);
-                }
-            }
-            relevantEntityData = relevantDataList.toArray(new String[][]{});
-        }
-        return relevantEntityData;
-    }
+//    @Deprecated // I think this is no longer used or needed
+//    private String[][] relevantEntityData = null;
+//
+//    @Deprecated // I think this is no longer used or needed
+//    public String[][] getRelevantEntityData() {
+//        if (relevantEntityData == null) {
+//            ArrayList<String[]> relevantDataList = new ArrayList<String[]>();
+//            for (IndexerParam currentIndexerParam : new IndexerParam[]{labelFields, symbolFieldsFields/*, showEntityFields*/}) {
+//                for (String[] currentData : currentIndexerParam.getValues()) {
+//                    relevantDataList.add(currentData);
+//                }
+//            }
+//            relevantEntityData = relevantDataList.toArray(new String[][]{});
+//        }
+//        return relevantEntityData;
+//    }
 }
