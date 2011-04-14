30de23e39c6ee04f01e4976dca54366fb21476b1
Peter Withers <Peter.Withers@mpi.nl>
2011-04-14 12:34:56 +0000

    Added the use of jaxb to save the current settings used to generate the graph in the graph svg file. Added the use of jaxb to load indexersettings and kin parameters from the svg. Separated Index Parameters into a separate class.

---
+++ src/main/java/nl/mpi/kinnate/entityindexer/ParameterElement.java
@@ -0,0 +1,44 @@
+package nl.mpi.kinnate.entityindexer;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+/**
+ *  Document   : ParameterElement
+ *  Created on : Apr 14, 2011, 11:23:20 AM
+ *  Author     : Peter Withers
+ */
+public class ParameterElement {
+
+    @XmlAttribute(name = "XPath", namespace = "http://mpi.nl/tla/kin")
+    String xpathString = null;
+    @XmlAttribute(name = "Value", namespace = "http://mpi.nl/tla/kin")
+    String selectedValue = null;
+
+    public ParameterElement() {
+    }
+
+    public ParameterElement(String xpathStringLocal, String selectedValueLocal) {
+        xpathString = xpathStringLocal;
+        selectedValue = selectedValueLocal;
+    }
+
+    public ParameterElement(String[] valuesArrayLocal) {
+        xpathString = valuesArrayLocal[0];
+        if (valuesArrayLocal.length > 1) {
+            selectedValue = valuesArrayLocal[1];
+        }
+    }
+
+    public boolean hasSelectedValue() {
+        return selectedValue != null;
+    }
+
+    public String getSelectedValue() {
+        return selectedValue;
+    }
+
+    public String getXpathString() {
+        return xpathString;
+    }
+}
