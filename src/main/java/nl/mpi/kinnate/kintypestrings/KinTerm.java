3bd64abaa86ae0a7bb9a1624376af04784b113ca
Peter Withers <Peter.Withers@mpi.nl>
2011-04-15 10:03:13 +0000

    Added a framework for triangular kin terms and added the kin term groups to the svg storage.

---
+++ src/main/java/nl/mpi/kinnate/kintypestrings/KinTerm.java
@@ -0,0 +1,16 @@
+package nl.mpi.kinnate.kintypestrings;
+
+/**
+ *  Document   : KinTerm
+ *  Created on : Apr 15, 2011, 11:49:26 AM
+ *  Author     : Peter Withers
+ */
+public class KinTerm {
+
+    String kinTerm = null;
+    String kinTermDescription = null;
+    String alterKinTypeString = null;
+    String propositusKinTypeStrings = null;
+    String anchorKinTypeStrings = null;
+    String alterKinTypeStrings = null;
+}
