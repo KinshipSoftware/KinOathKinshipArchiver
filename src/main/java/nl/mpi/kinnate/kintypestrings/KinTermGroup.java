6c2c0334075d45e9192d7a16892719a07aab9275
Peter Withers <Peter.Withers@mpi.nl>
2011-04-15 12:08:01 +0000

    Added a framework for triangular kin terms and added the kin term groups to the svg storage.

--- src/main/java/nl/mpi/kinnate/kintypestrings/KinTermGroup.java
+++ src/main/java/nl/mpi/kinnate/kintypestrings/KinTermGroup.java
@@ -1,6 +1,7 @@
 package nl.mpi.kinnate.kintypestrings;
 
 import java.util.ArrayList;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 
 /**
@@ -10,55 +11,54 @@ import javax.xml.bind.annotation.XmlElement;
  */
 public class KinTermGroup {
 
-    @XmlElement(name = "GroupName", namespace = "http://mpi.nl/tla/kin")
+    @XmlAttribute(name = "GroupName", namespace = "http://mpi.nl/tla/kin")
     public String titleString;
-    @XmlElement(name = "GroupColour", namespace = "http://mpi.nl/tla/kin")
+    @XmlAttribute(name = "GroupColour", namespace = "http://mpi.nl/tla/kin")
     public String graphColour;
     @XmlElement(name = "KinTerm", namespace = "http://mpi.nl/tla/kin")
-    ArrayList<String[]> kinTermArray;
+    ArrayList<KinTerm> kinTermArray;
 
     public KinTermGroup() {
         titleString = "Sample Kin Terms";
         graphColour = "blue";
         //todo: sample kin terms to put into a sensible defaults place
-        kinTermArray = new ArrayList<String[]>();
-        kinTermArray.add(new String[]{"MM", "Grand Mother"});
-        kinTermArray.add(new String[]{"MZ" + "|" + "FZ", "Aunt"});
-        kinTermArray.add(new String[]{"MB" + "|" + "FB", "Uncle"});
-        kinTermArray.add(new String[]{"FF", "Grand Father"});
+        kinTermArray = new ArrayList<KinTerm>();
+        kinTermArray.add(new KinTerm("Grand Mother", null, "MM", null, null));
+        kinTermArray.add(new KinTerm("Aunt", null, "MZ" + "|" + "FZ", null, null));
+        kinTermArray.add(new KinTerm("Uncle", null, "MB" + "|" + "FB", null, null));
+        kinTermArray.add(new KinTerm("Grand Father", null, "FF", null, null));
     }
 
-    public void addKinTerm(String kinTypeStrings, String kinTermLabel) {
-        kinTermArray.add(new String[]{kinTypeStrings, kinTermLabel});
+    public void addKinTerm(String kinTypeStrings, String kinTerm) {
+        kinTermArray.add(new KinTerm(kinTerm, null, kinTypeStrings, null, null));
     }
 
-    public void updateKinTerm(String kinTypeStrings, String kinTermLabel) {
-        for (String[] kinTermItem : kinTermArray) {
-            if (kinTermItem[1].equals(kinTermLabel)) {
-                kinTermItem[0] = kinTypeStrings;
+    public void updateKinTerm(String kinTypeStrings, String kinTerm) {
+        for (KinTerm kinTermItem : kinTermArray) {
+            if (kinTermItem.kinTerm.equals(kinTerm)) {
+                kinTermItem.alterKinTypeStrings = kinTypeStrings;
             }
         }
     }
 
-    public void removeKinTerm(String kinTermLabel) {
-        ArrayList<String[]> removeTermArray = new ArrayList<String[]>();
-        for (String[] kinTermItem : kinTermArray) {
-            if (kinTermItem[1].equals(kinTermLabel)) {
-                removeTermArray.add(kinTermItem);
+    public void removeKinTerm(String kinTerm) {
+        for (KinTerm kinTermItem : getKinTerms()) {
+            if (kinTermItem.kinTerm.equals(kinTerm)) {
+                kinTermArray.remove(kinTermItem);
             }
         }
-        kinTermArray.removeAll(removeTermArray);
     }
 
-    public String[][] getKinTerms() {
-        return kinTermArray.toArray(new String[][]{});
+    public KinTerm[] getKinTerms() {
+        return kinTermArray.toArray(new KinTerm[]{});
     }
 
     public String getTermLabel(String kinTypeString) {
-        for (String[] kinTermItem : kinTermArray) {
-            for (String kinType : kinTermItem[0].split("\\|")) {
+        // todo: add the propositus to the selection criteria
+        for (KinTerm kinTermItem : kinTermArray) {
+            for (String kinType : kinTermItem.alterKinTypeStrings.split("\\|")) {
                 if (kinTypeString.trim().equals(kinType.trim())) {
-                    return kinTermItem[1];
+                    return kinTermItem.kinTerm;
                 }
             }
         }
