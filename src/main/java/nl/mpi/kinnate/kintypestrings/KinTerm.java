f1441a36e194047db31e846066f4978fcc766aa9
Peter Withers <Peter.Withers@mpi.nl>
2011-04-15 13:10:38 +0000

    Added a framework for triangular kin terms and added the kin term groups to the svg storage.

--- src/main/java/nl/mpi/kinnate/kintypestrings/KinTerm.java
+++ src/main/java/nl/mpi/kinnate/kintypestrings/KinTerm.java
@@ -12,9 +12,10 @@ public class KinTerm {
     public KinTerm() {
     }
 
-    public KinTerm(String kinTermLocal, String kinTermDescriptionLocal, String alterKinTypeStringsLocal, String propositusKinTypeStringsLocal, String anchorKinTypeStringsLocal) {
+    public KinTerm(String kinTermLocal, String kinTermDescriptionLocal, String egoTypeLocal, String alterKinTypeStringsLocal, String propositusKinTypeStringsLocal, String anchorKinTypeStringsLocal) {
         kinTerm = kinTermLocal;
         kinTermDescription = kinTermDescriptionLocal;
+        egoType = egoTypeLocal;
         alterKinTypeStrings = alterKinTypeStringsLocal;
         propositusKinTypeStrings = propositusKinTypeStringsLocal;
         anchorKinTypeStrings = anchorKinTypeStringsLocal;
@@ -23,6 +24,8 @@ public class KinTerm {
     public String kinTerm = null;
     @XmlAttribute(name = "Description", namespace = "http://mpi.nl/tla/kin")
     public String kinTermDescription = null;
+    @XmlAttribute(name = "Ego", namespace = "http://mpi.nl/tla/kin")
+    public String egoType = null;
     @XmlAttribute(name = "Alter", namespace = "http://mpi.nl/tla/kin")
     public String alterKinTypeStrings = null;
     @XmlAttribute(name = "Propositus", namespace = "http://mpi.nl/tla/kin")
