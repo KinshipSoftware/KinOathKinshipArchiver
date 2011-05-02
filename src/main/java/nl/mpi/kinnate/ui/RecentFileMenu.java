7f534d7a5223ba25d6a810d31457e75e14efad8e
Peter Withers <Peter.Withers@mpi.nl>
2011-05-02 13:03:52 +0000

    Added a recent file menu. Added kin type group description. Added sample Japanese kin terms.

---
+++ src/main/java/nl/mpi/kinnate/ui/RecentFileMenu.java
@@ -0,0 +1,15 @@
+package nl.mpi.kinnate.ui;
+
+import javax.swing.JMenu;
+
+/**
+ *  Document   : RecentFileMenu
+ *  Created on : Apr 15, 2011, 11:30:39 AM
+ *  Author     : Peter Withers
+ */
+public class RecentFileMenu extends JMenu {
+
+    public void addRecentFile(String recentFile) {
+        // todo: store the accessed and saved files and provide a menu of recent files
+    }
+}
