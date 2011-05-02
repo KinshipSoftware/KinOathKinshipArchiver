ff3334caa94fab29a1862353e6ff55b8c7a75efe
Peter Withers <Peter.Withers@mpi.nl>
2011-05-02 12:55:42 +0000

    Added kin type group description. Added sample Japanese kin terms.

--- src/main/java/nl/mpi/kinnate/kintypestrings/KinTermGroup.java
+++ src/main/java/nl/mpi/kinnate/kintypestrings/KinTermGroup.java
@@ -13,6 +13,8 @@ public class KinTermGroup {
 
     @XmlAttribute(name = "GroupName", namespace = "http://mpi.nl/tla/kin")
     public String titleString;
+    @XmlAttribute(name = "GroupDescription", namespace = "http://mpi.nl/tla/kin")
+    public String descriptionString;
     @XmlAttribute(name = "GroupColour", namespace = "http://mpi.nl/tla/kin")
     public String graphColour;
     @XmlAttribute(name = "Show", namespace = "http://mpi.nl/tla/kin")
@@ -23,14 +25,59 @@ public class KinTermGroup {
     private ArrayList<KinTerm> kinTermArray;
 
     public KinTermGroup() {
-        titleString = "Unnamed Kin Term Group";
-        graphColour = "blue";
-        //todo: sample kin terms to put into a sensible defaults place
         kinTermArray = new ArrayList<KinTerm>();
-        kinTermArray.add(new KinTerm("Grand Mother", null, null, "MM", null, null));
-        kinTermArray.add(new KinTerm("Aunt", null, null, "MZ" + "|" + "FZ", null, null));
-        kinTermArray.add(new KinTerm("Uncle", null, null, "MB" + "|" + "FB", null, null));
-        kinTermArray.add(new KinTerm("Grand Father", null, null, "FF", null, null));
+    }
+
+    public KinTermGroup(int type) {
+        switch (type) {
+            case 0:
+                titleString = "English terms";
+                graphColour = "blue";
+                //todo: sample kin terms to put into a sensible defaults place
+                kinTermArray = new ArrayList<KinTerm>();
+                kinTermArray.add(new KinTerm("Grand Mother", null, null, "MM", null, null));
+                kinTermArray.add(new KinTerm("Aunt", null, null, "MZ" + "|" + "FZ", null, null));
+                kinTermArray.add(new KinTerm("Uncle", null, null, "MB" + "|" + "FB", null, null));
+                kinTermArray.add(new KinTerm("Grand Father", null, null, "FF", null, null));
+                break;
+            case 1:
+                titleString = "Hawaiian Kin Terms";
+                descriptionString = "http://umanitoba.ca/faculties/arts/anthropology/tutor/kinterms/hawaiian.html";
+                graphColour = "cyan";
+                kinTermArray = new ArrayList<KinTerm>();
+                kinTermArray.add(new KinTerm("Makuakane", "Father, Uncle", null, "F|FB|MB", null, null));
+                kinTermArray.add(new KinTerm("Makuahini", "Mother, Aunt", null, "M|MZ|FZ", null, null));
+                kinTermArray.add(new KinTerm("Kaikua'ana", "", null, "E(male)B|E(male)FBS|E(male)FZS|E(male)MBS|E(male)MZS", null, null));
+                kinTermArray.add(new KinTerm("Kaikua'ana", "Brother/Sister, Cousin", null, "E(female)Z|E(female)FBD|E(female)FZD|E(female)MBD|E(female)MZD", null, null));
+                kinTermArray.add(new KinTerm("Kaikuane", "Brother, Cousin", null, "E(female)B|E(female)FBS|E(female)FZS|E(female)MBS|E(female)MZS", null, null));
+                kinTermArray.add(new KinTerm("Kaikuane", "Sister, Cousin", null, "E(male)Z|E(male)FBD|E(male)FZD|E(male)MBD|E(male)MZD", null, null));
+                kinTermArray.add(new KinTerm("Keikikane", "Son, Nephew", null, "S|BS|ZS", null, null));
+                kinTermArray.add(new KinTerm("Keikamahini", "Daughter, Niece", null, "D|BD|ZD", null, null));
+                break;
+
+//    <kin:KinTermGroups kin:Show="true" kin:GroupColour="blue" kin:GroupName="Japanese Kin Terms" kin:Generate="true">
+//      <kin:KinTerm kin:Term="?" kin:Alter="B" kin:Propositus="^B" kin:Description="Elder Brother (when speaking to other than family)"/>
+//      <kin:KinTerm kin:Term="?" kin:Alter="Z(younger)" kin:Propositus="^Z" kin:Description="Younger Sister (when speaking to other than family)"/>
+//      <kin:KinTerm kin:Term="?" kin:Alter="F" kin:Propositus="^F" kin:Description="Father (when speaking to other than family)"/>
+//      <kin:KinTerm kin:Term="?" kin:Alter="M" kin:Propositus="^M" kin:Description="Mother (when speaking to other than family)"/>
+//      <kin:KinTerm kin:Term="?" kin:Alter="Z" kin:Propositus="^Z" kin:Description="Elder Sister (when speaking to other than family)"/>
+//      <kin:KinTerm kin:Term="?" kin:Alter="B" kin:Propositus="^B (this needs to be able to indicate not family)" kin:Description="Yonger Brother (when speaking to other than family)"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Younger brother of parent (when speaking to other than family)" kin:Alter="PB"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Younger sister of parent (when speaking to other than family)" kin:Alter="PZ"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Older brother of parents (when speaking to other than family)" kin:Alter="PB"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Older sister of parents (when speaking to other than family)" kin:Alter="PZ"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Older male cousin (when speaking to other than family)" kin:Alter="PZS|PBS"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Older female cousin (when speaking to other than family)" kin:Alter="PZD|PBD"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Yonger male cousin (when speaking to other than family)" kin:Alter="PZS|PBS"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Yonger female cousin (when speaking to other than family)" kin:Alter="PZD|PBD"/>
+//      <kin:KinTerm kin:Term="???" kin:Description="Second Cousin" kin:Alter="PPZCC|PPBCC"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Grand Father" kin:Alter="PF"/>
+//      <kin:KinTerm kin:Term="??" kin:Description="Grand Mother" kin:Alter="PM"/>
+//    </kin:KinTermGroups>
+
+            default:
+                kinTermArray = new ArrayList<KinTerm>();
+        }
     }
 
     public void addKinTerm(String kinTypeStrings, String kinTerm) {
