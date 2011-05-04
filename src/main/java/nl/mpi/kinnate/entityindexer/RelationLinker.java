85d0be6fd0e9845eab151ec339cf9e59b2156167
Peter Withers <Peter.Withers@mpi.nl>
2011-05-04 14:35:53 +0000

    Added context menu item to link entities with various kinds of relations.

--- src/main/java/nl/mpi/kinnate/entityindexer/RelationLinker.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/RelationLinker.java
@@ -1,7 +1,18 @@
 package nl.mpi.kinnate.entityindexer;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.xml.parsers.ParserConfigurationException;
+import nl.mpi.arbil.data.ArbilComponentBuilder;
+import nl.mpi.arbil.util.ArbilBugCatcher;
 import nl.mpi.kinnate.kindata.DataTypes;
 import nl.mpi.kinnate.svg.GraphPanel;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 /**
  *  Document   : RelationLinker
@@ -11,6 +22,48 @@ import nl.mpi.kinnate.svg.GraphPanel;
 public class RelationLinker {
 
     public void linkEntities(GraphPanel graphPanel, String[] selectedIdentifiers, DataTypes.RelationType relationType) {
-        // graphPanel.getPathForElementId()
+        String targetPath = graphPanel.getPathForElementId(selectedIdentifiers[0]);
+        try {
+            // todo: any changes to the ArbilDateNode should be saved here, although this should not be an issue because nodes are only appended to the end
+            URI targetUri = new URI(targetPath);
+            Document metadataDom = ArbilComponentBuilder.getDocument(targetUri);
+            for (int relationCounter = 1 /* skip the first because that is the target */; relationCounter < selectedIdentifiers.length; relationCounter++) {
+                Element relationElement = metadataDom.createElement("Relation");
+                metadataDom.getDocumentElement().appendChild(relationElement);
+
+                Element linkElement = metadataDom.createElement("Link");
+                linkElement.setTextContent(graphPanel.getPathForElementId(selectedIdentifiers[relationCounter]));
+                relationElement.appendChild(linkElement);
+
+                // add a unique identifier of the target entity to the link
+                Element uniqueIdentifierElement = metadataDom.createElement("UniqueIdentifier");
+                Element localIdentifierElement = metadataDom.createElement("LocalIdentifier");
+                localIdentifierElement.setTextContent(selectedIdentifiers[relationCounter]);
+                uniqueIdentifierElement.appendChild(localIdentifierElement);
+                relationElement.appendChild(uniqueIdentifierElement);
+
+                Element typeElement = metadataDom.createElement("Type");
+                typeElement.setTextContent(relationType.name());
+                relationElement.appendChild(typeElement);
+
+                Element targetNameElement = metadataDom.createElement("TargetName");
+//                targetNameElement.setTextContent(lineParts[2]);
+                relationElement.appendChild(targetNameElement);
+            }
+            ArbilComponentBuilder.savePrettyFormatting(metadataDom, new File(targetUri));
+            new EntityCollection().updateDatabase(targetUri);
+            //ArbilDataNodeLoader.getSingleInstance().
+            // todo: the ArbilDateNode will need to be reloaded here
+        } catch (URISyntaxException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } catch (DOMException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } catch (IOException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } catch (ParserConfigurationException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } catch (SAXException exception) {
+            new ArbilBugCatcher().logError(exception);
+        }
     }
 }
