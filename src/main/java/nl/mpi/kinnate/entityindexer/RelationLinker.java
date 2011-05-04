83800d4756a746d87aabf759c45fc21f43477de5
Peter Withers <Peter.Withers@mpi.nl>
2011-05-04 10:12:24 +0000

    Added context menu item to link entities with various kinds of relations.

---
+++ src/main/java/nl/mpi/kinnate/entityindexer/RelationLinker.java
@@ -0,0 +1,16 @@
+package nl.mpi.kinnate.entityindexer;
+
+import nl.mpi.kinnate.kindata.DataTypes;
+import nl.mpi.kinnate.svg.GraphPanel;
+
+/**
+ *  Document   : RelationLinker
+ *  Created on : Apr 12, 2011, 1:45:01 PM
+ *  Author     : Peter Withers
+ */
+public class RelationLinker {
+
+    public void linkEntities(GraphPanel graphPanel, String[] selectedIdentifiers, DataTypes.RelationType relationType) {
+        // graphPanel.getPathForElementId()
+    }
+}
