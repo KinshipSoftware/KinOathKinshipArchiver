11ea87e9885c3e71be670ed3c646a7d1a2943e74
Peter Withers <Peter.Withers@mpi.nl>
2011-05-04 09:14:04 +0000

    Migrated kinnate trunk to use the latest Arbil jar.

--- src/main/java/nl/mpi/kinnate/ui/ArchiveEntityLinkerPanel.java
+++ src/main/java/nl/mpi/kinnate/ui/ArchiveEntityLinkerPanel.java
@@ -9,13 +9,13 @@ import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import nl.mpi.arbil.ImdiNodeSearchPanel;
-import nl.mpi.arbil.ImdiTable;
-import nl.mpi.arbil.ImdiTableModel;
-import nl.mpi.arbil.ImdiTree;
-import nl.mpi.arbil.LinorgSplitPanel;
-import nl.mpi.arbil.data.ImdiLoader;
-import nl.mpi.arbil.data.ImdiTreeObject;
+import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.data.ArbilDataNodeLoader;
+import nl.mpi.arbil.ui.ArbilNodeSearchPanel;
+import nl.mpi.arbil.ui.ArbilSplitPanel;
+import nl.mpi.arbil.ui.ArbilTable;
+import nl.mpi.arbil.ui.ArbilTableModel;
+import nl.mpi.arbil.ui.ArbilTree;
 
 /**
  *  Document   : ArchiveEntityLinkerPanel
@@ -25,9 +25,9 @@ import nl.mpi.arbil.data.ImdiTreeObject;
 public class ArchiveEntityLinkerPanel extends JPanel implements ActionListener {
 
     private JTabbedPane tabbedPane;
-    private ImdiTree archiveTree = new ImdiTree();
+    private ArbilTree archiveTree = new ArbilTree();
     private JButton nextButton;
-//    ImdiNodeSearchPanel archiveSearch = new ImdiNodeSearchPanel();
+//    ArbilNodeSearchPanel archiveSearch = new ArbilNodeSearchPanel();
 
     public ArchiveEntityLinkerPanel() {
         this.setLayout(new BorderLayout());
@@ -45,8 +45,8 @@ public class ArchiveEntityLinkerPanel extends JPanel implements ActionListener {
 
     private void loadTreeNodes() {
         try {
-            ImdiTreeObject imdiCorporaNode = ImdiLoader.getSingleInstance().getImdiObject(null, new URI("http://corpus1.mpi.nl/IMDI/metadata/IMDI.imdi"));
-            ImdiTreeObject[] allEntities = new ImdiTreeObject[]{imdiCorporaNode};
+            ArbilDataNode imdiCorporaNode = ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, new URI("http://corpus1.mpi.nl/IMDI/metadata/IMDI.imdi"));
+            ArbilDataNode[] allEntities = new ArbilDataNode[]{imdiCorporaNode};
             archiveTree.rootNodeChildren = allEntities;
             archiveTree.requestResort();
         } catch (URISyntaxException exception) {
@@ -58,12 +58,12 @@ public class ArchiveEntityLinkerPanel extends JPanel implements ActionListener {
     private void getSeachPanel() {
         JPanel searchPanel = new JPanel(new BorderLayout());
         String frameTitle = "Archive Search";
-        ImdiTableModel resultsTableModel = new ImdiTableModel();
-        ImdiTable imdiTable = new ImdiTable(resultsTableModel, frameTitle);
-        LinorgSplitPanel imdiSplitPanel = new LinorgSplitPanel(imdiTable);
+        ArbilTableModel resultsTableModel = new ArbilTableModel();
+        ArbilTable imdiTable = new ArbilTable(resultsTableModel, frameTitle);
+        ArbilSplitPanel imdiSplitPanel = new ArbilSplitPanel(imdiTable);
         // todo: take care of main window actions such as pack that might cause odd visuals
         JInternalFrame searchFrame = new JInternalFrame();
-        searchPanel.add(new ImdiNodeSearchPanel(searchFrame, resultsTableModel, archiveTree.getSelectedNodes()), BorderLayout.PAGE_START);
+        searchPanel.add(new ArbilNodeSearchPanel(searchFrame, resultsTableModel, archiveTree.getSelectedNodes()), BorderLayout.PAGE_START);
         searchPanel.add(imdiSplitPanel, BorderLayout.CENTER);
         imdiSplitPanel.setSplitDisplay();
 //        imdiSplitPanel.addFocusListener(searchFrame);
