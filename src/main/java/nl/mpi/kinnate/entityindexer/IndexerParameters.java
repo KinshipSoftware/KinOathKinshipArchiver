f8d5324e692a2c11213f235c49d1fea759e0ee3b
Peter Withers <Peter.Withers@mpi.nl>
2011-05-04 16:21:19 +0000

    Added a tree and drag handler to allow the creation of kin entities from archive actors and to add actors to kin entities.

--- src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
@@ -18,14 +18,15 @@ public class IndexerParameters {
     @XmlTransient
     public IndexerParam relevantLinkData = new IndexerParam(this, new String[][]{{"Type"}});
     @XmlElement(name = "LabelFields", namespace = "http://mpi.nl/tla/kin")
-    public IndexerParam labelFields = new IndexerParam(this, new String[][]{{"*:Kinnate/*:Gedcom/*:Entity/*:NAME/*:NAME"}, {"*:Kinnate/*:Gedcom/*:Entity[*:GedcomType='FAM']/*:GedcomType"}, {"*:Kinnate/*:Gedcom/*:Entity/*:Text"}, {"*:Kinnate/*:Gedcom/*:Entity/*:NAME/*:NPFX"}, {"*:Kinnate/*:Gedcom/*:Entity/*:NoteText"}});
+    public IndexerParam labelFields = new IndexerParam(this, new String[][]{{"*:Kinnate/*:Entity/*:Name"}, {"*:Kinnate/*:Gedcom/*:Entity/*:NAME/*:NAME"}, {"*:Kinnate/*:Gedcom/*:Entity[*:GedcomType='FAM']/*:GedcomType"}, {"*:Kinnate/*:Gedcom/*:Entity/*:Text"}, {"*:Kinnate/*:Gedcom/*:Entity/*:NAME/*:NPFX"}, {"*:Kinnate/*:Gedcom/*:Entity/*:NoteText"}});
     @XmlElement(name = "SymbolFieldsFields", namespace = "http://mpi.nl/tla/kin")
     public IndexerParam symbolFieldsFields = new IndexerParam(this, new String[][]{{"*:Kinnate/*:Gedcom/*:Entity[*:SEX='M']", "triangle"}, {"*:Kinnate/*:Gedcom/*:Entity[*:SEX='F']", "circle"}, {"*:Kinnate/*:Gedcom/*:Entity[*:GedcomType='FAM']", "union"}});
     @XmlElement(name = "AncestorFields", namespace = "http://mpi.nl/tla/kin")
-    public IndexerParam ancestorFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.FAMC"}, {"Kinnate.Gedcom.Entity.BIRT.FAMC"}, {"Kinnate.Gedcom.Entity.CHR.FAMC"}, {"Kinnate.Gedcom.Entity.ADOP.FAMC"}, {"Kinnate.Gedcom.Entity.SLGC.FAMC"}, {"Kinnate.Gedcom.Entity.HUSB"}, {"Kinnate.Gedcom.Entity.WIFE"}});
+    public IndexerParam ancestorFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.FAMC"}, {"Kinnate.Gedcom.Entity.BIRT.FAMC"}, {"Kinnate.Gedcom.Entity.CHR.FAMC"}, {"Kinnate.Gedcom.Entity.ADOP.FAMC"}, {"Kinnate.Gedcom.Entity.SLGC.FAMC"}, {"Kinnate.Gedcom.Entity.HUSB"}, {"Kinnate.Gedcom.Entity.WIFE"}, {"ancestor"}});
 //    public IndexerParam siblingFields = new IndexerParam(new String[]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}});
     @XmlElement(name = "DecendantFields", namespace = "http://mpi.nl/tla/kin")
-    public IndexerParam decendantFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}});
+    public IndexerParam decendantFields = new IndexerParam(this, new String[][]{{"Kinnate.Gedcom.Entity.CHIL"}, {"Kinnate.Gedcom.Entity.FAMS"}, {"descendant"}});
+//    sibling, , union, none
     //    public IndexerParam showEntityFields = new IndexerParam(new String[][]{{"Kinnate/Gedcom/Entity/GedcomType=INDI"}, {"Kinnate/Gedcom/Entity/GedcomType=FAM"}}); // todo: add fields that can be used to controll which nodes are shown
 //    @Deprecated // I think this is no longer used or needed
 //    private String[][] relevantEntityData = null;
