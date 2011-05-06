0f55175384427f31ed661a0f22ae3aeb40b2a0e7
Peter Withers <Peter.Withers@mpi.nl>
2011-05-06 17:44:10 +0000

    Added import menu for gedcom samples.

---
+++ src/main/java/nl/mpi/kinnate/ui/ImportSamplesFileMenu.java
@@ -0,0 +1,37 @@
+package nl.mpi.kinnate.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+/**
+ *  Document   : RecentFileMenu
+ *  Created on : Apr 15, 2011, 11:30:39 AM
+ *  Author     : Peter Withers
+ */
+public class ImportSamplesFileMenu extends JMenu implements ActionListener {
+
+    MainFrame mainFrame;
+
+    public ImportSamplesFileMenu(MainFrame mainFrameLocal) {
+        mainFrame = mainFrameLocal;
+
+        addSampleToMenu("Hawaiian Kin Terms", "HawaiianKinTerms.svg");
+        addSampleToMenu("Japanese Kin Terms", "JapaneseKinTerms.svg");
+        addSampleToMenu("Cha'palaa Kin Terms", "ChapalaaKinTerms.svg");
+    }
+
+    private void addSampleToMenu(String menuText, String sampleFileString) {
+        String currentFilePath = ImportSamplesFileMenu.class.getResource("../../../../svgsamples/" + sampleFileString).getPath();
+        JMenuItem currentMenuItem = new JMenuItem(menuText);
+        currentMenuItem.setActionCommand(currentFilePath);
+        currentMenuItem.addActionListener(this);
+        this.add(currentMenuItem);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        mainFrame.openDiagram(new File(e.getActionCommand()));
+    }
+}
