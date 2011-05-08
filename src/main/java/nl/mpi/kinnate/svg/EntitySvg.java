ac7a13fb3ddc93f4b44ad52fe1ca9d46e2a05d93
Peter Withers <Peter.Withers@mpi.nl>
2011-05-08 08:17:34 +0000

    Modified the graph drawing process so that the locations of each entity are kept, including when an existing svg file is loaded.

--- src/main/java/nl/mpi/kinnate/svg/EntitySvg.java
+++ src/main/java/nl/mpi/kinnate/svg/EntitySvg.java
@@ -1,9 +1,14 @@
 package nl.mpi.kinnate.svg;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import nl.mpi.kinnate.kindata.EntityData;
 import org.w3c.dom.svg.SVGDocument;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.Text;
+import org.w3c.dom.events.EventTarget;
 import org.w3c.dom.svg.SVGLocatable;
 import org.w3c.dom.svg.SVGMatrix;
 import org.w3c.dom.svg.SVGRect;
@@ -15,9 +20,21 @@ import org.w3c.dom.svg.SVGRect;
  */
 public class EntitySvg {
 
+    protected HashMap<String, Float[]> entityPositions;
+    private int symbolSize = 15;
+    static protected int strokeWidth = 2;
+
+    public EntitySvg() {
+        entityPositions = new HashMap<String, Float[]>();
+    }
+
+    public void readEntityPositions(Node entityGroup) {
+        // todo: read the entity positions from the existing dom
+        // todo: only the x pos needs to be sotred because the y pos is determined by the generations
+//        entityPositions
+    }
+
     public void insertSymbols(SVGDocument doc, String svgNameSpace) {
-        int symbolSize = 15;
-        int strokeWidth = 2;
         Element svgRoot = doc.getDocumentElement();
         Element defsNode = doc.createElementNS(svgNameSpace, "defs");
         defsNode.setAttribute("id", "KinSymbols");
@@ -166,31 +183,37 @@ public class EntitySvg {
         return symbolArray.toArray(new String[]{});
     }
 
-    public float[] getEntityLocation(SVGDocument doc, String entityId) {
-        Element entitySymbol = doc.getElementById(entityId + "symbol");
-//        Element entitySymbol = doc.getElementById(entityId); // the sybol group node
-        if (entitySymbol != null) {
-            SVGRect bbox = ((SVGLocatable) entitySymbol).getBBox();
-            SVGMatrix sVGMatrix = ((SVGLocatable) entitySymbol).getCTM();
-//            System.out.println("getA: " + sVGMatrix.getA());
-//            System.out.println("getB: " + sVGMatrix.getB());
-//            System.out.println("getC: " + sVGMatrix.getC());
-//            System.out.println("getD: " + sVGMatrix.getD());
-//            System.out.println("getE: " + sVGMatrix.getE());
-//            System.out.println("getF: " + sVGMatrix.getF());
-
-//            System.out.println("bbox X: " + bbox.getX());
-//            System.out.println("bbox Y: " + bbox.getY());
-//            System.out.println("bbox W: " + bbox.getWidth());
-//            System.out.println("bbox H: " + bbox.getHeight());
-            return new float[]{sVGMatrix.getE() + bbox.getWidth() / 2, sVGMatrix.getF() + bbox.getHeight() / 2};
-//            bbox.setX(sVGMatrix.getE());
-//            bbox.setY(sVGMatrix.getF());
-//            return bbox;
-        } else {
-            return null;
-        }
+    public Float[] getEntityLocation(String entityId) {
+        Float[] returnLoc = entityPositions.get(entityId);
+        Float xPos = returnLoc[0] + (symbolSize / 2);
+        Float yPos = returnLoc[1] + (symbolSize / 2);
+        return new Float[]{xPos, yPos};
     }
+//    public float[] getEntityLocation(SVGDocument doc, String entityId) {
+//        Element entitySymbol = doc.getElementById(entityId + "symbol");
+////        Element entitySymbol = doc.getElementById(entityId); // the sybol group node
+//        if (entitySymbol != null) {
+//            SVGRect bbox = ((SVGLocatable) entitySymbol).getBBox();
+//            SVGMatrix sVGMatrix = ((SVGLocatable) entitySymbol).getCTM();
+////            System.out.println("getA: " + sVGMatrix.getA());
+////            System.out.println("getB: " + sVGMatrix.getB());
+////            System.out.println("getC: " + sVGMatrix.getC());
+////            System.out.println("getD: " + sVGMatrix.getD());
+////            System.out.println("getE: " + sVGMatrix.getE());
+////            System.out.println("getF: " + sVGMatrix.getF());
+//
+////            System.out.println("bbox X: " + bbox.getX());
+////            System.out.println("bbox Y: " + bbox.getY());
+////            System.out.println("bbox W: " + bbox.getWidth());
+////            System.out.println("bbox H: " + bbox.getHeight());
+//            return new float[]{sVGMatrix.getE() + bbox.getWidth() / 2, sVGMatrix.getF() + bbox.getHeight() / 2};
+////            bbox.setX(sVGMatrix.getE());
+////            bbox.setY(sVGMatrix.getF());
+////            return bbox;
+//        } else {
+//            return null;
+//        }
+//    }
 
     public float[] moveEntity(SVGDocument doc, String entityId, float shiftX, float shiftY, boolean snapToGrid) {
         Element entitySymbol = doc.getElementById(entityId);
@@ -224,10 +247,104 @@ public class EntitySvg {
                 }
             }
 //            System.out.println("updatedPosition after snap: " + updatedPosition);
+            entityPositions.put(entityId, new Float[]{updatedPositionX, updatedPositionY});
             ((Element) entitySymbol).setAttribute("transform", "translate(" + String.valueOf(updatedPositionX) + ", " + String.valueOf(updatedPositionY) + ")");
 //            ((Element) entitySymbol).setAttribute("transform", "translate(" + String.valueOf(sVGMatrix.getE() + shiftX) + ", " + (sVGMatrix.getF() + shiftY) + ")");
         }
 //        System.out.println("remainderAfterSnap: " + remainderAfterSnap);
         return new float[]{remainderAfterSnapX, remainderAfterSnapY};
+    }
+
+    public Element createEntitySymbol(GraphPanel graphPanel, EntityData currentNode, int hSpacing, int vSpacing) {
+        Element groupNode = graphPanel.doc.createElementNS(graphPanel.svgNameSpace, "g");
+        groupNode.setAttribute("id", currentNode.getUniqueIdentifier());
+        groupNode.setAttributeNS(DataStoreSvg.kinDataNameSpaceLocation, "kin:path", currentNode.getEntityPath());
+        // the kin type strings are stored here so that on selection in the graph the add kin term panel can be pre populatedwith the kin type strings of the selection
+        groupNode.setAttributeNS(DataStoreSvg.kinDataNameSpaceLocation, "kin:kintype", currentNode.getKinTypeString());
+//        counterTest++;
+        Element symbolNode;
+        String symbolType = currentNode.getSymbolType();
+        if (symbolType == null || symbolType.length() == 0) {
+            symbolType = "square";
+        }
+        // todo: check that if an entity is already placed in which case do not recreate
+        // todo: do not create a new dom each time but reuse it instead, or due to the need to keep things up to date maybe just store an array of entity locations instead
+        symbolNode = graphPanel.doc.createElementNS(graphPanel.svgNameSpace, "use");
+        symbolNode.setAttribute("id", currentNode.getUniqueIdentifier() + "symbol");
+        symbolNode.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#" + symbolType); // the xlink: of "xlink:href" is required for some svg viewers to render correctly
+        Float[] storedPosition = entityPositions.get(currentNode.getUniqueIdentifier());
+        if (storedPosition == null) {
+            storedPosition = new Float[]{currentNode.getxPos() * hSpacing + hSpacing - symbolSize / 2.0f,
+                        currentNode.getyPos() * vSpacing + vSpacing - symbolSize / 2.0f};
+            entityPositions.put(currentNode.getUniqueIdentifier(), storedPosition);
+        }
+        groupNode.setAttribute("transform", "translate(" + Float.toString(storedPosition[0]) + ", " + Float.toString(storedPosition[1]) + ")");
+        if (currentNode.isEgo) {
+            symbolNode.setAttribute("fill", "black");
+        } else {
+            symbolNode.setAttribute("fill", "white");
+        }
+
+        symbolNode.setAttribute("stroke", "black");
+        symbolNode.setAttribute("stroke-width", "2");
+        groupNode.appendChild(symbolNode);
+
+////////////////////////////// tspan method appears to fail in batik rendering process unless saved and reloaded ////////////////////////////////////////////////
+//            Element labelText = doc.createElementNS(svgNS, "text");
+////            labelText.setAttribute("x", Integer.toString(currentNode.xPos * hSpacing + hSpacing + symbolSize / 2));
+////            labelText.setAttribute("y", Integer.toString(currentNode.yPos * vSpacing + vSpacing - symbolSize / 2));
+//            labelText.setAttribute("fill", "black");
+//            labelText.setAttribute("fill-opacity", "1");
+//            labelText.setAttribute("stroke-width", "0");
+//            labelText.setAttribute("font-size", "14px");
+////            labelText.setAttribute("text-anchor", "end");
+////            labelText.setAttribute("style", "font-size:14px;text-anchor:end;fill:black;fill-opacity:1");
+//            //labelText.setNodeValue(currentChild.toString());
+//
+//            //String textWithUni = "\u0041";
+//            int textSpanCounter = 0;
+//            int lineSpacing = 10;
+//            for (String currentTextLable : currentNode.getLabel()) {
+//                Text textNode = doc.createTextNode(currentTextLable);
+//                Element tspanElement = doc.createElement("tspan");
+//                tspanElement.setAttribute("x", Integer.toString(currentNode.xPos * hSpacing + hSpacing + symbolSize / 2));
+//                tspanElement.setAttribute("y", Integer.toString((currentNode.yPos * vSpacing + vSpacing - symbolSize / 2) + textSpanCounter));
+////                tspanElement.setAttribute("y", Integer.toString(textSpanCounter * lineSpacing));
+//                tspanElement.appendChild(textNode);
+//                labelText.appendChild(tspanElement);
+//                textSpanCounter += lineSpacing;
+//            }
+//            groupNode.appendChild(labelText);
+////////////////////////////// end tspan method appears to fail in batik rendering process ////////////////////////////////////////////////
+
+////////////////////////////// alternate method ////////////////////////////////////////////////
+        ArrayList<String> labelList = new ArrayList<String>();
+        if (graphPanel.dataStoreSvg.showLabels) {
+            labelList.addAll(Arrays.asList(currentNode.getLabel()));
+        }
+        if (graphPanel.dataStoreSvg.showKinTypeLabels) {
+            labelList.addAll(Arrays.asList(currentNode.getKinTypeStringArray()));
+        }
+        if (graphPanel.dataStoreSvg.showKinTermLabels) {
+            labelList.addAll(Arrays.asList(currentNode.getKinTermStrings()));
+        }
+        // todo: this method has the draw back that the text is not selectable as a block
+        int textSpanCounter = 0;
+        int lineSpacing = 15;
+        for (String currentTextLable : labelList) {
+            Element labelText = graphPanel.doc.createElementNS(graphPanel.svgNameSpace, "text");
+            labelText.setAttribute("x", Double.toString(symbolSize * 1.5));
+            labelText.setAttribute("y", Integer.toString(textSpanCounter));
+            labelText.setAttribute("fill", "black");
+            labelText.setAttribute("stroke-width", "0");
+            labelText.setAttribute("font-size", "14");
+            Text textNode = graphPanel.doc.createTextNode(currentTextLable);
+            labelText.appendChild(textNode);
+            textSpanCounter += lineSpacing;
+            groupNode.appendChild(labelText);
+        }
+////////////////////////////// end alternate method ////////////////////////////////////////////////
+        ((EventTarget) groupNode).addEventListener("mousedown", new MouseListenerSvg(graphPanel), false);
+        return groupNode;
     }
 }
