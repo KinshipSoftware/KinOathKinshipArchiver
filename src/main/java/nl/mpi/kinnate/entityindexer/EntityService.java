ea4798d2998299c91f0b826a8770040f51294e0a
Peter Withers <Peter.Withers@mpi.nl>
2011-05-25 13:59:16 +0000

    Disabled the use existing entities from an svg file when their relations might not exist, so the existing entities to be used on the graph but not for database actions.

--- src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
+++ src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
@@ -12,5 +12,7 @@ import nl.mpi.kinnate.kindata.EntityData;
  */
 public interface EntityService {
 
+//    public void primeWithEntities(EntityData[] preLoadedEntities); // it would seem not to be a good idea to try and use existing entities from an svg file when their relations might not exist, so we will allow the existing entities to be used on the graph but not for database actions
+
     public EntityData[] getRelationsOfEgo(URI[] egoNodes, HashSet<String> egoIdentifiers, HashSet<String> requiredEntityIdentifiers, String[] kinTypeStrings, ParserHighlight[] parserHighlight, IndexerParameters indexParameters) throws EntityServiceException;
 }
