2d29c631df97e27d5d690ad239ae77d437e8457a
Peter Withers <Peter.Withers@mpi.nl>
2011-05-30 14:21:21 +0000

    Added syntax highlighting to the kintype strings when read into transient nodes.

---
+++ src/main/java/nl/mpi/kinnate/kintypestrings/ParserHighlight.java
@@ -0,0 +1,25 @@
+package nl.mpi.kinnate.kintypestrings;
+
+/**
+ *  Document   : ParserHighlight
+ *  Created on : May 30, 2011, 3:06:02 PM
+ *  Author     : Peter Withers
+ */
+public class ParserHighlight {
+
+    public enum ParserHighlightType {
+
+        KinType, Comment, Error, Query, Unknown
+    }
+    public ParserHighlight nextHighlight = null;
+    public ParserHighlightType highlight;
+    public int startChar = 0;
+
+    public ParserHighlight addHighlight(ParserHighlightType highlightType, int startChar) {
+
+        this.highlight = highlightType;
+        this.startChar = startChar;
+        this.nextHighlight = new ParserHighlight();
+        return this.nextHighlight;
+    }
+}
