c00857128ab614ff83d4d589166e87e853511ef3
Peter Withers <Peter.Withers@mpi.nl>
2011-05-30 10:11:00 +0000

    Separated the Gedcom import into an interface and super class so that other importers can easily be written.

---
+++ src/main/java/nl/mpi/kinnate/gedcomimport/EntityImporter.java
@@ -0,0 +1,95 @@
+package nl.mpi.kinnate.gedcomimport;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JProgressBar;
+import javax.swing.JTextArea;
+import nl.mpi.arbil.util.ArbilBugCatcher;
+
+/**
+ *  Document   : EntityImporter
+ *  Created on : May 30, 2011, 10:28:59 AM
+ *  Author     : Peter Withers
+ */
+public class EntityImporter implements GenericImporter {
+
+    protected JProgressBar progressBar = null;
+    protected int inputLineCount;
+    protected String inputFileMd5Sum;
+    protected boolean overwriteExisting;
+    protected HashMap<String, ArrayList<String>> createdNodeIds;
+
+    public EntityImporter(boolean overwriteExistingLocal) {
+        overwriteExisting = overwriteExistingLocal;
+    }
+
+    public HashMap<String, ArrayList<String>> getCreatedNodeIds() {
+        return createdNodeIds;
+    }
+
+    public void appendToTaskOutput(JTextArea importTextArea, String lineOfText) {
+        importTextArea.append(lineOfText + "\n");
+        importTextArea.setCaretPosition(importTextArea.getText().length());
+    }
+
+    public void setProgressBar(JProgressBar progressBarLocal) {
+        progressBar = progressBarLocal;
+    }
+
+    public void calculateFileNameAndFileLength(BufferedReader bufferedReader) {
+        // count the lines in the file (for progress) and calculate the md5 sum (for unique file naming)
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            StringBuilder hexString = new StringBuilder();
+            String strLine;
+            inputLineCount = 0;
+            while ((strLine = bufferedReader.readLine()) != null) {
+                digest.update(strLine.getBytes());
+                inputLineCount++;
+            }
+            byte[] md5sum = digest.digest();
+            for (int byteCounter = 0; byteCounter < md5sum.length; ++byteCounter) {
+                hexString.append(Integer.toHexString(0x0100 + (md5sum[byteCounter] & 0x00FF)).substring(1));
+            }
+            inputFileMd5Sum = hexString.toString();
+        } catch (NoSuchAlgorithmException algorithmException) {
+            new ArbilBugCatcher().logError(algorithmException);
+        } catch (IOException iOException) {
+            new ArbilBugCatcher().logError(iOException);
+        }
+    }
+
+    public String cleanFileName(String fileName) {
+        // prevent bad file names being created from the gedcom internal name part
+        return fileName.replaceAll("[^A-z0-9]", "_") + ".cmdi";
+    }
+
+    public URI[] importTestFile(JTextArea importTextArea, File testFile) {
+        try {
+            calculateFileNameAndFileLength(new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(testFile)))));
+            return importTestFile(importTextArea, new InputStreamReader(new DataInputStream(new FileInputStream(testFile))));
+        } catch (FileNotFoundException exception) {
+            // todo: handle this
+            return null;
+        }
+    }
+
+    public URI[] importTestFile(JTextArea importTextArea, String testFileString) {
+        calculateFileNameAndFileLength(new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(testFileString))));
+        return importTestFile(importTextArea, new InputStreamReader(getClass().getResourceAsStream(testFileString)));
+    }
+
+    public URI[] importTestFile(JTextArea importTextArea, InputStreamReader inputStreamReader) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+}
