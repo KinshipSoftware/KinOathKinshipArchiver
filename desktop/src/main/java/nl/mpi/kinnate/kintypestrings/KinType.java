443b3c909d577c206da377ef49694cad03897ffb
Peter Withers <Peter.Withers@mpi.nl>
2011-06-28 09:47:50 +0000

    Refactored the kin type string classes. Added non ego kin types to be used to start a kin type strings but cannot be used except at the beginning. Allowed comments on lines even after an error in the kin type strings. Prevented relation types being added to kin type string start kin types either for ego or non ego types. Updated the kin type string parsing to look for any existing relaitons that match the required kin type rather than always creating new entities. Updated the insert relation to also insert reverse relations and sibling parents when constructing from kin type strings. Added an export csv output for the pedigree R module.

---
+++ desktop/src/main/java/nl/mpi/kinnate/kintypestrings/KinType.java
@@ -0,0 +1,82 @@
+package nl.mpi.kinnate.kintypestrings;
+
+import nl.mpi.kinnate.kindata.DataTypes;
+import nl.mpi.kinnate.kindata.EntityData;
+import nl.mpi.kinnate.kindata.EntityRelation;
+
+/**
+ *  Document   : KinType
+ *  Created on : Jun 28, 2011, 11:31:35 AM
+ *  Author     : Peter Withers
+ */
+public class KinType {
+
+    private KinType(String codeStringLocal, DataTypes.RelationType relationTypeLocal, EntityData.SymbolType symbolTypeLocal) {
+        codeString = codeStringLocal;
+        relationType = relationTypeLocal;
+        symbolType = symbolTypeLocal;
+    }
+    protected String codeString;
+    protected DataTypes.RelationType relationType;
+    protected EntityData.SymbolType symbolType;
+
+    public String getCodeString() {
+        return codeString;
+    }
+
+    public boolean isEgoType() {
+        // todo: this could be better handled by adding a boolean: isego to each KinType
+        return codeString.contains("E");
+    }
+
+    protected boolean matchesRelation(EntityRelation entityRelation) {
+        if (entityRelation.getAlterNode().isEgo != this.isEgoType()) {
+            return false;
+        }
+        if (!relationType.equals(entityRelation.relationType)) {
+            return false;
+        }
+        if (!symbolType.name().equals(entityRelation.getAlterNode().getSymbolType())) {
+            return false;
+        }
+        return true;
+    }
+    protected static KinType[] referenceKinTypes = new KinType[]{
+        // other types
+        // todo: the gendered ego kin types Em and Ef are probably not correct and should be verified
+        new KinType("Ef", DataTypes.RelationType.none, EntityData.SymbolType.circle),
+        new KinType("Em", DataTypes.RelationType.none, EntityData.SymbolType.triangle),
+        // type 1
+        new KinType("Fa", DataTypes.RelationType.ancestor, EntityData.SymbolType.triangle),
+        new KinType("Mo", DataTypes.RelationType.ancestor, EntityData.SymbolType.circle),
+        new KinType("Br", DataTypes.RelationType.sibling, EntityData.SymbolType.triangle),
+        new KinType("Si", DataTypes.RelationType.sibling, EntityData.SymbolType.circle),
+        new KinType("So", DataTypes.RelationType.descendant, EntityData.SymbolType.triangle),
+        new KinType("Da", DataTypes.RelationType.descendant, EntityData.SymbolType.circle),
+        new KinType("Hu", DataTypes.RelationType.union, EntityData.SymbolType.triangle),
+        new KinType("Wi", DataTypes.RelationType.union, EntityData.SymbolType.circle),
+        new KinType("Pa", DataTypes.RelationType.ancestor, EntityData.SymbolType.square),
+        new KinType("Sb", DataTypes.RelationType.sibling, EntityData.SymbolType.square), //todo: are Sp and Sb correct?
+        new KinType("Sp", DataTypes.RelationType.sibling, EntityData.SymbolType.square),
+        new KinType("Ch", DataTypes.RelationType.descendant, EntityData.SymbolType.square),
+        // type 2
+        new KinType("F", DataTypes.RelationType.ancestor, EntityData.SymbolType.triangle),
+        new KinType("M", DataTypes.RelationType.ancestor, EntityData.SymbolType.circle),
+        new KinType("B", DataTypes.RelationType.sibling, EntityData.SymbolType.triangle),
+        new KinType("Z", DataTypes.RelationType.sibling, EntityData.SymbolType.circle),
+        new KinType("S", DataTypes.RelationType.descendant, EntityData.SymbolType.triangle),
+        new KinType("D", DataTypes.RelationType.descendant, EntityData.SymbolType.circle),
+        new KinType("H", DataTypes.RelationType.union, EntityData.SymbolType.triangle),
+        new KinType("W", DataTypes.RelationType.union, EntityData.SymbolType.circle),
+        new KinType("P", DataTypes.RelationType.ancestor, EntityData.SymbolType.square),
+        new KinType("G", DataTypes.RelationType.sibling, EntityData.SymbolType.square),
+        new KinType("E", DataTypes.RelationType.none, EntityData.SymbolType.square),
+        new KinType("C", DataTypes.RelationType.descendant, EntityData.SymbolType.square),
+        //        new KinType("X", DataTypes.RelationType.none, EntityData.SymbolType.none) // X is intended to indicate unknown or no type, for instance this is used after import to add all nodes to the graph
+
+        // non ego types to be used to start a kin type string but cannot be used except at the beginning
+        new KinType("m", DataTypes.RelationType.none, EntityData.SymbolType.triangle),
+        new KinType("f", DataTypes.RelationType.none, EntityData.SymbolType.circle),
+        new KinType("x", DataTypes.RelationType.none, EntityData.SymbolType.square)
+    };
+}
