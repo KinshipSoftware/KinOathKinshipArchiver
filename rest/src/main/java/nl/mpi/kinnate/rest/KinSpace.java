0cb2488824d2cca8a8e79ab403ccd3f13e7b2658
Peter Withers <Peter.Withers@mpi.nl>
2011-07-01 12:33:55 +0000

    Added rest service to receive the uploaded entities. Added a search for and a rest upload of new and modified entities. Added a UI for upload of local entities to the entity server.

--- rest/src/main/java/nl/mpi/kinnate/rest/KinSpace.java
+++ rest/src/main/java/nl/mpi/kinnate/rest/KinSpace.java
@@ -1,15 +1,13 @@
 package nl.mpi.kinnate.rest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import nl.mpi.kinnate.kinhive.HiveException;
 import nl.mpi.kinnate.kinhive.HiveManager;
 
@@ -21,21 +19,28 @@ import nl.mpi.kinnate.kinhive.HiveManager;
 @Path("/kinspace")
 public class KinSpace {
 
+    String listWorkSpacesHref = "<br><a href=/kinoath-rest/kinoath/kinspace/>list workspaces</a>";
+
     @GET
     @Produces("text/html")
-    @Path("")
+//    @Path("")
     public String listWorkSpaces() {
         StringBuilder resultStringBuilder = new StringBuilder();
         resultStringBuilder.append("<b>Available Workspaces</b><br><p>");
-        for (String currentWorkspace : new HiveManager().listWorkspaces()) {
-            resultStringBuilder.append("<a href=/kinoath-rest/kinoath/kinspace/");
-            resultStringBuilder.append(currentWorkspace);
-            resultStringBuilder.append(">");
-            resultStringBuilder.append(currentWorkspace);
-            resultStringBuilder.append("</a>");
-            resultStringBuilder.append("<br>");
+        String[] workspaceDirs = new HiveManager().listWorkspaces();
+        if (workspaceDirs == null) {
+            resultStringBuilder.append("No workspace directory found");
+        } else {
+            for (String currentWorkspace : workspaceDirs) {
+                resultStringBuilder.append("<a href=/kinoath-rest/kinoath/kinspace/");
+                resultStringBuilder.append(currentWorkspace);
+                resultStringBuilder.append(">");
+                resultStringBuilder.append(currentWorkspace);
+                resultStringBuilder.append("</a>");
+                resultStringBuilder.append("<br>");
+            }
+            resultStringBuilder.append("</p><br><a href=/kinoath-rest/kinoath/kinspace/sampleworkspace/create>create a sample workspace (change url to change the workspace name)</a>");
         }
-        resultStringBuilder.append("</p><br><a href=/kinoath-rest/kinoath/kinspace/sampleworkspace/create>create a sample workspace (change url to change the workspace name)</a>");
         return resultStringBuilder.toString();
     }
 
@@ -45,9 +50,9 @@ public class KinSpace {
     public String createWorkspace(@PathParam("workspaceName") String workspaceName) {
         try {
             new HiveManager().createWorkspace(workspaceName);
-            return "created workspace<br><a href=/kinoath-rest/kinoath/kinspace/>" + workspaceName + "</a>";
+            return "created workspace<br><a href=/kinoath-rest/kinoath/kinspace/>" + workspaceName + "</a>" + listWorkSpacesHref;
         } catch (HiveException exception) {
-            return exception.getMessage();
+            return exception.getMessage() + listWorkSpacesHref;
         }
 
     }
@@ -60,16 +65,19 @@ public class KinSpace {
         resultStringBuilder.append("<b>Workspace: ");
         resultStringBuilder.append(workspaceName);
         resultStringBuilder.append("</b><br><p>");
-        boolean filesFound = false;
-        for (String currentWorkspaceFile : new HiveManager().listWorkspaceFiles(workspaceName)) {
-            resultStringBuilder.append(currentWorkspaceFile);
-            resultStringBuilder.append("<br>");
-            filesFound = true;
-        }
-        if (!filesFound) {
+        String[] workspaceFiles = new HiveManager().listWorkspaceFiles(workspaceName);
+        if (workspaceFiles == null) {
+            resultStringBuilder.append("Workspace not found");
+        } else if (workspaceFiles.length < 1) {
             resultStringBuilder.append("no files found<br>");
+        } else {
+            for (String currentWorkspaceFile : workspaceFiles) {
+                resultStringBuilder.append(currentWorkspaceFile);
+                resultStringBuilder.append("<br>");
+            }
         }
-        resultStringBuilder.append("</p><br><a href=/kinoath-rest/kinoath/kinspace/>list workspaces</a>");
+        resultStringBuilder.append("</p>");
+        resultStringBuilder.append(listWorkSpacesHref);
         return resultStringBuilder.toString();
     }
 
@@ -77,17 +85,11 @@ public class KinSpace {
     @PUT
     @Consumes("text/xml")
     public String putXml(@PathParam("workspaceName") String workspaceName, InputStream entityXmlStream) {
-        StringBuilder putContents = new StringBuilder();
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(entityXmlStream, "UTF-8"));
-            for (String responseLine = bufferedReader.readLine(); responseLine != null; responseLine = bufferedReader.readLine()) {
-                putContents.append(responseLine);
-                putContents.append("\n");
-            }
-            putContents.append("\n");
-        } catch (IOException exception) {
-            return exception.getMessage();
+            String kinHivePid = new HiveManager().addToWorkspace(workspaceName, entityXmlStream);
+            return "Workspace: " + workspaceName + " Received: " + kinHivePid;
+        } catch (HiveException exception) {
+            throw new WebApplicationException(404);
         }
-        return "Workspace: " + workspaceName + " Received: " + putContents;
     }
 }
