b2ef6933680edb36d459d4fc63fd80f6820c3761
Peter Withers <Peter.Withers@mpi.nl>
2011-07-01 12:58:52 +0000

    Added rest service to receive the uploaded entities. Added a search for and a rest upload of new and modified entities. Added a UI for upload of local entities to the entity server.

---
+++ desktop/src/main/java/nl/mpi/kinnate/export/ExportException.java
@@ -0,0 +1,13 @@
+package nl.mpi.kinnate.export;
+
+/**
+ *  Document   : ExportException
+ *  Created on : Jul 1, 2011, 2:44:17 PM
+ *  Author     : Peter Withers
+ */
+public class ExportException extends Exception {
+
+    public ExportException(String messageString) {
+        super(messageString);
+    }
+}
