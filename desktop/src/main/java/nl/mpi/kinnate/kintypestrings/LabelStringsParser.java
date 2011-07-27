ec971d6f0c9a3bf914d17f0e66be9be4ae443aef
Peter Withers <Peter.Withers@mpi.nl>
2011-07-27 08:18:28 +0000

    Modified the unique identifier that transient entities use to be based on an md5 sum of the users input so that they have the same identifier on each redraw and on loading a saved document, otherwise the entity positions on the graph get lost. Simplified the unique identifier and its xml form. Modified the import process to insert back references for each entity relation so that the xml queries can take 1/10th of the time. Added options to highlight or not the relation lines of selected entities and to hide or show the diagram border. Replaced the string based unique identifier with the unique identifier object. Added a common unique identifier used by entities and graphics elements alike.

--- desktop/src/main/java/nl/mpi/kinnate/kintypestrings/LabelStringsParser.java
+++ desktop/src/main/java/nl/mpi/kinnate/kintypestrings/LabelStringsParser.java
@@ -2,6 +2,7 @@ package nl.mpi.kinnate.kintypestrings;
 
 import java.util.Date;
 import nl.mpi.kinnate.kindata.EntityData;
+import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 
 /**
  *  Document   : LabelStringsParser
@@ -10,11 +11,12 @@ import nl.mpi.kinnate.kindata.EntityData;
  */
 public class LabelStringsParser {
 
-    String userDefinedIdentifier = null;
-    String labelsStrings[] = new String[]{};
-    Date dateOfBirth = null; // todo: read in the dates and if found set the in the entities
-    Date dateOfDeath = null;
-    String remainingInputString;
+    protected boolean userDefinedIdentifierFound = false;
+    public UniqueIdentifier uniqueIdentifier;
+    public String labelsStrings[] = new String[]{};
+    public Date dateOfBirth = null; // todo: read in the dates and if found set the in the entities
+    public Date dateOfDeath = null;
+    protected String remainingInputString;
 
     protected LabelStringsParser(String inputString, EntityData parentData, String currentKinTypeString) {
         if (inputString.startsWith(":")) {
@@ -23,16 +25,28 @@ public class LabelStringsParser {
                 // todo: document and give examples of how these user defined identifiers work and that if the #0-9 type is not provided then the entire label string is used as the identifier
                 labelsStrings = inputStringParts[1].split(";");
                 if (labelsStrings[0].matches("^#[0-9]*")) {
-                    userDefinedIdentifier = "id:" + labelsStrings[0];
+                    // use the user defined number to identify the entity
+                    uniqueIdentifier = new UniqueIdentifier("id:" + labelsStrings[0], UniqueIdentifier.IdentifierType.tid);
+                    userDefinedIdentifierFound = true;
                 }
-                if (userDefinedIdentifier == null) {
-                    userDefinedIdentifier = "label:" + inputStringParts[1];
+                if (uniqueIdentifier == null) {
+                    // use the entire label sting to identify the entity
+                    uniqueIdentifier = new UniqueIdentifier("label:" + inputStringParts[1], UniqueIdentifier.IdentifierType.tid);
+                    userDefinedIdentifierFound = true;
                 }
             }
             if (inputStringParts.length > 2) {
                 remainingInputString = inputStringParts[2];
             } else {
                 remainingInputString = "";
+            }
+        }
+        if (uniqueIdentifier == null) {
+            if (parentData != null) {
+                // the parent id is used here to differentiate for instance M in the following two cases EmM EmFM, where if only M were used these two strings would get the same entity and be incorrect
+                uniqueIdentifier = new UniqueIdentifier(parentData.getUniqueIdentifier().getAttributeIdentifier() + currentKinTypeString, UniqueIdentifier.IdentifierType.tid);
+            } else {
+                uniqueIdentifier = new UniqueIdentifier("label:" + "kintype:" + currentKinTypeString, UniqueIdentifier.IdentifierType.tid);
             }
         }
     }
