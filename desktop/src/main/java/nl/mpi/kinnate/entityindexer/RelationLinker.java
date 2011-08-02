64f8038934e09a17b5fe934907314f05468e355f
Peter Withers <Peter.Withers@mpi.nl>
2011-08-02 15:28:22 +0000

    Updated the relation linking process (triggered via the context menu) to use jaxb and the entity data object in the same way the csv import now does. Updated the namespace for the kin xml data. Modified the csv and gedcom import to use the entitydata and jaxb to create the xml adding any extra metadata later in the file, so that entity relations are always created in the same way. Modified the unique identifier that transient entities use to be based on an md5 sum of the users input so that they have the same identifier on each redraw and on loading a saved document, otherwise the entity positions on the graph get lost. Simplified the unique identifier and its xml form. Modified the import process to insert back references for each entity relation so that the xml queries can take 1/10th of the time. Added options to highlight or not the relation lines of selected entities and to hide or show the diagram border. Replaced the string based unique identifier with the unique identifier object. Added a common unique identifier used by entities and graphics elements alike.

--- desktop/src/main/java/nl/mpi/kinnate/entityindexer/RelationLinker.java
+++ desktop/src/main/java/nl/mpi/kinnate/entityindexer/RelationLinker.java
@@ -4,14 +4,23 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.parsers.ParserConfigurationException;
 import nl.mpi.arbil.data.ArbilComponentBuilder;
 import nl.mpi.arbil.util.ArbilBugCatcher;
 import nl.mpi.kinnate.kindata.DataTypes;
+import nl.mpi.kinnate.kindata.EntityData;
+import nl.mpi.kinnate.kindata.RelationArray;
+import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 import nl.mpi.kinnate.svg.GraphPanel;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -21,49 +30,62 @@ import org.xml.sax.SAXException;
  */
 public class RelationLinker {
 
-    public void linkEntities(GraphPanel graphPanel, String[] selectedIdentifiers, DataTypes.RelationType relationType) {
-        String targetPath = graphPanel.getPathForElementId(selectedIdentifiers[0]);
-        try {
-            // todo: any changes to the ArbilDateNode should be saved here, although this should not be an issue because nodes are only appended to the end
-            URI targetUri = new URI(targetPath);
-            Document metadataDom = ArbilComponentBuilder.getDocument(targetUri);
-            for (int relationCounter = 1 /* skip the first because that is the target */; relationCounter < selectedIdentifiers.length; relationCounter++) {
-                Element relationElement = metadataDom.createElement("Relation");
-                metadataDom.getDocumentElement().appendChild(relationElement);
+    private Node getEntityNode(Document entityDocument) {
+        //org.apache.xpath.XPathAPI.selectSingleNode(metadataDom.getDocumentElement(), "//:Entity/:Relations");
+        Element roodNode = entityDocument.getDocumentElement();
+        NodeList entityNodeList = roodNode.getElementsByTagNameNS("http://mpi.nl/tla/kin", "Entity");
+        Element entityNode = ((Element) entityNodeList.item(0));
+        NodeList relationsNodeList = entityNode.getElementsByTagNameNS("http://mpi.nl/tla/kin", "Relations");
+        Node relationsNode = relationsNodeList.item(0);
+        if (relationsNode != null) {
+            // remove the old relations
+            entityNode.removeChild(relationsNode);
+        }
+        return entityNode;
+    }
 
-                Element linkElement = metadataDom.createElement("Link");
-                linkElement.setTextContent(graphPanel.getPathForElementId(selectedIdentifiers[relationCounter]));
-                relationElement.appendChild(linkElement);
-
-                // add a unique identifier of the target entity to the link
-                Element uniqueIdentifierElement = metadataDom.createElement("UniqueIdentifier");
-                Element localIdentifierElement = metadataDom.createElement("LocalIdentifier");
-                localIdentifierElement.setTextContent(selectedIdentifiers[relationCounter]);
-                uniqueIdentifierElement.appendChild(localIdentifierElement);
-                relationElement.appendChild(uniqueIdentifierElement);
-
-                Element typeElement = metadataDom.createElement("Type");
-                typeElement.setTextContent(relationType.name());
-                relationElement.appendChild(typeElement);
-
-                Element targetNameElement = metadataDom.createElement("TargetName");
-//                targetNameElement.setTextContent(lineParts[2]);
-                relationElement.appendChild(targetNameElement);
+    public void linkEntities(GraphPanel graphPanel, UniqueIdentifier[] selectedIdentifiers, DataTypes.RelationType relationType) {
+        HashMap<UniqueIdentifier, EntityData> selectedEntityMap = graphPanel.getEntitiesById(selectedIdentifiers);
+        EntityData leadSelectionEntity = selectedEntityMap.get(selectedIdentifiers[0]);
+        for (EntityData alterEntity : selectedEntityMap.values()) {
+            if (!alterEntity.equals(leadSelectionEntity)) {
+                // add the new relation
+                leadSelectionEntity.addRelatedNode(alterEntity, 0, relationType, DataTypes.RelationLineType.sanguineLine, null, null);
             }
-            ArbilComponentBuilder.savePrettyFormatting(metadataDom, new File(targetUri));
-            new EntityCollection().updateDatabase(targetUri);
-            //ArbilDataNodeLoader.getSingleInstance().
-            // todo: the ArbilDateNode will need to be reloaded here
-        } catch (URISyntaxException exception) {
-            new ArbilBugCatcher().logError(exception);
-        } catch (DOMException exception) {
-            new ArbilBugCatcher().logError(exception);
-        } catch (IOException exception) {
-            new ArbilBugCatcher().logError(exception);
-        } catch (ParserConfigurationException exception) {
-            new ArbilBugCatcher().logError(exception);
-        } catch (SAXException exception) {
-            new ArbilBugCatcher().logError(exception);
+        }
+        for (EntityData saveEntity : selectedEntityMap.values()) {
+            String targetPath = saveEntity.getEntityPath();
+            try {
+                URI targetUri = new URI(targetPath);
+                Document metadataDom = ArbilComponentBuilder.getDocument(targetUri);
+                Node entityNode = getEntityNode(metadataDom);
+
+                RelationArray relationArray = new RelationArray(saveEntity.getAllRelations());
+                // add all the current relaions
+                JAXBContext jaxbContext = JAXBContext.newInstance(RelationArray.class);
+                Marshaller marshaller = jaxbContext.createMarshaller();
+                marshaller.marshal(relationArray, entityNode);
+                // save the xml file
+                ArbilComponentBuilder.savePrettyFormatting(metadataDom, new File(targetUri));
+                // update the database
+                new EntityCollection().updateDatabase(targetUri);
+            } catch (JAXBException exception) {
+                // todo: inform the user if there is an error
+                new ArbilBugCatcher().logError(exception);
+//                throw new ImportException("Error: " + exception.getMessage());
+//            } catch (TransformerException exception) {
+//                new ArbilBugCatcher().logError(exception);
+            } catch (URISyntaxException exception) {
+                new ArbilBugCatcher().logError(exception);
+            } catch (DOMException exception) {
+                new ArbilBugCatcher().logError(exception);
+            } catch (IOException exception) {
+                new ArbilBugCatcher().logError(exception);
+            } catch (ParserConfigurationException exception) {
+                new ArbilBugCatcher().logError(exception);
+            } catch (SAXException exception) {
+                new ArbilBugCatcher().logError(exception);
+            }
         }
     }
 }
