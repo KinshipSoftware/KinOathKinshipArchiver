a47a815a1b855c7f8f31f0d46fd621e1e40765fd
Peter Withers <Peter.Withers@mpi.nl>
2011-08-10 11:58:48 +0000

    Made required changes for the recent Arbil update. Updated the svg relation elements to handle non rendering relations. Added handling of gedcom FILE fields and add them to the imported entities as resource links. Added gender and date of birth/death handling to the gedcom importer. Made the Gedcom importer more generic. Made the importers more generic. Updated the CSV importer to handle more of the formats obscure data.

---
+++ desktop/src/main/java/nl/mpi/kinnate/KinnateArbilInjector.java
@@ -0,0 +1,68 @@
+package nl.mpi.kinnate;
+
+import java.awt.datatransfer.ClipboardOwner;
+import nl.mpi.arbil.ArbilInjector;
+import nl.mpi.arbil.data.ArbilDataNodeLoader;
+import nl.mpi.arbil.data.ArbilTreeHelper;
+import nl.mpi.arbil.data.DataNodeLoader;
+import nl.mpi.arbil.ui.ArbilWindowManager;
+import nl.mpi.arbil.ui.GuiHelper;
+import nl.mpi.arbil.userstorage.ArbilSessionStorage;
+import nl.mpi.arbil.userstorage.SessionStorage;
+import nl.mpi.arbil.util.ArbilMimeHashQueue;
+import nl.mpi.arbil.util.BugCatcher;
+import nl.mpi.arbil.util.MessageDialogHandler;
+import nl.mpi.arbil.util.MimeHashQueue;
+import nl.mpi.arbil.util.TreeHelper;
+import nl.mpi.arbil.util.WindowManager;
+
+/**
+ * Takes care of injecting certain class instances into objects or classes.
+ * This provides us with a sort of dependency injection, which enables loosening
+ * the coupling between for example data classes and UI classes.
+ *
+ * @author Twan Goosen <twan.goosen@mpi.nl>
+ * Cut down version as required for kinship
+ * Created on : May 3, 2011, 12:17:34 PM
+ * @author     : Peter Withers
+ */
+public class KinnateArbilInjector extends ArbilInjector {
+
+    /**
+     * Does initial injection into static classes. Needs to be called only once.
+     */
+    public static synchronized void injectHandlers() {
+        final BugCatcher bugCatcher = GuiHelper.linorgBugCatcher;
+        ArbilSessionStorage.setBugCatcher(bugCatcher);
+        ArbilMimeHashQueue.setBugCatcher(bugCatcher);
+        injectBugCatcher(bugCatcher);
+
+        final MessageDialogHandler messageDialogHandler = ArbilWindowManager.getSingleInstance();
+        ArbilSessionStorage.setMessageDialogHandler(messageDialogHandler);
+        ArbilMimeHashQueue.setMessageDialogHandler(messageDialogHandler);
+        injectDialogHandler(messageDialogHandler);
+
+        final WindowManager windowManager = ArbilWindowManager.getSingleInstance();
+        ArbilSessionStorage.setWindowManager(windowManager);
+        injectWindowManager(windowManager);
+
+        final ClipboardOwner clipboardOwner = GuiHelper.getClipboardOwner();
+        injectClipboardOwner(clipboardOwner);
+
+        ArbilSessionStorage.setBugCatcher(bugCatcher);
+        final SessionStorage sessionStorage = ArbilSessionStorage.getSingleInstance();
+        ArbilDataNodeLoader.setSessionStorage(sessionStorage);
+        ArbilMimeHashQueue.setSessionStorage(sessionStorage);
+        injectSessionStorage(sessionStorage);
+
+        final MimeHashQueue mimeHashQueue = ArbilMimeHashQueue.getSingleInstance();
+        injectMimeHashQueue(mimeHashQueue);
+
+        final DataNodeLoader dataNodeLoader = ArbilDataNodeLoader.getSingleInstance();
+        ArbilMimeHashQueue.setDataNodeLoader(dataNodeLoader);
+        injectDataNodeLoader(dataNodeLoader);
+
+        final TreeHelper treeHelper = ArbilTreeHelper.getSingleInstance();
+        injectTreeHelper(treeHelper);
+    }
+}
