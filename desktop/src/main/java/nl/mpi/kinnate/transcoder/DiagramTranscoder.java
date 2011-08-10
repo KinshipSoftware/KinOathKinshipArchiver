e180e9a5019c42375c588907382120e36f4d96a3
Peter Withers <Peter.Withers@mpi.nl>
2011-08-10 12:43:04 +0000

    Updated the pdf export but more work to be done here. Made required changes for the recent Arbil update. Updated the svg relation elements to handle non rendering relations. Added handling of gedcom FILE fields and add them to the imported entities as resource links. Added gender and date of birth/death handling to the gedcom importer. Made the Gedcom importer more generic. Made the importers more generic. Updated the CSV importer to handle more of the formats obscure data.

--- desktop/src/main/java/nl/mpi/kinnate/transcoder/DiagramTranscoder.java
+++ desktop/src/main/java/nl/mpi/kinnate/transcoder/DiagramTranscoder.java
@@ -1,12 +1,19 @@
 package nl.mpi.kinnate.transcoder;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import nl.mpi.arbil.util.ArbilBugCatcher;
 import nl.mpi.kinnate.SavePanel;
-//import org.apache.batik.transcoder.image.JPEGTranscoder;
-//import org.apache.batik.transcoder.TranscoderInput;
-//import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.Transcoder;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.XMLAbstractTranscoder;
+import org.apache.batik.transcoder.image.ImageTranscoder;
+import org.apache.batik.transcoder.image.JPEGTranscoder;
+import org.apache.fop.svg.PDFTranscoder;
 
 /**
  *  Document   : DiagramTranscoder
@@ -16,6 +23,54 @@ import nl.mpi.kinnate.SavePanel;
 public class DiagramTranscoder {
 
     public void saveAsJpg(SavePanel savePanel) {
+        // todo: offer user to select an export file name
+        if (savePanel.hasSaveFileName()) {
+            // todo: tell user to save as
+        }
+        if (savePanel.requiresSave()) {
+            // todo: tell user to save
+        }
+
+        File diagramSvg = savePanel.getFileName();
+        File diagramJpg = new File(diagramSvg.getParentFile(), diagramSvg.getName().replaceFirst("\\.[Ss][Vv][Gg]$", ".jpg"));
+        JPEGTranscoder transcoder = new JPEGTranscoder();
+
+        transcoder.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, new Float(.8));
+        final int dpi = 300;
+        transcoder.addTranscodingHint(ImageTranscoder.KEY_PIXEL_UNIT_TO_MILLIMETER, new Float((float) (25.4 / dpi)));
+        transcoder.addTranscodingHint(XMLAbstractTranscoder.KEY_XML_PARSER_VALIDATING, Boolean.FALSE);
+        transcoder.addTranscodingHint(PDFTranscoder.KEY_STROKE_TEXT, Boolean.FALSE);
+        InputStream inputStream = null;
+        try {
+            inputStream = new java.io.FileInputStream(diagramSvg);
+            TranscoderInput transcoderInput = new TranscoderInput(inputStream);
+            transcoderInput.setURI(diagramSvg.toURI().toASCIIString());
+            OutputStream outputStream = new java.io.FileOutputStream(diagramJpg);
+            outputStream = new java.io.BufferedOutputStream(outputStream);
+            try {
+                TranscoderOutput transcoderOutput = new TranscoderOutput(outputStream);
+                // todo: resolve the issue here when transcoding to jpg
+                transcoder.transcode(transcoderInput, transcoderOutput);
+            } finally {
+                outputStream.close();
+            }
+        } catch (TranscoderException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } catch (IOException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException exception) {
+                    new ArbilBugCatcher().logError(exception);
+                }
+            }
+        }
+    }
+
+    public void saveAsPdf(SavePanel savePanel) {
+        // todo: offer user to select an export file name
         if (savePanel.hasSaveFileName()) {
             // todo: tell user to save as
         }
@@ -24,17 +79,46 @@ public class DiagramTranscoder {
         }
         File diagramSvg = savePanel.getFileName();
         File diagramPdf = new File(diagramSvg.getParentFile(), diagramSvg.getName().replaceFirst("\\.[Ss][Vv][Gg]$", ".pdf"));
-        File diagramJpg = new File(diagramSvg.getParentFile(), diagramSvg.getName().replaceFirst("\\.[Ss][Vv][Gg]$", ".jpg"));
-//        JPEGTranscoder transcoder = new JPEGTranscoder();
-//        transcoder.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, new Float(.8));
-//        TranscoderInput transcoderInput = new TranscoderInput(diagramSvg.toString());
-//        OutputStream outputStream = new FileOutputStream(diagramJpg);
-//        TranscoderOutput transcoderOutput = new TranscoderOutput(outputStream);
-//        transcoder.transcode(transcoderInput, transcoderOutput);
-//        outputStream.flush();
-//        outputStream.close();
-    }
+        Transcoder transcoder = new PDFTranscoder();
 
-    public void saveAsPdf(SavePanel savePanel) {
+        //Configure the transcoder
+//        try {
+//            DefaultConfigurationBuilder cfgBuilder = new DefaultConfigurationBuilder();
+//            Configuration cfg = cfgBuilder.buildFromFile(new File("pdf-renderer-cfg.xml"));
+//            ContainerUtil.configure(transcoder, cfg);
+//        } catch (Exception e) {
+//            throw new TranscoderException(e);
+//        }
+
+        final int dpi = 300;
+        transcoder.addTranscodingHint(ImageTranscoder.KEY_PIXEL_UNIT_TO_MILLIMETER, new Float((float) (25.4 / dpi)));
+        transcoder.addTranscodingHint(XMLAbstractTranscoder.KEY_XML_PARSER_VALIDATING, Boolean.FALSE);
+        transcoder.addTranscodingHint(PDFTranscoder.KEY_STROKE_TEXT, Boolean.FALSE);
+        InputStream inputStream = null;
+        try {
+            inputStream = new java.io.FileInputStream(diagramSvg);
+            TranscoderInput transcoderInput = new TranscoderInput(inputStream);
+            transcoderInput.setURI(diagramSvg.toURI().toASCIIString());
+            OutputStream outputStream = new java.io.FileOutputStream(diagramPdf);
+            outputStream = new java.io.BufferedOutputStream(outputStream);
+            try {
+                TranscoderOutput transcoderOutput = new TranscoderOutput(outputStream);
+                transcoder.transcode(transcoderInput, transcoderOutput);
+            } finally {
+                outputStream.close();
+            }
+        } catch (TranscoderException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } catch (IOException exception) {
+            new ArbilBugCatcher().logError(exception);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException exception) {
+                    new ArbilBugCatcher().logError(exception);
+                }
+            }
+        }
     }
 }
