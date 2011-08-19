59c963ede6c52ebf73291943101313694e0736cd
Peter Withers <Peter.Withers@mpi.nl>
2011-08-19 13:23:13 +0000

    Added more user feedback when loading entity data to draw the kin diagram.

--- desktop/src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
+++ desktop/src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
@@ -2,6 +2,7 @@ package nl.mpi.kinnate.entityindexer;
 
 import java.net.URI;
 import java.util.HashSet;
+import javax.swing.JProgressBar;
 import nl.mpi.kinnate.kindata.EntityData;
 import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 import nl.mpi.kinnate.kintypestrings.ParserHighlight;
@@ -14,6 +15,5 @@ import nl.mpi.kinnate.kintypestrings.ParserHighlight;
 public interface EntityService {
 
 //    public void primeWithEntities(EntityData[] preLoadedEntities); // it would seem not to be a good idea to try and use existing entities from an svg file when their relations might not exist, so we will allow the existing entities to be used on the graph but not for database actions
-
-    public EntityData[] processKinTypeStrings(URI[] egoNodes, HashSet<UniqueIdentifier> egoIdentifiers, HashSet<UniqueIdentifier> requiredEntityIdentifiers, String[] kinTypeStrings, ParserHighlight[] parserHighlight, IndexerParameters indexParameters) throws EntityServiceException;
+    public EntityData[] processKinTypeStrings(URI[] egoNodes, HashSet<UniqueIdentifier> egoIdentifiers, HashSet<UniqueIdentifier> requiredEntityIdentifiers, String[] kinTypeStrings, ParserHighlight[] parserHighlight, IndexerParameters indexParameters, JProgressBar progressBar) throws EntityServiceException;
 }
