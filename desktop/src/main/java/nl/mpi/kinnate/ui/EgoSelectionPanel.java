87e126d3cdb846f235ee71f3e3cbd9707908141f
Peter Withers <Peter.Withers@mpi.nl>
2011-08-25 14:52:22 +0000

    Modified the search pane to use the KinTree nodes and query the database with a single query that returns all the entity data so that no further processing needs to be done to show the results. The resulting speed is very much improved with 8222 entities being shown in the tree in a reasonable amount of time.

--- desktop/src/main/java/nl/mpi/kinnate/ui/EgoSelectionPanel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/EgoSelectionPanel.java
@@ -20,17 +20,10 @@ import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 public class EgoSelectionPanel extends JPanel {
 
     private KinTree egoTree;
-//    DefaultTreeModel egoModel;
-//    DefaultMutableTreeNode egoRootNode;
     private KinTree requiredTree;
-//    DefaultTreeModel requiredModel;
-//    DefaultMutableTreeNode requiredRootNode;
     private KinTree impliedTree;
     private KinTree transientTree;
-//    DefaultTreeModel impliedModel;
-//    DefaultMutableTreeNode impliedRootNode;
     JPanel labelPanel3;
-//    JLabel transientLabel;
     JScrollPane metadataNodeScrolPane;
     JScrollPane transientNodeScrolPane;
 
@@ -45,25 +38,14 @@ public class EgoSelectionPanel extends JPanel {
         transientNodeScrolPane = new JScrollPane(transientNodePanel);
 
         metadataNodePanel = new JPanel(new BorderLayout());
-//        egoRootNode = new DefaultMutableTreeNode(new JLabel("Ego", cellRenderer.getDefaultOpenIcon(), JLabel.LEFT));
-//        egoModel = new DefaultTreeModel(egoRootNode);
-//        egoTree = new JTree(egoModel);
         egoTree = new KinTree();
-        // todo: add trac task to modify the arbil tree such that each tree can have its own preview table
-//        egoTree.getModel().
 //        egoTree.setRootVisible(false);
         egoTree.setCellRenderer(new ArbilTreeRenderer());
 
-//        requiredRootNode = new DefaultMutableTreeNode(new JLabel("Required", cellRenderer.getDefaultOpenIcon(), JLabel.LEFT));
-//        requiredModel = new DefaultTreeModel(requiredRootNode);
-//        requiredTree = new JTree(requiredModel);
         requiredTree = new KinTree();
 //        requiredTree.setRootVisible(false);
         requiredTree.setCellRenderer(new ArbilTreeRenderer());
 
-//        impliedRootNode = new DefaultMutableTreeNode(new JLabel("Implied", cellRenderer.getDefaultOpenIcon(), JLabel.LEFT));
-//        impliedModel = new DefaultTreeModel(impliedRootNode);
-//        impliedTree = new JTree(impliedModel);
         impliedTree = new KinTree();
 //        impliedTree.setRootVisible(false);
         impliedTree.setCellRenderer(new ArbilTreeRenderer());
@@ -71,17 +53,13 @@ public class EgoSelectionPanel extends JPanel {
 //        egoTree.setRootVisible(false);
         this.setLayout(new BorderLayout());
         JPanel treePanel2 = new JPanel(new BorderLayout());
-//        treePanel.setLayout(new BoxLayout(treePanel, BoxLayout.PAGE_AXIS));
         metadataNodeScrolPane = new JScrollPane(metadataNodePanel);
-//        this.setBorder(javax.swing.BorderFactory.createTitledBorder("Selected Ego"));
         JPanel labelPanel1 = new JPanel(new BorderLayout());
         JPanel labelPanel2 = new JPanel(new BorderLayout());
         labelPanel3 = new JPanel(new BorderLayout());
-//        JPanel labelPanel4 = new JPanel(new BorderLayout());
         labelPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Ego"));
         labelPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Required"));
         labelPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Implied"));
-//        labelPanel4.setBorder(javax.swing.BorderFactory.createTitledBorder("Transient"));
         labelPanel1.add(egoTree, BorderLayout.CENTER);
         labelPanel2.add(requiredTree, BorderLayout.CENTER);
         labelPanel3.add(impliedTree, BorderLayout.CENTER);
@@ -118,20 +96,14 @@ public class EgoSelectionPanel extends JPanel {
                 }
             }
         }
-//            setEgoNodes(egoNodeArray, egoTree, egoModel, egoRootNode);
         egoTree.rootNodeChildren = egoNodeArray.toArray(new ArbilNode[]{});
         egoTree.requestResort();
-//            setEgoNodes(requiredNodeArray, requiredTree, requiredModel, requiredRootNode);
         requiredTree.rootNodeChildren = requiredNodeArray.toArray(new ArbilNode[]{});
         requiredTree.requestResort();
-//            setEgoNodes(remainingNodeArray, impliedTree, impliedModel, impliedRootNode);
         impliedTree.rootNodeChildren = remainingNodeArray.toArray(new ArbilNode[]{});
         impliedTree.requestResort();
         transientTree.rootNodeChildren = new ArbilNode[]{};
         transientTree.requestResort();
-//        if (transientLabel != null) {
-//            labelPanel3.remove(transientLabel);
-//        }
     }
 
     public void setTransientNodes(EntityData[] allEntities) {
@@ -148,26 +120,9 @@ public class EgoSelectionPanel extends JPanel {
 
         egoTree.rootNodeChildren = new ArbilNode[]{};
         egoTree.requestResort();
-//            setEgoNodes(requiredNodeArray, requiredTree, requiredModel, requiredRootNode);
         requiredTree.rootNodeChildren = new ArbilNode[]{};
         requiredTree.requestResort();
-//            setEgoNodes(remainingNodeArray, impliedTree, impliedModel, impliedRootNode);
         impliedTree.rootNodeChildren = new ArbilNode[]{};
         impliedTree.requestResort();
-
-
-//        transientLabel = new JLabel(allEntities.length + " transient entities have been generated");
-//        labelPanel3.add(transientLabel, BorderLayout.PAGE_START);
     }
-//    private void setEgoNodes(ArrayList<ArbilDataNode> selectedNodes, JTree currentTree, DefaultTreeModel currentModel, DefaultMutableTreeNode currentRootNode) {
-//        currentRootNode.removeAllChildren();
-//        currentModel.nodeStructureChanged(currentRootNode);
-//        if (selectedNodes != null) {
-//            for (ArbilDataNode imdiTreeObject : selectedNodes) {
-////                System.out.println("adding node: " + imdiTreeObject.toString());
-//                currentModel.insertNodeInto(new DefaultMutableTreeNode(imdiTreeObject), currentRootNode, 0);
-//            }
-//            currentTree.expandPath(new TreePath(currentRootNode.getPath()));
-//        }
-//    }
 }
