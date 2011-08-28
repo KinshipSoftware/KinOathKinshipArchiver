2311237bd92749add0a6429747c454eaf3f00a70
Peter Withers <Peter.Withers@mpi.nl>
2011-08-28 08:24:09 +0000

    Connected the kin trees to the graph panel so that the user in the tree can update the selection in the kin diagram.

--- desktop/src/main/java/nl/mpi/kinnate/ui/KinTree.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/KinTree.java
@@ -1,8 +1,16 @@
 package nl.mpi.kinnate.ui;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.data.ArbilDataNodeLoader;
 import nl.mpi.arbil.data.ArbilNode;
+import nl.mpi.arbil.ui.ArbilTableModel;
 import nl.mpi.arbil.ui.ArbilTree;
+import nl.mpi.arbil.ui.GuiHelper;
+import nl.mpi.kinnate.data.KinTreeNode;
+import nl.mpi.kinnate.svg.GraphPanel;
+import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 
 /**
  *  Document   : KinTree
@@ -11,14 +19,33 @@ import nl.mpi.arbil.ui.ArbilTree;
  */
 public class KinTree extends ArbilTree {
 
+    GraphPanel graphPanel;
+
+    public KinTree(GraphPanel graphPanel) {
+        this.graphPanel = graphPanel;
+    }
+
     @Override
     protected void putSelectionIntoPreviewTable() {
+        // todo: in arbil the preview table only shows the lead selection, however in the kinship tree it might best to show the entire selection
         ArbilNode arbilNode = getLeadSelectionNode();
         if (arbilNode instanceof ArbilDataNode) {
-            // todo: clear the graph selection
+            // clear the graph selection
+            graphPanel.setSelectedIds(new UniqueIdentifier[]{});
             super.putSelectionIntoPreviewTable();
-        } else {
-            // todo: set the graph selection
+        } else if (arbilNode instanceof KinTreeNode) {
+            // set the graph selection
+            graphPanel.setSelectedIds(new UniqueIdentifier[]{((KinTreeNode) arbilNode).entityData.getUniqueIdentifier()});
+            final ArbilTableModel previewTableModel = customPreviewTable.getArbilTableModel();
+            try {
+                final ArbilDataNode arbilDataNode = ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, new URI(((KinTreeNode) arbilNode).entityData.getEntityPath()));
+                if (!(previewTableModel.getArbilDataNodeCount() == 1 && previewTableModel.containsArbilDataNode(arbilDataNode))) {
+                    previewTableModel.removeAllArbilDataNodeRows();
+                    previewTableModel.addSingleArbilDataNode(arbilDataNode);
+                }
+            } catch (URISyntaxException urise) {
+                GuiHelper.linorgBugCatcher.logError(urise);
+            }
         }
     }
 }
