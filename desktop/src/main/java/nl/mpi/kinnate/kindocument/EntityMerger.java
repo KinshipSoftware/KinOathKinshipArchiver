96179c4e24b75930e3536deff117774d6cab845b
Peter Withers <Peter.Withers@mpi.nl>
2011-09-04 09:31:03 +0000

    Added menu item to merge selected entities into one entity taking all the relations. Added menu item to duplicate an existing entity with its relations.

--- desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityMerger.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityMerger.java
@@ -18,31 +18,41 @@ import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
  */
 public class EntityMerger {
 
-    public UniqueIdentifier[] mergeEntities(GraphPanel graphPanel, UniqueIdentifier[] selectedIdentifiers) throws ImportException {
+    private EntityDocument getEntityDocuments(GraphPanel graphPanel, UniqueIdentifier[] selectedIdentifiers, HashMap<UniqueIdentifier, EntityDocument> entityMap, ArrayList<EntityDocument> entityDocumentList) throws ImportException, URISyntaxException {
         EntityDocument leadEntityDocument = null;
+        for (EntityData alterEntity : graphPanel.getEntitiesById(selectedIdentifiers).values()) {
+            EntityDocument entityDocument = new EntityDocument(new URI(alterEntity.getEntityPath()), new ImportTranslator(true));
+            if (leadEntityDocument == null) {
+                leadEntityDocument = entityDocument;
+            } else {
+                entityDocumentList.add(entityDocument);
+            }
+            entityMap.put(entityDocument.entityData.getUniqueIdentifier(), entityDocument);
+        }
+        for (EntityDocument alterEntity : entityMap.values()) {
+            for (EntityRelation entityRelation : alterEntity.entityData.getDistinctRelateNodes()) {
+                EntityDocument relatedDocument = entityMap.get(entityRelation.alterUniqueIdentifier);
+                if (relatedDocument == null) {
+                    // todo: this might need to make sure the relate entity is loaded or just get the path from the database
+                    relatedDocument = new EntityDocument(new URI(entityRelation.getAlterNode().getEntityPath()), new ImportTranslator(true));
+                    entityMap.put(relatedDocument.entityData.getUniqueIdentifier(), relatedDocument);
+                }
+            }
+        }
+        return leadEntityDocument;
+    }
+
+    public UniqueIdentifier[] mergeEntities(GraphPanel graphPanel, UniqueIdentifier[] selectedIdentifiers) throws ImportException {
         ArrayList<EntityDocument> entityDocumentList = new ArrayList<EntityDocument>();
         HashMap<UniqueIdentifier, EntityDocument> entityMap = new HashMap<UniqueIdentifier, EntityDocument>();
         try {
-            for (EntityData alterEntity : graphPanel.getEntitiesById(selectedIdentifiers).values()) {
-                EntityDocument entityDocument = new EntityDocument(new URI(alterEntity.getEntityPath()), new ImportTranslator(true));
-                if (leadEntityDocument == null) {
-                    leadEntityDocument = entityDocument;
-                } else {
-                    entityDocumentList.add(entityDocument);
-                }
-                entityMap.put(entityDocument.entityData.getUniqueIdentifier(), entityDocument);
-            }
+            EntityDocument leadEntityDocument = getEntityDocuments(graphPanel, selectedIdentifiers, entityMap, entityDocumentList);
             for (EntityDocument alterEntity : entityDocumentList) {
                 for (EntityRelation entityRelation : alterEntity.entityData.getDistinctRelateNodes()) {
-                    // add the new relation
-                    leadEntityDocument.entityData.addRelatedNode(entityRelation.getAlterNode(), entityRelation.relationType, entityRelation.relationLineType, entityRelation.lineColour, entityRelation.labelString);
-                    // remove the old entity relation
                     EntityDocument relatedDocument = entityMap.get(entityRelation.alterUniqueIdentifier);
-                    if (relatedDocument == null) {
-                        // todo: this might need to make sure the relate entity is loaded or just get the path from the database
-                        relatedDocument = new EntityDocument(new URI(entityRelation.getAlterNode().getEntityPath()), new ImportTranslator(true));
-                        entityMap.put(relatedDocument.entityData.getUniqueIdentifier(), relatedDocument);
-                    }
+                    // add the new relation
+                    leadEntityDocument.entityData.addRelatedNode(relatedDocument.entityData, entityRelation.relationType, entityRelation.relationLineType, entityRelation.lineColour, entityRelation.labelString);
+                    // remove the old entity relation
                     relatedDocument.entityData.removeRelationsWithNode(alterEntity.entityData);
                 }
                 alterEntity.setAsDeleteDocument();
@@ -59,6 +69,28 @@ public class EntityMerger {
     }
 
     public UniqueIdentifier[] duplicateEntities(GraphPanel graphPanel, UniqueIdentifier[] selectedIdentifiers) throws ImportException {
-        return selectedIdentifiers;
+        ArrayList<UniqueIdentifier> addedIdentifiers = new ArrayList<UniqueIdentifier>();
+        ArrayList<EntityDocument> entityDocumentList = new ArrayList<EntityDocument>();
+        HashMap<UniqueIdentifier, EntityDocument> entityMap = new HashMap<UniqueIdentifier, EntityDocument>();
+        try {
+            getEntityDocuments(graphPanel, selectedIdentifiers, entityMap, entityDocumentList);
+            for (UniqueIdentifier uniqueIdentifier : selectedIdentifiers) {
+                EntityDocument masterDocument = entityMap.get(uniqueIdentifier);
+                EntityDocument duplicateEntityDocument = new EntityDocument(new ImportTranslator(true));
+                for (EntityRelation entityRelation : masterDocument.entityData.getDistinctRelateNodes()) {
+                    EntityDocument relatedDocument = entityMap.get(entityRelation.alterUniqueIdentifier);
+                    // copy the relations
+                    duplicateEntityDocument.entityData.addRelatedNode(relatedDocument.entityData, entityRelation.relationType, entityRelation.relationLineType, entityRelation.lineColour, entityRelation.labelString);
+                }
+                // todo: the date and any other metadata not in the metadata node will be missed by this step, it would be best to move or modify the dates location in the file
+                // copy the metadata
+                duplicateEntityDocument.importNode(masterDocument.getMetadataNode());
+                duplicateEntityDocument.saveDocument();
+            }
+            return addedIdentifiers.toArray(new UniqueIdentifier[]{});
+        } catch (URISyntaxException exception) {
+            new ArbilBugCatcher().logError(exception);
+            throw new ImportException("Error: " + exception.getMessage());
+        }
     }
 }
