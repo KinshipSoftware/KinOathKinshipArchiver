0e5d88d61b407b493c8d8ca925d905b387c2e7ec
Peter Withers <Peter.Withers@mpi.nl>
2011-09-28 12:27:43 +0000

    Updated the entity document loaded to be in shared code and always set the ego's alter nodes.

--- desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityMerger.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityMerger.java
@@ -17,41 +17,7 @@ import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
  *  Created on : Aug 29, 2011, 3:47:29 PM
  *  Author     : Peter Withers
  */
-public class EntityMerger {
-
-    private EntityDocument getEntityDocuments(GraphPanel graphPanel, UniqueIdentifier[] selectedIdentifiers, HashMap<UniqueIdentifier, EntityDocument> entityMap, ArrayList<EntityDocument> entityDocumentList) throws ImportException, URISyntaxException {
-        EntityDocument leadEntityDocument = null;
-        for (EntityData alterEntity : graphPanel.getEntitiesById(selectedIdentifiers).values()) {
-            EntityDocument entityDocument = new EntityDocument(new URI(alterEntity.getEntityPath()), new ImportTranslator(true));
-            if (leadEntityDocument == null) {
-                leadEntityDocument = entityDocument;
-            } else {
-                entityDocumentList.add(entityDocument);
-            }
-            entityMap.put(entityDocument.entityData.getUniqueIdentifier(), entityDocument);
-        }
-        for (EntityDocument alterEntity : entityMap.values().toArray(new EntityDocument[]{})) {
-            for (EntityRelation entityRelation : alterEntity.entityData.getDistinctRelateNodes()) {
-                EntityDocument relatedDocument = entityMap.get(entityRelation.alterUniqueIdentifier);
-                if (relatedDocument == null) {
-                    // get the path from the database
-                    final URI entityUri = new URI(new EntityCollection().getEntityPath(entityRelation.alterUniqueIdentifier));
-                    relatedDocument = new EntityDocument(entityUri, new ImportTranslator(true));
-                    entityMap.put(entityRelation.alterUniqueIdentifier, relatedDocument);
-//                    entityRelation.setAlterNode(relatedDocument.entityData);
-                }
-            }
-        }
-        // set the alter entity for each relation if not already set (based on the known unique identifier)
-        for (EntityDocument entityDocument : entityMap.values()) {
-            for (EntityRelation nodeRelation : entityDocument.entityData.getRelatedNodesToBeLoaded()) {
-                nodeRelation.setAlterNode(entityMap.get(nodeRelation.alterUniqueIdentifier).entityData);
-            }
-        }
-
-
-        return leadEntityDocument;
-    }
+public class EntityMerger extends DocumentLoader{
 
     public void mergeEntities(GraphPanel graphPanel, UniqueIdentifier[] selectedIdentifiers) throws ImportException {
         ArrayList<EntityDocument> nonLeadEntityDocuments = new ArrayList<EntityDocument>();
