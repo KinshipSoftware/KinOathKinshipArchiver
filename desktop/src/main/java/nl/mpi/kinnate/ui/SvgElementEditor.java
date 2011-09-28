2223dbfb9883c8409bdaaa1231f4110abcff3771
Peter Withers <Peter.Withers@mpi.nl>
2011-09-28 13:57:56 +0000

    Updated the ArbilDataNode registration process. Updated the icon change call back handler such that it ignores the initial loading process but updates the DB and graph when a valid save action has occurred.

--- desktop/src/main/java/nl/mpi/kinnate/ui/SvgElementEditor.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/SvgElementEditor.java
@@ -1,5 +1,6 @@
 package nl.mpi.kinnate.ui;
 
+import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import org.w3c.dom.Element;
@@ -12,7 +13,15 @@ import org.w3c.dom.Element;
 public class SvgElementEditor extends JPanel {
 
     public SvgElementEditor(Element svgElement) {
+        this.setLayout(new GridLayout(0, 2));
         this.add(new JLabel("SvgElementEditor"));
+        this.add(new JLabel(svgElement.getTagName()));
+        this.add(new JLabel("Label Text:"));
+        this.add(new JLabel(svgElement.getTextContent()));
+        for (String attribiteName : new String[]{"fill", "font-size", "stroke-width"}) {
+            this.add(new JLabel(attribiteName));
+            this.add(new JLabel(svgElement.getAttribute(attribiteName)));
+        }
         // todo: Ticket #1065 Enable editing of the text and font size of labels in the diagram.
     }
 }
