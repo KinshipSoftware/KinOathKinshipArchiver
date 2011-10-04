7cdd5ae402f43a33301c3130e1a78a85a48fa210
Peter Withers <Peter.Withers@mpi.nl>
2011-10-04 11:26:13 +0000

    Added trac tickets. Added editing of label text and font size to graphics editor panel.

--- desktop/src/main/java/nl/mpi/kinnate/ui/SvgElementEditor.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/SvgElementEditor.java
@@ -1,8 +1,15 @@
 package nl.mpi.kinnate.ui;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import org.apache.batik.bridge.UpdateManager;
 import org.w3c.dom.Element;
 
 /**
@@ -12,16 +19,71 @@ import org.w3c.dom.Element;
  */
 public class SvgElementEditor extends JPanel {
 
-    public SvgElementEditor(Element svgElement) {
-        this.setLayout(new GridLayout(0, 2));
-        this.add(new JLabel("SvgElementEditor"));
-        this.add(new JLabel(svgElement.getTagName()));
-        this.add(new JLabel("Label Text:"));
-        this.add(new JLabel(svgElement.getTextContent()));
-        for (String attribiteName : new String[]{"fill", "font-size", "stroke-width"}) {
-            this.add(new JLabel(attribiteName));
-            this.add(new JLabel(svgElement.getAttribute(attribiteName)));
+    UpdateManager updateManager;
+
+    public SvgElementEditor(final UpdateManager updateManager, final Element svgElement) {
+        this.updateManager = updateManager;
+        this.setLayout(new BorderLayout());
+        JPanel outerPanel = new JPanel(new BorderLayout());
+        JPanel sidePanel = new JPanel(new GridLayout(0, 2));
+        String editorMode = svgElement.getTagName();
+        if (editorMode.equals("text")) {
+//            this.add(new JLabel("Label Text:"));
+            final JTextArea textArea = new JTextArea(svgElement.getTextContent());
+            textArea.addFocusListener(new FocusListener() {
+
+                public void focusGained(FocusEvent e) {
+                }
+
+                public void focusLost(FocusEvent e) {
+                    updateValue(svgElement, textArea.getText());
+                }
+            });
+            for (final String attribiteName : new String[]{"fill", "font-size", "stroke-width"}) {
+                sidePanel.add(new JLabel(attribiteName));
+                final JTextField textField = new JTextField(svgElement.getAttribute(attribiteName));
+                textField.addFocusListener(new FocusListener() {
+
+                    public void focusGained(FocusEvent e) {
+                    }
+
+                    public void focusLost(FocusEvent e) {
+                        updateValue(svgElement, attribiteName, textField.getText());
+                    }
+                });
+                sidePanel.add(textField);
+            }
+            outerPanel.add(textArea, BorderLayout.CENTER);
+//            JPanel sindeInnd
+            outerPanel.add(sidePanel, BorderLayout.LINE_END);
+        } else {
+            sidePanel.add(new JLabel("SvgElementEditor"));
+            sidePanel.add(new JLabel(svgElement.getTagName()));
+            outerPanel.add(sidePanel, BorderLayout.CENTER);
         }
+        this.add(new JScrollPane(outerPanel));
         // todo: Ticket #1065 Enable editing of the text and font size of labels in the diagram.
+    }
+
+    protected void updateValue(final Element changeTarget, final String attributeName, final String changeValue) {
+        if (updateManager != null) {
+            updateManager.getUpdateRunnableQueue().invokeLater(new Runnable() {
+
+                public void run() {
+                    changeTarget.setAttribute(attributeName, changeValue);
+                }
+            });
+        }
+    }
+
+    protected void updateValue(final Element changeTarget, final String changeValue) {
+        if (updateManager != null) {
+            updateManager.getUpdateRunnableQueue().invokeLater(new Runnable() {
+
+                public void run() {
+                    changeTarget.setTextContent(changeValue);
+                }
+            });
+        }
     }
 }
