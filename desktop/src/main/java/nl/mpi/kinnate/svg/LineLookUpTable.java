dc9fd22bfa92639ed47ca9562950c7764582139e
Peter Withers <Peter.Withers@mpi.nl>
2011-10-04 15:29:22 +0000

    Added a structure to detect crossing lines and add loops as required (in progress).

---
+++ desktop/src/main/java/nl/mpi/kinnate/svg/LineLookUpTable.java
@@ -0,0 +1,80 @@
+package nl.mpi.kinnate.svg;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ *  Document   : LineLookUpTable
+ *  Created on : Sep 30, 2011, 3:24:55 PM
+ *  Author     : Peter Withers
+ */
+public class LineLookUpTable {
+
+    // this hashset keeps one line record for line part for each pair of entities, the line segments might be updated when an entity is dragged
+    // todo: there will probably be multiple line parts for each pari of entities: start segment, end segment, main line and maybe some zig zag bits, even if these zig zag bits are not ued they probably should always be there for simplicity
+    HashSet<LineRecord> lineRecords = new HashSet<LineRecord>();
+
+    protected class LineRecord {
+
+        public LineRecord(String lineIdString, Point[] initialPointsList) {
+            this.lineIdSring = lineIdString;
+            this.pointsList = new ArrayList<Point>(Arrays.asList(initialPointsList));
+        }
+        private String lineIdSring;
+        private ArrayList<Point> pointsList;
+
+        protected Point getIntersection(LineRecord lineRecord) {
+            return null; //Point((lineRecord.startPoint.x + lineRecord.endPoint.x) / 2, (lineRecord.startPoint.y + lineRecord.endPoint.y) / 2);
+            // todo:...
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final LineRecord other = (LineRecord) obj;
+            if ((this.lineIdSring == null) ? (other.lineIdSring != null) : !this.lineIdSring.equals(other.lineIdSring)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 97 * hash + (this.lineIdSring != null ? this.lineIdSring.hashCode() : 0);
+            return hash;
+        }
+    }
+
+    public void getIntersectsEntity() {
+    }
+
+    public void getOverlapsOtherLine() {
+    }
+
+    public Point[] getIntersections(LineRecord localLineRecord) {
+        HashSet<Point> intersectionPoints = new HashSet<Point>();
+        for (LineRecord lineRecord : lineRecords) {
+            Point intersectionPoint = localLineRecord.getIntersection(lineRecord);
+            if (lineRecord != null) {
+                intersectionPoints.add(intersectionPoint);
+            }
+        }
+        return intersectionPoints.toArray(new Point[]{});
+    }
+
+    public Point[] adjustLineToObstructions(String lineIdString, Point[] initialPointsList) {
+        LineRecord localLineRecord = new LineRecord(lineIdString, initialPointsList);
+        getIntersections(localLineRecord);
+        //localLineRecord.pointsList.set(3, new Point(0, 0));
+        lineRecords.add(localLineRecord);
+        return localLineRecord.pointsList.toArray(new Point[]{});
+    }
+}
