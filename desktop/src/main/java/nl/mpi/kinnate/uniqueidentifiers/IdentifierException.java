a0c00e40adcaba850749ebb20b5fbf0d3090d5fd
Peter Withers <Peter.Withers@mpi.nl>
2011-10-07 07:59:53 +0000

    Added error handling and user feedback when an entity identifier on the diagram cannot be read.

---
+++ desktop/src/main/java/nl/mpi/kinnate/uniqueidentifiers/IdentifierException.java
@@ -0,0 +1,12 @@
+package nl.mpi.kinnate.uniqueidentifiers;
+
+/**
+ *  Document   : IdentifierException
+ *  Created on : Oct 6, 2011, 6:50:15 PM
+ *  Author     : Peter Withers
+ */
+public class IdentifierException extends Exception {
+
+    public IdentifierException(String message) {
+    }
+}
