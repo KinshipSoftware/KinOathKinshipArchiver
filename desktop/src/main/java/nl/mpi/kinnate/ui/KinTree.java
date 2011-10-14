7c4d91e4111c5ee6ca0441e1c3cbaeea577a2f16
Peter Withers <Peter.Withers@mpi.nl>
2011-10-14 14:59:59 +0000

    Added metadata panel to handle tree and graph selection by hiding and showing of the table, editors and linked archive data.

--- desktop/src/main/java/nl/mpi/kinnate/ui/KinTree.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/KinTree.java
@@ -2,10 +2,10 @@ package nl.mpi.kinnate.ui;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import nl.mpi.arbil.data.ArbilDataNode;
 import nl.mpi.arbil.data.ArbilDataNodeLoader;
 import nl.mpi.arbil.data.ArbilNode;
-import nl.mpi.arbil.ui.ArbilTableModel;
 import nl.mpi.arbil.ui.ArbilTree;
 import nl.mpi.arbil.ui.GuiHelper;
 import nl.mpi.kinnate.data.KinTreeNode;
@@ -28,27 +28,30 @@ public class KinTree extends ArbilTree {
     @Override
     protected void putSelectionIntoPreviewTable() {
         // todo: in arbil the preview table only shows the lead selection, however in the kinship tree it might best to show the entire selection
-        ArbilNode arbilNode = getLeadSelectionNode();
-        if (arbilNode instanceof ArbilDataNode) {
-            // clear the graph selection
-            graphPanel.setSelectedIds(new UniqueIdentifier[]{});
-            super.putSelectionIntoPreviewTable();
-        } else if (arbilNode instanceof KinTreeNode) {
-            final KinTreeNode kinTreeNode = (KinTreeNode) arbilNode;
-            // set the graph selection
-            if (kinTreeNode.entityData != null) {
-                graphPanel.setSelectedIds(new UniqueIdentifier[]{kinTreeNode.entityData.getUniqueIdentifier()});
-                final ArbilTableModel previewTableModel = customPreviewTable.getArbilTableModel();
-                try {
-                    final ArbilDataNode arbilDataNode = ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, new URI(((KinTreeNode) arbilNode).entityData.getEntityPath()));
-                    if (!(previewTableModel.getArbilDataNodeCount() == 1 && previewTableModel.containsArbilDataNode(arbilDataNode))) {
-                        previewTableModel.removeAllArbilDataNodeRows();
-                        previewTableModel.addSingleArbilDataNode(arbilDataNode);
+        ArbilNode[] arbilNodeArray = getAllSelectedNodes(); //LeadSelectionNode();
+        ArrayList<UniqueIdentifier> identifierList = new ArrayList<UniqueIdentifier>();
+//        ArrayList<URI> uriList = new ArrayList<URI>();
+        graphPanel.metadataPanel.removeAllArbilDataNodeRows();
+        for (ArbilNode arbilNode : arbilNodeArray) {
+            if (arbilNode instanceof ArbilDataNode) {
+//                uriList.add(((ArbilDataNode) arbilNode).getURI());
+                graphPanel.metadataPanel.addSingleArbilDataNode((ArbilDataNode) arbilNode);
+            } else if (arbilNode instanceof KinTreeNode) {
+                final KinTreeNode kinTreeNode = (KinTreeNode) arbilNode;
+                // set the graph selection
+                if (kinTreeNode.entityData != null) {
+                    identifierList.add(kinTreeNode.entityData.getUniqueIdentifier());
+                    try {
+                        final ArbilDataNode arbilDataNode = ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, new URI(((KinTreeNode) arbilNode).entityData.getEntityPath()));
+                        graphPanel.metadataPanel.addSingleArbilDataNode(arbilDataNode);
+                    } catch (URISyntaxException urise) {
+                        GuiHelper.linorgBugCatcher.logError(urise);
                     }
-                } catch (URISyntaxException urise) {
-                    GuiHelper.linorgBugCatcher.logError(urise);
                 }
             }
         }
+        // set the graph selection
+        graphPanel.setSelectedIds(identifierList.toArray(new UniqueIdentifier[]{}));
+        graphPanel.metadataPanel.updateEditorPane();
     }
 }
