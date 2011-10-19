8ec65cb44e21396b4053191b83b37c3970e2b2d1
Peter Withers <Peter.Withers@mpi.nl>
2011-10-19 08:23:01 +0000

    Added a panel to edit the "Kin Type Definitions" for a diagram and stored any changes made in the svg, but only if changes have been made. Added the editing of existing kin type definitions and the ability to add new kin type definitions.

--- desktop/src/main/java/nl/mpi/kinnate/ui/kintypeeditor/KinTypeTableModel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/kintypeeditor/KinTypeTableModel.java
@@ -1,7 +1,13 @@
 package nl.mpi.kinnate.ui.kintypeeditor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.table.AbstractTableModel;
 import nl.mpi.kinnate.SavePanel;
+import nl.mpi.kinnate.kindata.DataTypes;
+import nl.mpi.kinnate.kindata.DataTypes.RelationType;
+import nl.mpi.kinnate.kindata.EntityData;
+import nl.mpi.kinnate.kindata.EntityData.SymbolType;
 import nl.mpi.kinnate.kintypestrings.KinType;
 import nl.mpi.kinnate.svg.DataStoreSvg;
 
@@ -67,5 +73,63 @@ public class KinTypeTableModel extends AbstractTableModel {
         } else {
             return ""; // add a blank row at the end
         }
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        String stringValue = aValue.toString();
+        final KinType[] kinTypeDefinitions = dataStoreSvg.getKinTypeDefinitions();
+        if (rowIndex < dataStoreSvg.getKinTypeDefinitions().length) {
+            KinType kinType = kinTypeDefinitions[rowIndex];
+            String codeString = kinType.getCodeString();
+            RelationType relationType = kinType.getRelationType();
+            SymbolType symbolType = kinType.getSymbolType();
+            String displayString = kinType.getDisplayString();
+            switch (columnIndex) {
+                case 0:
+                    codeString = stringValue;
+                    break;
+                case 1:
+                    relationType = DataTypes.RelationType.valueOf(stringValue);
+                    break;
+                case 2:
+                    symbolType = EntityData.SymbolType.valueOf(stringValue);
+                    break;
+                case 3:
+                    displayString = stringValue;
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Too many columns");
+            }
+            kinTypeDefinitions[rowIndex] = new KinType(codeString, relationType, symbolType, displayString);
+            dataStoreSvg.setKinTypeDefinitions(kinTypeDefinitions);
+        } else {
+            String codeString = "undefined";
+            RelationType relationType = DataTypes.RelationType.ancestor;
+            SymbolType symbolType = EntityData.SymbolType.square;
+            String displayString = "";
+            switch (columnIndex) {
+                case 0:
+                    codeString = stringValue;
+                    break;
+                case 1:
+                    relationType = DataTypes.RelationType.valueOf(stringValue);
+                    break;
+                case 2:
+                    symbolType = EntityData.SymbolType.valueOf(stringValue);
+                    break;
+                case 3:
+                    displayString = stringValue;
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Too many columns");
+            }
+            ArrayList<KinType> kinTypesList = new ArrayList<KinType>(Arrays.asList(kinTypeDefinitions));
+            kinTypesList.add(new KinType(codeString, relationType, symbolType, displayString));
+            dataStoreSvg.setKinTypeDefinitions(kinTypesList.toArray(new KinType[]{}));
+        }
+        savePanel.updateGraph();
+        savePanel.requiresSave();
+        super.setValueAt(aValue, rowIndex, columnIndex);
     }
 }
