3b6ede65e68db421f23d121ba623e5b27b45e732
Peter Withers <Peter.Withers@mpi.nl>
2011-10-26 09:12:38 +0000

    Added a panel to edit the "Kin Type Definitions" for a diagram and stored any changes made in the svg, but only if changes have been made. Added the editing of existing kin type definitions and the ability to add new kin type definitions. Added deletion of kin types from the kin type table.

--- desktop/src/main/java/nl/mpi/kinnate/ui/kintypeeditor/KinTypeTableModel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/kintypeeditor/KinTypeTableModel.java
@@ -1,7 +1,11 @@
 package nl.mpi.kinnate.ui.kintypeeditor;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import javax.swing.JButton;
 import javax.swing.table.AbstractTableModel;
 import nl.mpi.kinnate.SavePanel;
 import nl.mpi.kinnate.kindata.DataTypes;
@@ -16,14 +20,19 @@ import nl.mpi.kinnate.svg.DataStoreSvg;
  *  Created on : Oct 18, 2011, 11:20:55 AM
  *  Author     : Peter Withers
  */
-public class KinTypeTableModel extends AbstractTableModel {
+public class KinTypeTableModel extends AbstractTableModel implements ActionListener {
 
     SavePanel savePanel;
     DataStoreSvg dataStoreSvg;
+    HashSet<KinType> checkBoxSet = new HashSet<KinType>();
+    JButton deleteSelectedButton;
 
-    public KinTypeTableModel(SavePanel savePanel, DataStoreSvg dataStoreSvg) {
+    public KinTypeTableModel(SavePanel savePanel, DataStoreSvg dataStoreSvg, JButton deleteSelectedButton) {
         this.savePanel = savePanel;
         this.dataStoreSvg = dataStoreSvg;
+        this.deleteSelectedButton = deleteSelectedButton;
+        deleteSelectedButton.setEnabled(false);
+        deleteSelectedButton.addActionListener(this);
     }
 
     @Override
@@ -32,7 +41,7 @@ public class KinTypeTableModel extends AbstractTableModel {
     }
 
     public int getColumnCount() {
-        return 4;
+        return 5;
     }
 
     @Override
@@ -46,8 +55,20 @@ public class KinTypeTableModel extends AbstractTableModel {
                 return "Symbol Type";
             case 3:
                 return "Display Name";
+            case 4:
+                return "";
             default:
                 throw new UnsupportedOperationException("Too many columns");
+        }
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 4:
+                return Boolean.class;
+            default:
+                return super.getColumnClass(columnIndex);
         }
     }
 
@@ -67,11 +88,18 @@ public class KinTypeTableModel extends AbstractTableModel {
                     return kinType.getSymbolType();
                 case 3:
                     return kinType.getDisplayString();
+                case 4:
+                    return checkBoxSet.contains(kinType);
                 default:
                     throw new UnsupportedOperationException("Too many columns");
             }
         } else {
-            return ""; // add a blank row at the end
+            switch (columnIndex) {
+                case 4:
+                    return false;
+                default:
+                    return ""; // add a blank row at the end
+            }
         }
     }
 
@@ -79,50 +107,60 @@ public class KinTypeTableModel extends AbstractTableModel {
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
         String stringValue = aValue.toString();
         final KinType[] kinTypeDefinitions = dataStoreSvg.getKinTypeDefinitions();
-        if (rowIndex < dataStoreSvg.getKinTypeDefinitions().length) {
-            KinType kinType = kinTypeDefinitions[rowIndex];
-            String codeString = kinType.getCodeString();
-            RelationType relationType = kinType.getRelationType();
-            SymbolType symbolType = kinType.getSymbolType();
-            String displayString = kinType.getDisplayString();
-            switch (columnIndex) {
-                case 0:
-                    codeString = stringValue;
-                    break;
-                case 1:
-                    relationType = DataTypes.RelationType.valueOf(stringValue);
-                    break;
-                case 2:
-                    symbolType = EntityData.SymbolType.valueOf(stringValue);
-                    break;
-                case 3:
-                    displayString = stringValue;
-                    break;
-                default:
-                    throw new UnsupportedOperationException("Too many columns");
+        if (rowIndex >= dataStoreSvg.getKinTypeDefinitions().length && columnIndex == 4) {
+            if (checkBoxSet.isEmpty()) {
+                checkBoxSet.addAll(Arrays.asList(kinTypeDefinitions));
+            } else {
+                checkBoxSet.clear();
             }
+            deleteSelectedButton.setEnabled(!checkBoxSet.isEmpty());
+            fireTableDataChanged();
+            return;
+        }
+        String codeString = "undefined";
+        RelationType relationType = DataTypes.RelationType.ancestor;
+        SymbolType symbolType = EntityData.SymbolType.square;
+        String displayString = "";
+        KinType kinType = null;
+        if (rowIndex < dataStoreSvg.getKinTypeDefinitions().length) {
+            kinType = kinTypeDefinitions[rowIndex];
+            codeString = kinType.getCodeString();
+            relationType = kinType.getRelationType();
+            symbolType = kinType.getSymbolType();
+            displayString = kinType.getDisplayString();
+        }
+        switch (columnIndex) {
+            case 0:
+                codeString = stringValue;
+                break;
+            case 1:
+                relationType = DataTypes.RelationType.valueOf(stringValue);
+                break;
+            case 2:
+                symbolType = EntityData.SymbolType.valueOf(stringValue);
+                break;
+            case 3:
+                displayString = stringValue;
+                break;
+            case 4:
+                if ((Boolean) aValue) {
+                    checkBoxSet.add(kinType);
+                } else {
+                    checkBoxSet.remove(kinType);
+                }
+                deleteSelectedButton.setEnabled(!checkBoxSet.isEmpty());
+                fireTableDataChanged();
+                return;
+            default:
+                throw new UnsupportedOperationException("Too many columns");
+        }
+        if (rowIndex < dataStoreSvg.getKinTypeDefinitions().length) {
             kinTypeDefinitions[rowIndex] = new KinType(codeString, relationType, symbolType, displayString);
             dataStoreSvg.setKinTypeDefinitions(kinTypeDefinitions);
         } else {
-            String codeString = "undefined";
-            RelationType relationType = DataTypes.RelationType.ancestor;
-            SymbolType symbolType = EntityData.SymbolType.square;
-            String displayString = "";
-            switch (columnIndex) {
-                case 0:
-                    codeString = stringValue;
-                    break;
-                case 1:
-                    relationType = DataTypes.RelationType.valueOf(stringValue);
-                    break;
-                case 2:
-                    symbolType = EntityData.SymbolType.valueOf(stringValue);
-                    break;
-                case 3:
-                    displayString = stringValue;
-                    break;
-                default:
-                    throw new UnsupportedOperationException("Too many columns");
+            if ("".equals(aValue)) {
+                // ignore if no text has been entered
+                return;
             }
             ArrayList<KinType> kinTypesList = new ArrayList<KinType>(Arrays.asList(kinTypeDefinitions));
             kinTypesList.add(new KinType(codeString, relationType, symbolType, displayString));
@@ -131,5 +169,16 @@ public class KinTypeTableModel extends AbstractTableModel {
         savePanel.updateGraph();
         savePanel.requiresSave();
         super.setValueAt(aValue, rowIndex, columnIndex);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        ArrayList<KinType> kinTypesList = new ArrayList<KinType>(Arrays.asList(dataStoreSvg.getKinTypeDefinitions()));
+        for (KinType kinType : checkBoxSet) {
+            kinTypesList.remove(kinType);
+        }
+        dataStoreSvg.setKinTypeDefinitions(kinTypesList.toArray(new KinType[]{}));
+        checkBoxSet.clear();
+        fireTableDataChanged();
+        savePanel.updateGraph();
     }
 }
