c2360f80e1e6d72a5f4c46de5d5bcefd80337a16
Peter Withers <Peter.Withers@mpi.nl>
2011-11-02 14:15:28 +0000

    Added the kin term calculator to handle finding kin matching the kin terms and constructing renderable objects to represent them on the diagram.

---
+++ desktop/src/main/java/nl/mpi/kinnate/kintypestrings/KinTermCalculator.java
@@ -0,0 +1,34 @@
+package nl.mpi.kinnate.kintypestrings;
+
+import nl.mpi.kinnate.kindata.EntityData;
+
+/**
+ *  Document   : KinTermCalculator
+ *  Created on : Oct 28, 2011, 3:17:13 PM
+ *  Author     : Peter Withers
+ */
+public class KinTermCalculator {
+
+    public void insertKinTerms(EntityData[] graphNodeArray, KinTermGroup[] kinTermsArray) {
+
+//        for (EntityData grapgNode : graphNodeArray) {
+//            if (grapgNode.isEgo) {
+//                for (KinTermGroup kinTermGroup : kinTermsArray) {
+//                    for (KinTerm kinTerm : kinTermGroup.getKinTerms()) {
+//                        if (kinTermGroup.graphShow) {
+//                            // todo: replace this with a loop over egoDataNodeList and then calculate the actual kin type strings for each one rather than the user entered ones used here
+//                            // todo: this should probably be done after all nodes have been created so that subsequent relations are taken into account when calculating the kin terms
+////                        for (String kinTermLabel : kinTermGroup.getTermLabel(fullKinTypeString)) {
+////                            // todo: this could be running too many times, maybe check for efficiency
+////                            // todo maybe move this to the last point and search for the kin rather than looping for each
+////                            currentGraphDataNode.addKinTermString(kinTermLabel, kinTermGroup.graphColour);
+////                            egoDataNode.addRelatedNode(currentGraphDataNode, DataTypes.RelationType.none, DataTypes.RelationLineType.kinTermLine, kinTermGroup.graphColour, kinTermLabel);
+////                        }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
+    }
+}
