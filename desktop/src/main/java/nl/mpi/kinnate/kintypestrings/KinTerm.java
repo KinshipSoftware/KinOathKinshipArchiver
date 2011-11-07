6c69e6a2dfd50fe9f0bf33f89e73678f8fdb578a
Peter Withers <Peter.Withers@mpi.nl>
2011-11-07 14:05:53 +0000

    Modified the mouse wheel zoom to prevent negative zoom (upside down). Updated the metadata panel so that one table contains any entity data while another table (with tree) show any linked archive data/metadata. Added Swedish Kin Terms sample. Tidied up the kin terms table and the menu that adds/shows the kin terms groups. Added an abort query process so that queries with large time consuming results can be stopped mid process as the user types in new queries. The attribute Alter has been replaced by Referent in the Kin Terms XML and the older format files will be converted into the current format.

--- desktop/src/main/java/nl/mpi/kinnate/kintypestrings/KinTerm.java
+++ desktop/src/main/java/nl/mpi/kinnate/kintypestrings/KinTerm.java
@@ -22,8 +22,18 @@ public class KinTerm {
     public String kinTerm = null;
     @XmlAttribute(name = "Description", namespace = "http://mpi.nl/tla/kin")
     public String kinTermDescription = null;
-    @XmlAttribute(name = "Alter", namespace = "http://mpi.nl/tla/kin") // todo: this should be changed to referent rather than alter, but it will affect exiting diagrams
+    @XmlAttribute(name = "Referent", namespace = "http://mpi.nl/tla/kin") // this attribute name has been changed from alter to referent
     public String alterKinTypeStrings = null;
     @XmlAttribute(name = "Propositus", namespace = "http://mpi.nl/tla/kin")
     public String propositusKinTypeStrings = null;
+
+    @XmlAttribute(name = "Alter", namespace = "http://mpi.nl/tla/kin") // Alter has been replaced by Referent so this just reads the older format value and converts it into the current format
+    public void setAlter(String alterKinTypeStrings) {
+        this.alterKinTypeStrings = alterKinTypeStrings;
+    }
+
+    // alter has been replaced by Referent and this function is just here to update old files
+    public String getAlter() {
+        return null;
+    }
 }
