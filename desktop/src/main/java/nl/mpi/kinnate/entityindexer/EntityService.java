08aa43d825e6aa0f1824e1bd3f7834e040bbcd0a
Peter Withers <Peter.Withers@mpi.nl>
2011-11-07 16:07:05 +0000

    Modified the mouse wheel zoom to prevent negative zoom (upside down). Updated the metadata panel so that one table contains any entity data while another table (with tree) show any linked archive data/metadata. Added Swedish Kin Terms sample. Tidied up the kin terms table and the menu that adds/shows the kin terms groups. Added an abort query process so that queries with large time consuming results can be stopped mid process as the user types in new queries. The attribute Alter has been replaced by Referent in the Kin Terms XML and the older format files will be converted into the current format. Tidied the graph data array storage and made sure the diagram tree is populated when an svg is loaded.

--- desktop/src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
+++ desktop/src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
@@ -13,6 +13,10 @@ import nl.mpi.kinnate.svg.DataStoreSvg;
  */
 public interface EntityService {
 
+    public void clearAbortRequest();
+
+    public void requestAbortProcess();
+
 //    public void primeWithEntities(EntityData[] preLoadedEntities); // it would seem not to be a good idea to try and use existing entities from an svg file when their relations might not exist, so we will allow the existing entities to be used on the graph but not for database actions
-    public EntityData[] processKinTypeStrings(URI[] egoNodes, String[] kinTypeStrings, ParserHighlight[] parserHighlight, IndexerParameters indexParameters, DataStoreSvg dataStoreSvg, JProgressBar progressBar) throws EntityServiceException;
+    public EntityData[] processKinTypeStrings(URI[] egoNodes, String[] kinTypeStrings, ParserHighlight[] parserHighlight, IndexerParameters indexParameters, DataStoreSvg dataStoreSvg, JProgressBar progressBar) throws EntityServiceException, ProcessAbortException;
 }
