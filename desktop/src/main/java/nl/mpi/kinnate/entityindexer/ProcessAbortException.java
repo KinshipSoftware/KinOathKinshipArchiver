8e8ec2be563e4b6cefac4b6748c17c5e7f0b79e4
Peter Withers <Peter.Withers@mpi.nl>
2011-11-07 12:33:26 +0000

    Modified the mouse wheel zoom to prevent negative zoom (upside down). Updated the metadata panel so that one table contains any entity data while another table (with tree) show any linked archive data/metadata. Added Swedish Kin Terms sample. Tidied up the kin terms table and the menu that adds/shows the kin terms groups. Added an abort query process so that queries with large time consuming results can be stopped mid process as the user types in new queries.

---
+++ desktop/src/main/java/nl/mpi/kinnate/entityindexer/ProcessAbortException.java
@@ -0,0 +1,9 @@
+package nl.mpi.kinnate.entityindexer;
+
+/**
+ *  Document   : ProcessAbortException
+ *  Created on : Nov 4, 2011, 5:09:37 PM
+ *  Author     : Peter Withers
+ */
+public class ProcessAbortException extends Exception {
+}
