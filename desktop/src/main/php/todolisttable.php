dc7c9f2df7dc86de7b9b3d364fd3f5925f68a292
Peter Withers <Peter.Withers@mpi.nl>
2011-11-08 14:28:22 +0000

    Updated the table layout of the todo list when viewed from plone.

--- desktop/src/main/php/todolisttable.php
+++ desktop/src/main/php/todolisttable.php
@@ -1,10 +1,10 @@
 <html>
     <body>
-        <b>Current Known Issues and Requests</b>
+        <!-- <b>Current Known Issues and Requests</b> -->
         <table>
             <?php
             $csvFile = fopen("kinoath-all_todo.csv", "r");
-            while (($line = fgetcsv($csvFile)) !== false) {
+            while (($line = fgetcsv($csvFile, 1000, ",")) !== false) {
                 echo "<tr>";
                 foreach ($line as $cell) {
                     echo "<td>" . htmlspecialchars($cell) . "</td>";
@@ -15,5 +15,4 @@
             php ?>
         </table>
     </body>
-</html>";
-
+</html>
