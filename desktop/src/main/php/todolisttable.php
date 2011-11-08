535b0af2a74dce75f1cc5f569cb9b0c31c3f8744
Peter Withers <Peter.Withers@mpi.nl>
2011-11-08 13:50:09 +0000

    Added to the ant installer build file a target that gets from trac the current active tasks and uploads them to the server as a csv file.

---
+++ desktop/src/main/php/todolisttable.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <b>Current Known Issues and Requests</b>
+        <table>
+            <?php
+            $csvFile = fopen("kinoath-all_todo.csv", "r");
+            while (($line = fgetcsv($csvFile)) !== false) {
+                echo "<tr>";
+                foreach ($line as $cell) {
+                    echo "<td>" . htmlspecialchars($cell) . "</td>";
+                }
+                echo "<tr>\n";
+            }
+            fclose($csvFile);
+            php ?>
+        </table>
+    </body>
+</html>";
+
