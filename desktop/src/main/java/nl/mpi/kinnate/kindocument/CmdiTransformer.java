c10070605293f0d0fef77db3f634525f159a7b1a
Peter Withers <Peter.Withers@mpi.nl>
2011-11-17 15:30:31 +0000

    Started to add a clarin bridge schema so that clarin profiles can be used for entity metadata. Much work on this is still required.

--- desktop/src/main/java/nl/mpi/kinnate/kindocument/CmdiTransformer.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/CmdiTransformer.java
@@ -9,6 +9,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
@@ -27,53 +28,25 @@ import nl.mpi.arbil.util.ArbilBugCatcher;
  */
 public class CmdiTransformer {
 
-    private String component2SchemaXsl = "http://www.clarin.eu/cmd/xslt/comp2schema-v2/comp2schema.xsl";
+    private URL component2SchemaXsl = this.getClass().getResource("/xsd/comp2schema.xsl"); // "http://www.clarin.eu/cmd/xslt/comp2schema-v2/comp2schema.xsl";
 
     private void transformProfileXmlToXsd(String profileId, String entityType) throws IOException, TransformerException {
         String cmdiProfileXmlUrl = "http://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/profiles/" + profileId + "/xml";
-        File outputFile = new File(ArbilSessionStorage.getSingleInstance().getCacheDirectory(), entityType);
+        File outputFile = new File(ArbilSessionStorage.getSingleInstance().getCacheDirectory(), entityType + ".xsd");
         System.out.println("outputFile: " + outputFile.getAbsolutePath());
 
 //        System.out.println(ArbilSessionStorage.getSingleInstance().updateCache("http://www.clarin.eu/cmd/xslt/comp2schema-v2/comp2schema-header.xsl", 1));
 //        System.out.println(ArbilSessionStorage.getSingleInstance().updateCache("http://www.clarin.eu/cmd/xslt/comp2schema-v2/comp2schema.xsl", 1));
+//        System.out.println(ArbilSessionStorage.getSingleInstance().updateCache("http://www.clarin.eu/cmd/xslt/comp2schema-v2/cleanup-xsd.xsl", 1));
 
         generateXsd(cmdiProfileXmlUrl, outputFile);
-        return;
-
-//        // 1. Instantiate a TransformerFactory.
-//        javax.xml.transform.TransformerFactory tFactory = javax.xml.transform.TransformerFactory.newInstance();
-//        // 2. Use the TransformerFactory to process the stylesheet Source and generate a Transformer.
-//        // URL xslUrl = this.getClass().getResource("/nl/mpi/arbil/resources/xsl/imdi-viewer.xsl");
-//        URL xslUrl = new URL(component2SchemaXsl);
-//        // look in the current template for a custom xsl
-////	File xslFile = null;
-////	xslFile = new File(inputNode.getNodeTemplate().getTemplateDirectory(), "format.xsl");
-////	if (xslFile != null && xslFile.exists()) {
-////	    xslUrl = xslFile.toURL();
-////	}
-//        javax.xml.transform.Transformer transformer = tFactory.newTransformer(new javax.xml.transform.stream.StreamSource(xslUrl.toString()));
-//        // 3. Use the Transformer to transform an XML Source and send the output to a Result object.
-//        transformer.transform(new javax.xml.transform.stream.StreamSource(cmdiProfileXmlUrl), new javax.xml.transform.stream.StreamResult(new java.io.FileOutputStream(outputFile)));
     }
-//    public static <T> void marshal(T marshallableObject, OutputStream out) throws JAXBException, UnsupportedEncodingException {
-//	String packageName = marshallableObject.getClass().getPackage().getName();
-//	JAXBContext jc = JAXBContext.newInstance(packageName);
-//
-//	Marshaller m = jc.createMarshaller();
-//	m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-//	String schemaLocation = Configuration.getInstance().getSchemaLocation(marshallableObject.getClass().getName());
-//	if (schemaLocation != null) {
-//	    m.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, schemaLocation);
-//	}
-//	Writer writer = new OutputStreamWriter(out, "UTF-8");
-//	m.marshal(marshallableObject, writer);
-//    }
 
     public void generateXsd(String cmdiProfileXmlUrl, File outputFile) {
         Templates componentToSchemaTemplates;
         try {
             System.setProperty("javax.xml.transform.TransformerFactory", net.sf.saxon.TransformerFactoryImpl.class.getName());
-            componentToSchemaTemplates = TransformerFactory.newInstance().newTemplates(new StreamSource(component2SchemaXsl));
+            componentToSchemaTemplates = TransformerFactory.newInstance().newTemplates(new StreamSource(component2SchemaXsl.toString()));
         } catch (TransformerConfigurationException e) {
             new ArbilBugCatcher().logError("Cannot create Template", e);
             return;
@@ -88,8 +61,6 @@ public class CmdiTransformer {
             new ArbilBugCatcher().logError("Cannot create Transformer", e);
         } catch (TransformerException e) {
             new ArbilBugCatcher().logError("Cannot transform xml file: ", e);
-//        } catch (UnsupportedEncodingException e) {
-//            new ArbilBugCatcher().logError("Error in encoding: ", e);
         } catch (FileNotFoundException e) {
             new ArbilBugCatcher().logError(e);
         }
@@ -97,7 +68,7 @@ public class CmdiTransformer {
 
     static public void main(String[] args) {
         try {
-            new CmdiTransformer().transformProfileXmlToXsd("clarin.eu:cr1:p_1320657629627", "Individual");
+            new CmdiTransformer().transformProfileXmlToXsd("clarin.eu:cr1:p_1320657629627", "individual");
         } catch (IOException exception) {
             System.out.println("exception: " + exception.getMessage());
         } catch (TransformerException exception) {
