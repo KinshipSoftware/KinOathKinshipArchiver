297d277aac1d64e1021becc9b8a709d7b2df486b
Peter Withers <Peter.Withers@mpi.nl>
2011-11-21 15:32:21 +0000

    Started to add a clarin bridge schema so that clarin profiles can be used for entity metadata. Much work on this is still required.

---
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/KinXsdException.java
@@ -0,0 +1,9 @@
+package nl.mpi.kinnate.kindocument;
+
+/**
+ *  Document   : KinXsdException
+ *  Created on : Nov 21, 2011, 3:54:27 PM
+ *  Author     : Peter Withers
+ */
+public class KinXsdException extends Exception {
+}
