6f601de53c0664e6f9fb5cfb1e88f80777ef4ee0
Peter Withers <Peter.Withers@mpi.nl>
2011-11-28 15:25:54 +0000

    Resolved the double lines issue for siblings seen on a diagram used by Joe.

--- desktop/src/main/java/nl/mpi/kinnate/svg/RelationSvg.java
+++ desktop/src/main/java/nl/mpi/kinnate/svg/RelationSvg.java
@@ -58,15 +58,15 @@ public class RelationSvg {
         ArrayList<Point> initialPointsList = new ArrayList<Point>();
         float midSpacing = vSpacing / 2;
 //        float parentSpacing = 10;
-        if (averageParent == null) {
-            // if no parent location has been provided then just use the current parent
-            averageParent = new float[]{alterX, alterY};
-        }
         float egoYmid;
         float alterYmid;
         float centerX = (egoX + alterX) / 2;
         switch (relationType) {
             case ancestor:
+                if (averageParent == null) {
+                    // if no parent location has been provided then just use the current parent
+                    averageParent = new float[]{alterX, alterY};
+                }
                 egoYmid = egoY - midSpacing;
                 alterYmid = averageParent[1] + 30;
 //                alterYmid = alterY + midSpacing;
@@ -87,7 +87,7 @@ public class RelationSvg {
 //                centerX = (egoYmid < alterYmid) ? alterX : centerX;
 //                centerX = (egoY > alterY && egoX == alterX) ? centerX - midSpacing : centerX;
 //                break;
-            case sibling:
+            case sibling:                
                 egoYmid = egoY - midSpacing;
                 alterYmid = alterY - midSpacing;
                 centerX = (egoY < alterY) ? alterX : egoX;
