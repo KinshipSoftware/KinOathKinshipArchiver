3a2dc7d0890d56eb586a28398a68974822a77231
Peter Withers <Peter.Withers@mpi.nl>
2011-12-05 15:25:56 +0000

    Separated the file menu from the MainFrame class. Created a diagram window manager class and moved relevant code out of the MainFrame into the new class. Added a check for unsaved diagrams on application close. Separated out the edit menu into a different class.

--- desktop/src/main/java/nl/mpi/kinnate/ui/MainFrame.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/MainFrame.java
@@ -1,6 +1,6 @@
 package nl.mpi.kinnate.ui;
 
-import java.awt.BorderLayout;
+import javax.swing.JMenu;
 import nl.mpi.arbil.ArbilIcons;
 import nl.mpi.arbil.ui.ArbilWindowManager;
 import nl.mpi.arbil.util.ApplicationVersionManager;
@@ -19,11 +19,14 @@ public class MainFrame extends javax.swing.JFrame {
     public MainFrame() {
         initComponents();
         diagramWindowManager = new DiagramWindowManager(this);
+        JMenu editMenu;
+        editMenu = new EditMenu();
         ((EditMenu) editMenu).enableMenuKeys();
         final ApplicationVersionManager versionManager = new ApplicationVersionManager(new KinOathVersion());
         nl.mpi.kinnate.KinnateArbilInjector.injectHandlers(versionManager);
         diagramWindowManager.newDiagram();
         jMenuBar1.add(new FileMenu(diagramWindowManager));
+        jMenuBar1.add(editMenu);
         jMenuBar1.add(new DiagramPanelsMenu(diagramWindowManager));
         jMenuBar1.add(new KinTermsMenu(diagramWindowManager));
         jMenuBar1.add(new ArchiveMenu(diagramWindowManager));
@@ -52,41 +55,12 @@ public class MainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jMenuBar1 = new javax.swing.JMenuBar();
-        editMenu = new EditMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        editMenu.setText("Edit");
-        editMenu.addMenuListener(new javax.swing.event.MenuListener() {
-            public void menuSelected(javax.swing.event.MenuEvent evt) {
-                editMenuMenuSelected(evt);
-            }
-            public void menuDeselected(javax.swing.event.MenuEvent evt) {
-            }
-            public void menuCanceled(javax.swing.event.MenuEvent evt) {
-            }
-        });
-        editMenu.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                editMenuActionPerformed(evt);
-            }
-        });
-        jMenuBar1.add(editMenu);
-
         setJMenuBar(jMenuBar1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void editMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editMenuActionPerformed
-    }//GEN-LAST:event_editMenuActionPerformed
-
-    private void editMenuMenuSelected(javax.swing.event.MenuEvent evt) {//GEN-FIRST:event_editMenuMenuSelected
-//        // init the menu and pass it the current save panel
-////        int tabIndex = Integer.valueOf(jTabbedPane1.getSelectedIndex());
-////        SavePanel savePanel = getSavePanel(tabIndex);
-// todo: pass the current tab selection to the menu         ((EditMenu) editMenu).initMenu();
-    }//GEN-LAST:event_editMenuMenuSelected
 
     /**
      * @param args the command line arguments
@@ -102,7 +76,6 @@ public class MainFrame extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu editMenu;
     private javax.swing.JMenuBar jMenuBar1;
     // End of variables declaration//GEN-END:variables
 }
