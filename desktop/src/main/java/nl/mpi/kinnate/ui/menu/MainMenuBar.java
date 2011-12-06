671db498e272c417e542c321bdcdb566687468e3
Peter Withers <Peter.Withers@mpi.nl>
2011-12-06 18:33:26 +0000

    Separated the file menu from the MainFrame class. Created a diagram window manager class and moved relevant code out of the MainFrame into the new class. Added a check for unsaved diagrams on application close. Separated out the edit menu into a different class. Updated the import data menu. Updated the edit menu to use the SavePanel and better handle hot keys. Completed the code to expand the diagram selection to include immediate relations and enabled its menu item in the edit menu. Refactored the menu classes. Created an abstract diagram manager class so that the tabbed pane can be optionally replaced by separate windows or a jdesktop pane. Added a window select menu for when the tab pane is not used. Set the window tiles for non tabbed diagram application frames. Added diagram sub panels for import results which use tabbed panes inside the outer window for the generic panel. Added a windowed version of the window manager.

---
+++ desktop/src/main/java/nl/mpi/kinnate/ui/menu/MainMenuBar.java
@@ -0,0 +1,21 @@
+package nl.mpi.kinnate.ui.menu;
+
+import javax.swing.JMenuBar;
+import nl.mpi.kinnate.ui.window.AbstractDiagramManager;
+
+/**
+ *  Document   : MainMenuBar
+ *  Created on : Dec 6, 2011, 7:26:07 PM
+ *  Author     : Peter Withers
+ */
+public class MainMenuBar extends JMenuBar {
+
+    public MainMenuBar(AbstractDiagramManager abstractDiagramManager) {
+        this.add(new FileMenu(abstractDiagramManager));
+        this.add(new EditMenu(abstractDiagramManager));
+        this.add(new DiagramPanelsMenu(abstractDiagramManager));
+        this.add(new KinTermsMenu(abstractDiagramManager));
+        this.add(new ArchiveMenu(abstractDiagramManager));
+        this.add(new WindowMenu(abstractDiagramManager));
+    }
+}
