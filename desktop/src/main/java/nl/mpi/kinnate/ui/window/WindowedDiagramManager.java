623de6589f9019c0dddb4808531725fa5ee72199
Peter Withers <Peter.Withers@mpi.nl>
2011-12-06 17:00:07 +0000

    Separated the file menu from the MainFrame class. Created a diagram window manager class and moved relevant code out of the MainFrame into the new class. Added a check for unsaved diagrams on application close. Separated out the edit menu into a different class. Updated the import data menu. Updated the edit menu to use the SavePanel and better handle hot keys. Completed the code to expand the diagram selection to include immediate relations and enabled its menu item in the edit menu. Refactored the menu classes. Created an abstract diagram manager class so that the tabbed pane can be optionally replaced by separate windows or a jdesktop pane. Added a window select menu for when the tab pane is not used. Set the window tiles for non tabbed diagram application frames. Added diagram sub panels for import results which use tabbed panes inside the outer window for the generic panel.

---
+++ desktop/src/main/java/nl/mpi/kinnate/ui/window/WindowedDiagramManager.java
@@ -0,0 +1,118 @@
+package nl.mpi.kinnate.ui.window;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import nl.mpi.arbil.util.ApplicationVersionManager;
+
+/**
+ *  Document   : DiagramWindowManager
+ *  Created on : Dec 1, 2011, 4:03:01 PM
+ *  Author     : Peter Withers
+ */
+public class WindowedDiagramManager extends AbstractDiagramManager {
+
+    private javax.swing.JPanel mainPanel;
+    private HashMap<Component, String> titleMap = new HashMap<Component, String>();
+    private ArrayList<Component> diagramArray = new ArrayList<Component>();
+    private JFrame mainFrame;
+
+    public WindowedDiagramManager(ApplicationVersionManager versionManager, JFrame mainFrame) {
+        super(versionManager, mainFrame);
+        this.mainFrame = mainFrame;
+        mainPanel = new javax.swing.JPanel(new BorderLayout());
+        mainFrame.add(mainPanel, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void createDiagramContainer(String diagramTitle, Component diagramComponent) {
+        titleMap.put(diagramComponent, diagramTitle);
+        diagramArray.add(diagramComponent);
+        setSelectedDiagram(diagramComponent);
+    }
+
+    @Override
+    public void createDiagramSubPanel(String diagramTitle, Component diagramComponent) {
+        int currentDiagramIndex = getSavePanelIndex();
+        Component currentComponent = getDiagramAt(currentDiagramIndex);
+        JTabbedPane tabbedPane;
+        if (!(currentComponent instanceof JTabbedPane)) {
+            tabbedPane = new JTabbedPane();
+            final String savePanelTitle = getSavePanelTitle(currentDiagramIndex);
+            tabbedPane.addTab(savePanelTitle, currentComponent);
+            titleMap.remove(currentComponent);
+            titleMap.put(tabbedPane, savePanelTitle);
+            diagramArray.set(currentDiagramIndex, tabbedPane);
+            setSelectedDiagram(currentDiagramIndex);
+        } else {
+            tabbedPane = (JTabbedPane) currentComponent;
+        }
+        tabbedPane.addTab(diagramTitle, diagramComponent);
+    }
+
+    @Override
+    Component getSelectedDiagram() {
+        if (mainPanel.getComponents().length > 0) {
+            return mainPanel.getComponent(0);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void setSelectedDiagram(Component diagramComponent) {
+        mainPanel.removeAll();
+        String diagramTitle = null;
+        if (diagramComponent != null) {
+            mainPanel.add(diagramComponent, BorderLayout.CENTER);
+            diagramTitle = titleMap.get(diagramComponent);
+        }
+        setWindowTitle(mainFrame, diagramTitle);
+        mainPanel.revalidate();
+        mainPanel.repaint();
+    }
+
+    @Override
+    public void setSelectedDiagram(int diagramIndex) {
+        if (diagramArray.size() > diagramIndex) {
+            setSelectedDiagram(diagramArray.get(diagramIndex));
+        } else {
+            setSelectedDiagram(null);
+        }
+    }
+
+    public int getSavePanelIndex() {
+        return diagramArray.indexOf(getSelectedDiagram());
+    }
+
+    public String getSavePanelTitle(int selectedIndex) {
+        return titleMap.get(diagramArray.get(selectedIndex));
+    }
+
+    @Override
+    Component getDiagramAt(int diagramIndex) {
+        return diagramArray.get(diagramIndex);
+    }
+
+    public void closeSavePanel(int selectedIndex) {
+        titleMap.remove(diagramArray.get(selectedIndex));
+        diagramArray.remove(selectedIndex);
+        while (diagramArray.size() <= selectedIndex && selectedIndex > 0) {
+            selectedIndex--;
+        }
+        setSelectedDiagram(selectedIndex);
+    }
+
+    public void setDiagramTitle(int diagramIndex, String diagramTitle) {
+        titleMap.put(getSelectedDiagram(), diagramTitle);
+        setWindowTitle(mainFrame, diagramTitle);
+    }
+
+    @Override
+    public Component[] getAllDiagrams() {
+        return diagramArray.toArray(new Component[]{});
+    }
+}
