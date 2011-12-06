8268da57ae9fff5049ed1772d89c0f1cc0e7f86d
Peter Withers <Peter.Withers@mpi.nl>
2011-12-06 13:11:48 +0000

    Separated the file menu from the MainFrame class. Created a diagram window manager class and moved relevant code out of the MainFrame into the new class. Added a check for unsaved diagrams on application close. Separated out the edit menu into a different class. Updated the import data menu. Updated the edit menu to use the SavePanel and better handle hot keys. Completed the code to expand the diagram selection to include immediate relations and enabled its menu item in the edit menu. Refactored the menu classes. Created an abstract diagram manager class so that the tabbed pane can be optionally replaced by separate windows or a jdesktop pane. Added a window select menu for when the tab pane is not used.

---
+++ desktop/src/main/java/nl/mpi/kinnate/ui/window/LayeredDiagramManager.java
@@ -0,0 +1,68 @@
+package nl.mpi.kinnate.ui.window;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.JFrame;
+
+/**
+ *  Document   : DiagramWindowManager
+ *  Created on : Dec 1, 2011, 4:03:01 PM
+ *  Author     : Peter Withers
+ */
+public class LayeredDiagramManager extends AbstractDiagramManager {
+
+    private javax.swing.JTabbedPane jTabbedPane1;
+
+    public LayeredDiagramManager(JFrame mainFrame) {
+        super(mainFrame);
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        mainFrame.add(jTabbedPane1, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void createDiagramContainer(String diagramTitle, Component diagramComponent) {
+        jTabbedPane1.add(diagramTitle, diagramComponent);
+        jTabbedPane1.setSelectedComponent(diagramComponent);
+    }
+
+    @Override
+    Component getSelectedDiagram() {
+        return jTabbedPane1.getSelectedComponent();
+    }
+
+    @Override
+    public void setSelectedDiagram(Component diagramComponent) {
+        jTabbedPane1.setSelectedComponent(diagramComponent);
+    }
+
+    @Override
+    public void setSelectedDiagram(int diagramIndex) {
+        jTabbedPane1.setSelectedIndex(diagramIndex);
+    }
+
+    public int getSavePanelIndex() {
+        return jTabbedPane1.getSelectedIndex();
+    }
+
+    public String getSavePanelTitle(int selectedIndex) {
+        return jTabbedPane1.getTitleAt(selectedIndex);
+    }
+
+    @Override
+    Component getDiagramAt(int diagramIndex) {
+        return jTabbedPane1.getComponentAt(diagramIndex);
+    }
+
+    public void closeSavePanel(int selectedIndex) {
+        jTabbedPane1.remove(selectedIndex);
+    }
+
+    public void setDiagramTitle(int diagramIndex, String diagramTitle) {
+        jTabbedPane1.setTitleAt(diagramIndex, diagramTitle);
+    }
+
+    @Override
+    public Component[] getAllDiagrams() {
+        return jTabbedPane1.getComponents();
+    }
+}
