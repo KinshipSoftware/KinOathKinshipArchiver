4ca622d1a68355a17c1e6ff0eb3391ecc03341e1
Peter Withers <Peter.Withers@mpi.nl>
2011-12-06 11:23:56 +0000

    Separated the file menu from the MainFrame class. Created a diagram window manager class and moved relevant code out of the MainFrame into the new class. Added a check for unsaved diagrams on application close. Separated out the edit menu into a different class. Updated the import data menu. Updated the edit menu to use the SavePanel and better handle hot keys. Completed the code to expand the diagram selection to include immediate relations and enabled its menu item in the edit menu. Refactored the menu classes.

---
+++ desktop/src/main/java/nl/mpi/kinnate/ui/menu/ArchiveMenu.java
@@ -0,0 +1,106 @@
+package nl.mpi.kinnate.ui.menu;
+
+import nl.mpi.kinnate.ui.window.DiagramWindowManager;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import nl.mpi.kinnate.KinTermSavePanel;
+import nl.mpi.kinnate.kindata.VisiblePanelSetting.PanelType;
+
+/**
+ *  Document   : KinTermsMenu
+ *  Created on : Apr 1, 2011, 9:32:40 AM
+ *  Author     : Peter Withers
+ */
+public class ArchiveMenu extends JMenu {
+
+    DiagramWindowManager diagramWindowManager;
+    KinTermSavePanel currentKinTermSavePanel;
+    JCheckBoxMenuItem hideShowMenu;
+    JMenuItem showRemoteTreeMenu;
+    JMenuItem showLocalTreeMenu;
+    JMenu showResultsMenu;
+    JMenu closeResultsMenu;
+
+    public ArchiveMenu(DiagramWindowManager diagramWindowManager) {
+        this.diagramWindowManager = diagramWindowManager;
+        initMenu();
+    }
+
+    public ArchiveMenu() {
+        initMenu();
+    }
+
+    private void initMenu() {
+        hideShowMenu = new JCheckBoxMenuItem("Show");
+        showRemoteTreeMenu = new JMenuItem("Archive Tree");
+        showLocalTreeMenu = new JMenuItem("Local Tree");
+        closeResultsMenu = new JMenu("Close Results");
+        showResultsMenu = new JMenu("Show Results");
+        this.setText("Archive Linker");
+        this.add(hideShowMenu);
+        this.add(showRemoteTreeMenu);
+        this.add(showLocalTreeMenu);
+        this.add(closeResultsMenu);
+        this.add(showResultsMenu);
+        this.addMenuListener(new javax.swing.event.MenuListener() {
+
+            public void menuSelected(javax.swing.event.MenuEvent evt) {
+                // todo: check if a kin term pane is showing or associated with the diagram and that a diagam is showing
+                currentKinTermSavePanel = diagramWindowManager.getKinTermPanel();
+                if (currentKinTermSavePanel != null) {
+//                    hideShowMenu.setEnabled(true);
+                    if (currentKinTermSavePanel.getPanelState(PanelType.ArchiveLinker)) {
+//                        exportMenu.setEnabled(false);
+//                        importMenu.setEnabled(false);
+//                        newMenu.setEnabled(false);
+//                        deleteMenu.setEnabled(false);
+                        hideShowMenu.setSelected(true);
+                    } else {
+//                        exportMenu.setEnabled(true);
+//                        importMenu.setEnabled(true);
+//                        newMenu.setEnabled(true);
+//                        deleteMenu.setEnabled(false); // todo: enable deleting the current kin term group and update the menu to reflect the group name that would be deleted
+                        hideShowMenu.setSelected(false);
+                    }
+                } else {
+//                    exportMenu.setEnabled(false);
+//                    importMenu.setEnabled(false);
+//                    hideShowMenu.setEnabled(false);
+//                    newMenu.setEnabled(false);
+//                    deleteMenu.setEnabled(false);
+                }
+            }
+
+            public void menuDeselected(javax.swing.event.MenuEvent evt) {
+            }
+
+            public void menuCanceled(javax.swing.event.MenuEvent evt) {
+            }
+        });
+        hideShowMenu.addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                currentKinTermSavePanel.setPanelState(PanelType.ArchiveLinker, hideShowMenu.isSelected());
+            }
+        });
+//        exportMenu.addActionListener(new java.awt.event.ActionListener() {
+//
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                currentKinTermSavePanel.exportKinTerms();
+//            }
+//        });
+//        importMenu.addActionListener(new java.awt.event.ActionListener() {
+//
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                currentKinTermSavePanel.importKinTerms();
+//            }
+//        });
+//        newMenu.addActionListener(new java.awt.event.ActionListener() {
+//
+//            public void actionPerformed(java.awt.event.ActionEvent evt) {
+//                currentKinTermSavePanel.addKinTermGroup();
+//            }
+//        });
+    }
+}
