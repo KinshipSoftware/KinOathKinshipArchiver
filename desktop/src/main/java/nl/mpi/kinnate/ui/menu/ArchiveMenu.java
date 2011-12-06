70d60b81ff8cfb90d4006eb875663f107a21f0d6
Peter Withers <Peter.Withers@mpi.nl>
2011-12-06 13:51:20 +0000

    Separated the file menu from the MainFrame class. Created a diagram window manager class and moved relevant code out of the MainFrame into the new class. Added a check for unsaved diagrams on application close. Separated out the edit menu into a different class. Updated the import data menu. Updated the edit menu to use the SavePanel and better handle hot keys. Completed the code to expand the diagram selection to include immediate relations and enabled its menu item in the edit menu. Refactored the menu classes. Created an abstract diagram manager class so that the tabbed pane can be optionally replaced by separate windows or a jdesktop pane. Added a window select menu for when the tab pane is not used.

--- desktop/src/main/java/nl/mpi/kinnate/ui/menu/ArchiveMenu.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/menu/ArchiveMenu.java
@@ -1,11 +1,11 @@
 package nl.mpi.kinnate.ui.menu;
 
-import nl.mpi.kinnate.ui.window.DiagramWindowManager;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import nl.mpi.kinnate.KinTermSavePanel;
 import nl.mpi.kinnate.kindata.VisiblePanelSetting.PanelType;
+import nl.mpi.kinnate.ui.window.AbstractDiagramManager;
 
 /**
  *  Document   : KinTermsMenu
@@ -14,7 +14,7 @@ import nl.mpi.kinnate.kindata.VisiblePanelSetting.PanelType;
  */
 public class ArchiveMenu extends JMenu {
 
-    DiagramWindowManager diagramWindowManager;
+    AbstractDiagramManager diagramWindowManager;
     KinTermSavePanel currentKinTermSavePanel;
     JCheckBoxMenuItem hideShowMenu;
     JMenuItem showRemoteTreeMenu;
@@ -22,7 +22,7 @@ public class ArchiveMenu extends JMenu {
     JMenu showResultsMenu;
     JMenu closeResultsMenu;
 
-    public ArchiveMenu(DiagramWindowManager diagramWindowManager) {
+    public ArchiveMenu(AbstractDiagramManager diagramWindowManager) {
         this.diagramWindowManager = diagramWindowManager;
         initMenu();
     }
