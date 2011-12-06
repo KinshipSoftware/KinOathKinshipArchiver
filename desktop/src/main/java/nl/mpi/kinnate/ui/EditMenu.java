fc24fd60b9eede3a731cbfff470a4d49604305bd
Peter Withers <Peter.Withers@mpi.nl>
2011-12-06 09:30:14 +0000

    Separated the file menu from the MainFrame class. Created a diagram window manager class and moved relevant code out of the MainFrame into the new class. Added a check for unsaved diagrams on application close. Separated out the edit menu into a different class. Updated the import data menu. Updated the edit menu to use the SavePanel and better handle hot keys.

--- desktop/src/main/java/nl/mpi/kinnate/ui/EditMenu.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/EditMenu.java
@@ -1,14 +1,12 @@
 package nl.mpi.kinnate.ui;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.FocusManager;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
-import nl.mpi.kinnate.svg.GraphPanel;
+import nl.mpi.kinnate.SavePanel;
 import nl.mpi.kinnate.svg.MouseListenerSvg;
 
 /**
@@ -26,9 +24,11 @@ public class EditMenu extends JMenu implements ActionListener {
     JMenuItem selectRelatedMenu = null;
     JMenuItem expandSelectionMenu = null;
     JMenuItem deselectAllMenu = null;
-    GraphPanel initialGraphPanel = null;
+    SavePanel menuSavePanel = null;
+    DiagramWindowManager diagramWindowManager;
 
-    public EditMenu() {
+    public EditMenu(DiagramWindowManager diagramWindowManager) {
+        this.diagramWindowManager = diagramWindowManager;
         this.setText("Edit");
         this.addMenuListener(new MenuListener() {
 
@@ -37,78 +37,77 @@ public class EditMenu extends JMenu implements ActionListener {
             }
 
             public void menuDeselected(MenuEvent evt) {
-//                enableMenuKeys();
+                enableMenuKeys();
             }
 
             public void menuSelected(MenuEvent evt) {
                 initMenu();
             }
         });
+        selectAllMenu = new JMenuItem("Select All");
+        selectAllMenu.setActionCommand(MouseListenerSvg.ActionCode.selectAll.name());
+        selectAllMenu.addActionListener(EditMenu.this);
+        selectAllMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
+        selectRelatedMenu = new JMenuItem("Select Related");
+        selectRelatedMenu.setActionCommand(MouseListenerSvg.ActionCode.selectRelated.name());
+        selectRelatedMenu.addActionListener(EditMenu.this);
+        selectRelatedMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_MASK));
+        expandSelectionMenu = new JMenuItem("Expand Selection");
+        expandSelectionMenu.setActionCommand(MouseListenerSvg.ActionCode.expandSelection.name());
+        expandSelectionMenu.addActionListener(EditMenu.this);
+        expandSelectionMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_E, java.awt.event.InputEvent.CTRL_MASK));
+
+        deselectAllMenu = new JMenuItem("Deselect All");
+        deselectAllMenu.setActionCommand(MouseListenerSvg.ActionCode.deselectAll.name());
+        deselectAllMenu.addActionListener(EditMenu.this);
+        deselectAllMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.event.InputEvent.CTRL_MASK));
+        EditMenu.this.add(selectAllMenu);
+        EditMenu.this.add(selectRelatedMenu);
+        EditMenu.this.add(expandSelectionMenu);
+        EditMenu.this.add(deselectAllMenu);
     }
 
     private void initMenu() {
-        initialGraphPanel = getGraphPanel();
-        boolean graphPanelFocused = initialGraphPanel != null;
-        selectAllMenu.setEnabled(graphPanelFocused);
-        selectRelatedMenu.setEnabled(graphPanelFocused);
+        menuSavePanel = diagramWindowManager.getCurrentSavePanel();
+        boolean savePanelFocused = menuSavePanel != null;
+        selectAllMenu.setEnabled(savePanelFocused);
+        selectRelatedMenu.setEnabled(savePanelFocused);
         expandSelectionMenu.setEnabled(false); // graphPanelFocused); // removed until the code is completed
-        deselectAllMenu.setEnabled(graphPanelFocused);
+        deselectAllMenu.setEnabled(savePanelFocused);
     }
 
     protected void enableMenuKeys() {
-        initialGraphPanel = null;
-        if (selectAllMenu == null) {
-            selectAllMenu = new JMenuItem("Select All");
-            selectAllMenu.setActionCommand(MouseListenerSvg.ActionCode.selectAll.name());
-            selectAllMenu.addActionListener(EditMenu.this);
-            selectAllMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
-            selectRelatedMenu = new JMenuItem("Select Related");
-            selectRelatedMenu.setActionCommand(MouseListenerSvg.ActionCode.selectRelated.name());
-            selectRelatedMenu.addActionListener(EditMenu.this);
-            selectRelatedMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_MASK));
-            expandSelectionMenu = new JMenuItem("Expand Selection");
-            expandSelectionMenu.setActionCommand(MouseListenerSvg.ActionCode.expandSelection.name());
-            expandSelectionMenu.addActionListener(EditMenu.this);
-            expandSelectionMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_E, java.awt.event.InputEvent.CTRL_MASK));
-
-            deselectAllMenu = new JMenuItem("Deselect All");
-            deselectAllMenu.setActionCommand(MouseListenerSvg.ActionCode.deselectAll.name());
-            deselectAllMenu.addActionListener(EditMenu.this);
-            deselectAllMenu.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.event.InputEvent.CTRL_MASK));
-            EditMenu.this.add(selectAllMenu);
-            EditMenu.this.add(selectRelatedMenu);
-            EditMenu.this.add(expandSelectionMenu);
-            EditMenu.this.add(deselectAllMenu);
-        }
+        menuSavePanel = null;
         selectAllMenu.setEnabled(true);
         selectRelatedMenu.setEnabled(true);
         expandSelectionMenu.setEnabled(true);
         deselectAllMenu.setEnabled(true);
     }
 
-    private GraphPanel getGraphPanel() {
-        Component focusedComponent = FocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
-        GraphPanel graphPanel = null;
-        while (focusedComponent != null) {
-//            System.out.println("focus on: " + focusedComponent.toString());
-            if (focusedComponent instanceof GraphPanel) {
-//                System.out.println("bingo");
-                graphPanel = (GraphPanel) focusedComponent;
-                break;
-            }
-            focusedComponent = focusedComponent.getParent();
-        }
-        return graphPanel;
-    }
-
+//    private SavePanel getGraphPanel() {
+//        return diagramWindowManager.getCurrentSavePanel();
+//        // todo: this might not be the best way to do this when the tab change event could be used
+//        // todo: this could be casuing issues with the keyboard short cuts when the needed menu items are dissabled
+////        Component focusedComponent = FocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
+////        GraphPanel graphPanel = null;
+////        while (focusedComponent != null) {
+//////            System.out.println("focus on: " + focusedComponent.toString());
+////            if (focusedComponent instanceof GraphPanel) {
+//////                System.out.println("bingo");
+////                graphPanel = (GraphPanel) focusedComponent;
+////                break;
+////            }
+////            focusedComponent = focusedComponent.getParent();
+////        }
+////        return graphPanel;
+//    }
     public void actionPerformed(ActionEvent e) {
-        GraphPanel graphPanel = getGraphPanel();
-        if (graphPanel == null) {
-            //  initialGraphPanel is set before the menu is shown and should never be set if this is a key action 
-            graphPanel = initialGraphPanel;
+        if (menuSavePanel == null) {
+            // if this is a menu action then menuSavePanel was set as the menu was shown, if this is a key event then the current save panel must be abtained
+            menuSavePanel = diagramWindowManager.getCurrentSavePanel();
         }
-        if (graphPanel != null) {
-            graphPanel.mouseListenerSvg.performMenuAction(MouseListenerSvg.ActionCode.valueOf(e.getActionCommand()));
+        if (menuSavePanel != null) {
+            menuSavePanel.doActionCommand(MouseListenerSvg.ActionCode.valueOf(e.getActionCommand()));
         }
         enableMenuKeys();
     }
