b373b90db279c72198bd9c48b4b0f6f4edfe702b
Peter Withers <Peter.Withers@mpi.nl>
2011-12-28 14:18:31 +0000

    Refactored to match recent changes to Arbil so that singletons are not static but are injected via the constructor.

--- desktop/src/main/java/nl/mpi/kinnate/svg/GraphUserAgent.java
+++ desktop/src/main/java/nl/mpi/kinnate/svg/GraphUserAgent.java
@@ -4,8 +4,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import nl.mpi.arbil.data.ArbilDataNode;
 import nl.mpi.arbil.data.ArbilDataNodeLoader;
-import nl.mpi.arbil.ui.ArbilWindowManager;
-import nl.mpi.arbil.ui.GuiHelper;
+import nl.mpi.arbil.util.BugCatcher;
+import nl.mpi.arbil.util.MessageDialogHandler;
 import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 import org.apache.batik.swing.svg.SVGUserAgentGUIAdapter;
 
@@ -16,31 +16,37 @@ import org.apache.batik.swing.svg.SVGUserAgentGUIAdapter;
  */
 public class GraphUserAgent extends SVGUserAgentGUIAdapter {
 
-    GraphPanel graphPanel;
+    private GraphPanel graphPanel;
+    private MessageDialogHandler dialogHandler;
+    private BugCatcher bugCatcher;
+    private ArbilDataNodeLoader dataNodeLoader;
 
-    public GraphUserAgent(GraphPanel parentComponent) {
+    public GraphUserAgent(GraphPanel parentComponent, MessageDialogHandler dialogHandler, BugCatcher bugCatcher, ArbilDataNodeLoader dataNodeLoader) {
         super(parentComponent);
         graphPanel = parentComponent;
+        this.dialogHandler = dialogHandler;
+        this.bugCatcher = bugCatcher;
+        this.dataNodeLoader = dataNodeLoader;
     }
 
     @Override
     public void displayError(String message) {
-        ArbilWindowManager.getSingleInstance().addMessageDialogToQueue(message, "SVG Error");
+        dialogHandler.addMessageDialogToQueue(message, "SVG Error");
     }
 
     @Override
     public void displayError(Exception exception) {
-        GuiHelper.linorgBugCatcher.logError(exception);
+        bugCatcher.logError(exception);
     }
 
     @Override
     public void displayMessage(String message) {
-        ArbilWindowManager.getSingleInstance().addMessageDialogToQueue(message, "SVG Error");
+        dialogHandler.addMessageDialogToQueue(message, "SVG Error");
     }
 
     @Override
     public void showAlert(String message) {
-        ArbilWindowManager.getSingleInstance().addMessageDialogToQueue(message, "SVG Notification");
+        dialogHandler.addMessageDialogToQueue(message, "SVG Notification");
     }
 
     @Override
@@ -48,10 +54,10 @@ public class GraphUserAgent extends SVGUserAgentGUIAdapter {
         graphPanel.metadataPanel.removeAllArbilDataNodeRows();
         try {
             // put link target into the table
-            final ArbilDataNode arbilDataNode = ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, new URI(targetUri));
+            final ArbilDataNode arbilDataNode = dataNodeLoader.getArbilDataNode(null, new URI(targetUri));
             graphPanel.metadataPanel.addArbilDataNode(arbilDataNode);
         } catch (URISyntaxException urise) {
-            GuiHelper.linorgBugCatcher.logError(urise);
+            bugCatcher.logError(urise);
         }
         // set the graph selection
         graphPanel.setSelectedIds(new UniqueIdentifier[]{});
