9cf9dc74c687c8dec9b79ae411bdfdf936f382fb
Peter Withers <Peter.Withers@mpi.nl>
2011-12-28 16:01:56 +0000

    Refactored to match recent changes to Arbil so that singletons are not static but are injected via the constructor.

--- desktop/src/main/java/nl/mpi/kinnate/userstorage/KinSessionStorage.java
+++ desktop/src/main/java/nl/mpi/kinnate/userstorage/KinSessionStorage.java
@@ -1,154 +1,16 @@
 package nl.mpi.kinnate.userstorage;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
-import java.net.URI;
-import java.net.URL;
-import javax.swing.JLabel;
-import nl.mpi.arbil.data.importexport.ShibbolethNegotiator;
 import nl.mpi.arbil.userstorage.ArbilSessionStorage;
-import nl.mpi.arbil.userstorage.SessionStorage;
-import nl.mpi.arbil.util.BugCatcher;
-import nl.mpi.arbil.util.DownloadAbortFlag;
 
 /**
  *  Document   : KinSessionStorage
  *  Created on : Dec 9, 2011, 10:20:43 AM
  *  Author     : Peter Withers
  */
-public class KinSessionStorage implements SessionStorage {
+public class KinSessionStorage extends ArbilSessionStorage {
 
-//    static private SessionStorage singleInstance = null;
-    static synchronized public SessionStorage getSingleInstance() {
-//        if (singleInstance == null) {
-//            singleInstance = new KinSessionStorage();
-////            singleInstance.checkForMultipleStorageDirectories();
-//            HttpURLConnection.setFollowRedirects(false); // how sad it is that this method is static and global, sigh
-//        }
-//        return singleInstance;
-        return ArbilSessionStorage.getSingleInstance();
-    }
-//    private static BugCatcher bugCatcher;
-
-    public static void setBugCatcher(BugCatcher bugCatcherInstance) {
-//        bugCatcher = bugCatcherInstance;
-        ArbilSessionStorage.setBugCatcher(bugCatcherInstance);
-    }
-
-    public void changeCacheDirectory(File preferedCacheDirectory, boolean moveFiles) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File getCacheDirectory() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File getExportPath(String pathString, String destinationDirectory) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File getFavouritesDir() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File getFromCache(String pathString, boolean followRedirect) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
+    @Override
     public String[] getLocationOptions() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public URI getNewArbilFileName(File parentDirectory, String nodeType) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public URI getOriginatingUri(URI locationInCacheURI) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File getSaveLocation(String pathString) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File getStorageDirectory() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File getTypeCheckerConfig() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public boolean isTrackTableSelection() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public boolean isUseLanguageIdInColumnName() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public boolean loadBoolean(String filename, boolean defaultValue) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public Object loadObject(String filename) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public String loadString(String filename) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public String[] loadStringArray(String filename) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public boolean pathIsInFavourites(File fullTestFile) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public boolean pathIsInsideCache(File fullTestFile) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public boolean replaceCacheCopy(String pathString) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void saveBoolean(String filename, boolean storableValue) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void saveObject(Serializable object, String filename) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public boolean saveRemoteResource(URL targetUrl, File destinationFile, ShibbolethNegotiator shibbolethNegotiator, boolean expireCacheCopy, boolean followRedirect, DownloadAbortFlag abortFlag, JLabel progressLabel) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void saveString(String filename, String storableValue) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void saveStringArray(String filename, String[] storableValue) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void setTrackTableSelection(boolean trackTableSelection) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public void setUseLanguageIdInColumnName(boolean useLanguageIdInColumnName) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File updateCache(String pathString, int expireCacheDays, boolean followRedirect) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public File updateCache(String pathString, ShibbolethNegotiator shibbolethNegotiator, boolean expireCacheCopy, boolean followRedirect, DownloadAbortFlag abortFlag, JLabel progressLabel) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return super.getLocationOptions();
     }
 }
