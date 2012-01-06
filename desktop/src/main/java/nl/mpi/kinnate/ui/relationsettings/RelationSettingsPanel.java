081b23e595b3f4f8fd959b7a10d0e1034e0f5405
Peter Withers <Peter.Withers@mpi.nl>
2012-01-06 13:19:15 +0000

    Added a settings panel for the relation type definitions. Added the relation type definitions to the data stored in the svg. Added the DCR to the relation types and its settings UI. Restructured the relation type data structure to suit the recent changes. Added drag handles for each custom relation type. Enabled line colour and width to be displayed based in the relation settings for the diagram. Modified the kin type query to compare all relations for an individual rather than just the first relation. Modified the entity merger and duplicator to consider all relations for an individual rather than just the first relation for each pair.

--- desktop/src/main/java/nl/mpi/kinnate/ui/relationsettings/RelationSettingsPanel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/relationsettings/RelationSettingsPanel.java
@@ -1,12 +1,20 @@
 package nl.mpi.kinnate.ui.relationsettings;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import nl.mpi.kinnate.SavePanel;
 import nl.mpi.kinnate.kindata.DataTypes;
@@ -22,27 +30,54 @@ public class RelationSettingsPanel extends JPanel {
     public RelationSettingsPanel(String panelName, SavePanel savePanel, DataStoreSvg dataStoreSvg) {
         this.setName(panelName);
         this.setLayout(new BorderLayout());
-        JButton deleteButton = new JButton("Delete Selected");
-        JTable kinTypeTable = new JTable(new RelationTypesTableModel(savePanel, dataStoreSvg, deleteButton));
+        final JButton deleteButton = new JButton("Delete Selected");
+        final JColorChooser colourChooser = new JColorChooser();
+        final AbstractColorChooserPanel colourPickerPanel = colourChooser.getChooserPanels()[0];
+        final JTable kinTypeTable = new JTable(new RelationTypesTableModel(savePanel, dataStoreSvg, deleteButton)) {
 
+            @Override
+            public TableCellRenderer getCellRenderer(int row, int column) {
+                if (column == 3 && row < this.getRowCount() - 1) {
+                    final DefaultTableCellRenderer cellRenderer = new DefaultTableCellRenderer() {
+
+                        @Override
+                        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                            JPanel panel = new JPanel();
+                            try {
+                                panel.setBackground(Color.decode(value.toString()));
+                            } catch (NumberFormatException exception) {
+                            }
+                            return panel;
+                        }
+                    };
+                    return cellRenderer;
+                }
+                final TableCellRenderer standardCellRenderer = super.getCellRenderer(row, column);
+                return standardCellRenderer;
+            }
+        };
         TableColumn columnRelationType = kinTypeTable.getColumnModel().getColumn(2);
-//        TableColumn columnLineType = kinTypeTable.getColumnModel().getColumn(2);
 
         JComboBox comboBoxRelationType = new JComboBox();
         for (DataTypes.RelationType relationType : DataTypes.RelationType.values()) {
             comboBoxRelationType.addItem(relationType);
         }
         columnRelationType.setCellEditor(new DefaultCellEditor(comboBoxRelationType));
+        colourChooser.setPreviewPanel(new JPanel());
+        colourChooser.getSelectionModel().addChangeListener(new ChangeListener() {
 
-//        JComboBox comboBoxLineType = new JComboBox();
-//        for (DataTypes.RelationLineType relationLineType : DataTypes.RelationLineType.values()) {
-//            // todo: this is redundant and can be removed
-//            comboBoxLineType.addItem(relationLineType);
-//        }
-//        columnLineType.setCellEditor(new DefaultCellEditor(comboBoxLineType));
+            public void stateChanged(ChangeEvent e) {
+                for (int selectedRow : kinTypeTable.getSelectedRows()) {
+                    if (selectedRow < kinTypeTable.getRowCount() - 1) {
+                        kinTypeTable.getModel().setValueAt("#" + Integer.toHexString(colourChooser.getColor().getRGB()).substring(2), selectedRow, 3);
+                    }
+                }
+            }
+        });
         this.add(new JScrollPane(kinTypeTable), BorderLayout.CENTER);
         JPanel buttonPanel = new JPanel(new BorderLayout());
         buttonPanel.add(deleteButton, BorderLayout.PAGE_START);
+        buttonPanel.add(colourPickerPanel, BorderLayout.CENTER);
         this.add(buttonPanel, BorderLayout.LINE_END);
     }
 }
