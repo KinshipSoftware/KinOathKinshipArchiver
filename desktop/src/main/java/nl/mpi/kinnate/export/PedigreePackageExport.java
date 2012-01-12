3f6bc0032cd47953d5a379482d7cb1a48a8af3a8
Peter Withers <Peter.Withers@mpi.nl>
2012-01-12 10:41:41 +0000

    Updated some sample diagrams to reflect recent changes. Updated the line rendering to reflect recent changes. Replaced the entity relation comparator code. Updated the relation type and custom relation type data structure.

--- desktop/src/main/java/nl/mpi/kinnate/export/PedigreePackageExport.java
+++ desktop/src/main/java/nl/mpi/kinnate/export/PedigreePackageExport.java
@@ -29,7 +29,7 @@ public class PedigreePackageExport {
 
     private UniqueIdentifier getFirstMatchingParent(EntityData entityData, EntityData.SymbolType symbolType) {
         for (EntityRelation entityRelation : entityData.getAllRelations()) {
-            if (entityRelation.relationType.equals(DataTypes.RelationType.ancestor)) {
+            if (entityRelation.getRelationType().equals(DataTypes.RelationType.ancestor)) {
                 if (entityRelation.getAlterNode().getSymbolType().equals(symbolType.name())) {
                     return entityRelation.getAlterNode().getUniqueIdentifier();
                 }
