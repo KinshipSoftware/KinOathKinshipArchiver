28ed7037dd0d10d9cbf6ded52c4f3052fd728368
Peter Withers <Peter.Withers@mpi.nl>
2012-01-17 14:41:34 +0000

    Changed the working directory from .arbil to .kinoath and add the major, minor version numbers to the working directory name so that each minor version requires an export import operation allowing the internal data structure to be changed. When the internal data structure is stable the minor version can be replaced with an x so that the directory does not change. Exporting will require the use of the old version of the application and this could be achieved by creating a jnlp for the old jars and an export dialog instead of the main application. Resolves ticket #1305

--- desktop/src/main/java/nl/mpi/kinnate/userstorage/KinSessionStorage.java
+++ desktop/src/main/java/nl/mpi/kinnate/userstorage/KinSessionStorage.java
@@ -21,6 +21,11 @@ public class KinSessionStorage extends ArbilSessionStorage {
         this.versionManager = versionManager;
     }
 
+    // The major, minor version numbers will change the working directory name so that each minor version requires
+    // an export import operation allowing the internal data structure to be changed. When the internal data structure
+    // is stable the minor version can be replaced with an x so that the directory does not change. Exporting will
+    // require the use of the old version of the application and this could be achieved by creating a jnlp for the
+    // old jars and an export dialog instead of the main application.
     @Override
     public String[] getLocationOptions() {
         String directoryName = ".kinoath-" + versionManager.getApplicationVersion().currentMajor + "-" + versionManager.getApplicationVersion().currentMinor;
