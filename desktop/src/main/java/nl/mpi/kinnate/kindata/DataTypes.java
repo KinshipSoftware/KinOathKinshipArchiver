2339d347aebeb9889fdeef9fcab1f360f68f732d
Peter Withers <Peter.Withers@mpi.nl>
2012-01-17 14:53:28 +0000

    Updated some sample diagrams to reflect recent changes. Updated the line rendering to reflect recent changes. Replaced the entity relation comparator code. Updated the relation type and custom relation type data structure. Added table cell editor and renderers for the kin type definitions table so that multiple select is available for symbols and relations. Added action and button to scan the diagram for relation types to which styles can be applied.

--- desktop/src/main/java/nl/mpi/kinnate/kindata/DataTypes.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindata/DataTypes.java
@@ -12,13 +12,12 @@ public class DataTypes {
         // todo: replace affiliation, resource, collector, metadata, none  for OTHER..
         // maybe keep the: kinterm, curve1, curve2, none
         // sibling, ancestor, descendant, union, kin_term, custom_type1, custom_type2, none
-        sibling, ancestor, descendant, union, kinterm, other //, horizontalCurve, gedcom,/* probably remove the items after this */ custom, affiliation, resource, collector, metadata, none // todo: should metadata relations use the same link type as jpg files?
-
-
+        sibling, ancestor, descendant, union, kinterm, other
+        //, horizontalCurve, gedcom,/* probably remove the items after this */ custom, affiliation, resource, collector, metadata, none // todo: should metadata relations use the same link type as jpg files?
 // modify the kin type definitions to allow multi select for symbols and relation types eg P could be squ or cir and tri
-                // and add additional option after divider with <all> that toggles all selections 
-                // if other then enable extra column with line type of h or v
-                // modify relation type in kintype definitions to ude the custom relation types not the enum
+        // and add additional option after divider with <all> that toggles all selections
+        // if other then enable extra column with line type of h or v
+        // modify relation type in kintype definitions to ude the custom relation types not the enum
     }
 
     public static boolean isSanguinLine(String stringRelation) {
@@ -43,8 +42,6 @@ public class DataTypes {
     }
 
     public RelationTypeDefinition[] getReferenceRelations() {
-        return new RelationTypeDefinition[]{
-//                    new RelationTypeDefinition("SampeRelationType", "", RelationType.custom, "#999999", 4, "")
-                };
+        return new RelationTypeDefinition[]{};
     }
 }
