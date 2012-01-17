2645e8eecfc9ffe4628c239d87dc8f519e0c666a
Peter Withers <Peter.Withers@mpi.nl>
2012-01-17 14:38:29 +0000

    Changed the working directory from .arbil to .kinoath and add the major, minor version numbers to the working directory name so that each minor version requires an export import operation allowing the internal data structure to be changed. When the internal data structure is stable the minor version can be replaced with an x so that the directory does not change. Exporting will require the use of the old version of the application and this could be achieved by creating a jnlp for the old jars and an export dialog instead of the main application. Resolves ticket #1305

--- desktop/src/main/java/nl/mpi/kinnate/userstorage/KinSessionStorage.java
+++ desktop/src/main/java/nl/mpi/kinnate/userstorage/KinSessionStorage.java
@@ -1,6 +1,12 @@
 package nl.mpi.kinnate.userstorage;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import nl.mpi.arbil.userstorage.ArbilSessionStorage;
+import nl.mpi.arbil.util.ApplicationVersionManager;
 
 /**
  *  Document   : KinSessionStorage
@@ -9,8 +15,33 @@ import nl.mpi.arbil.userstorage.ArbilSessionStorage;
  */
 public class KinSessionStorage extends ArbilSessionStorage {
 
+    private ApplicationVersionManager versionManager;
+
+    public KinSessionStorage(ApplicationVersionManager versionManager) {
+        this.versionManager = versionManager;
+    }
+
     @Override
     public String[] getLocationOptions() {
-        return super.getLocationOptions();
+        String directoryName = ".kinoath-" + versionManager.getApplicationVersion().currentMajor + "-" + versionManager.getApplicationVersion().currentMinor;
+        String[] locationOptions = new String[]{
+            System.getProperty("user.home") + File.separatorChar + "Local Settings" + File.separatorChar + "Application Data" + File.separatorChar + directoryName + File.separatorChar,
+            System.getenv("APPDATA") + File.separatorChar + directoryName + File.separatorChar,
+            System.getProperty("user.home") + File.separatorChar + directoryName + File.separatorChar,
+            System.getenv("USERPROFILE") + File.separatorChar + directoryName + File.separatorChar,
+            System.getProperty("user.dir") + File.separatorChar + directoryName + File.separatorChar,};
+        List<String> uniqueArray = new ArrayList<String>();
+        uniqueArray.addAll(Arrays.asList(locationOptions));
+        for (Iterator<String> iterator = uniqueArray.iterator(); iterator.hasNext();) {
+            String element = iterator.next();
+            if (element.startsWith("null")) {
+                iterator.remove();
+            }
+        }
+        locationOptions = uniqueArray.toArray(new String[]{});
+        for (String currentLocationOption : locationOptions) {
+            System.out.println("LocationOption: " + currentLocationOption);
+        }
+        return locationOptions;
     }
 }
