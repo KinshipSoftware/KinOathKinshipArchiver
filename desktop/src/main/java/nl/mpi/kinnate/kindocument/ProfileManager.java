09063ca90d81f86fb065a9644d9c6ce0bfe651fc
Peter Withers <Peter.Withers@mpi.nl>
2012-01-30 13:56:16 +0000

    Corrected the sub panel title, for instance when importing gedcom and then adding sub panels for the xsd check output. Modified the loading of the profile list so that it is not done in the constructor and added the loading of all profiles selected for the diagram, so that they are already present when needed. Without this the user would have to wait for some time after adding an entity until when the entity is seen.

---
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/ProfileManager.java
@@ -0,0 +1,67 @@
+package nl.mpi.kinnate.kindocument;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.transform.TransformerException;
+import nl.mpi.arbil.clarin.profiles.CmdiProfileReader;
+import nl.mpi.arbil.clarin.profiles.CmdiProfileReader.ProfileSelection;
+import nl.mpi.arbil.userstorage.SessionStorage;
+import nl.mpi.arbil.util.BugCatcher;
+import nl.mpi.kinnate.ui.entityprofiles.CmdiProfileSelectionPanel;
+
+/**
+ *  Document   : ProfileManager
+ *  Created on : Jan 30, 2012, 12:03:11 PM
+ *  Author     : Peter Withers
+ */
+public class ProfileManager {
+
+    private SessionStorage sessionStorage;
+    private BugCatcher bugCatcher;
+
+    public ProfileManager(SessionStorage sessionStorage, BugCatcher bugCatcher) {
+        this.sessionStorage = sessionStorage;
+        this.bugCatcher = bugCatcher;
+    }
+
+    public void loadProfiles(final boolean forceUpdate, final CmdiProfileSelectionPanel cmdiProfileSelectionPanel) {
+        CmdiProfileReader.getSingleInstance().setSelection(ProfileSelection.ALL);
+        cmdiProfileSelectionPanel.setStatus(false, "Loading, please wait...", false);
+        new Thread("loadProfiles") {
+
+            @Override
+            public void run() {
+                ArrayList<String> problemProfiles = new ArrayList<String>();
+                // load the profiles selected for use on this diagram
+                for (String profileId : new String[]{"clarin.eu:cr1:p_1320657629627"}) { // todo: this array should come from the list of selected profiles in the diagram
+                    try {
+                        cmdiProfileSelectionPanel.setStatus(false, "Loading: " + profileId + ", please wait...", false);
+                        File xsdFile = new File(sessionStorage.getCacheDirectory(), "individual" + "-" + profileId + ".xsd");
+                        if (!xsdFile.exists() || forceUpdate) {
+                            new CmdiTransformer(sessionStorage, bugCatcher).transformProfileXmlToXsd(xsdFile, profileId);
+                        }
+                    } catch (IOException exception) {
+                        problemProfiles.add(profileId);
+                        bugCatcher.logError(exception);
+                    } catch (TransformerException exception) {
+                        problemProfiles.add(profileId);
+                        bugCatcher.logError(exception);
+                    }
+                }
+                // load the profile list from the clarin server
+                cmdiProfileSelectionPanel.setStatus(false, "Loading the profile list from the server, please wait...", false);
+                CmdiProfileReader cmdiProfileReader = CmdiProfileReader.getSingleInstance();
+                cmdiProfileReader.refreshProfiles(cmdiProfileSelectionPanel.getProfileReloadProgressBar(), forceUpdate);
+
+                cmdiProfileSelectionPanel.setCmdiProfileReader(cmdiProfileReader);
+                if (!problemProfiles.isEmpty()) {
+                    // todo: show a message dialogue
+                    cmdiProfileSelectionPanel.setStatus(true, "There were " + problemProfiles.size() + " profiles that could not be retrieved", true);
+                } else {
+                    cmdiProfileSelectionPanel.setStatus(true, "", false);
+                }
+            }
+        }.start();
+    }
+}
