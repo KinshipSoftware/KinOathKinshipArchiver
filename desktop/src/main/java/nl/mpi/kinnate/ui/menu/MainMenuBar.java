93a66ef46b238540f798ac47ad5af62d000ab375
Peter Withers <Peter.Withers@mpi.nl>
2012-03-12 16:17:03 +0000

    Updated to the latest Arbil version and made the changes required for the bug catcher modifications.

--- desktop/src/main/java/nl/mpi/kinnate/ui/menu/MainMenuBar.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/menu/MainMenuBar.java
@@ -4,7 +4,6 @@ import javax.swing.JMenuBar;
 import nl.mpi.arbil.ui.ArbilWindowManager;
 import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbil.util.ApplicationVersionManager;
-import nl.mpi.arbil.util.BugCatcher;
 import nl.mpi.kinnate.ui.window.AbstractDiagramManager;
 
 /**
@@ -14,14 +13,14 @@ import nl.mpi.kinnate.ui.window.AbstractDiagramManager;
  */
 public class MainMenuBar extends JMenuBar {
 
-    public MainMenuBar(AbstractDiagramManager abstractDiagramManager, SessionStorage sessionStorage, ArbilWindowManager dialogHandler, BugCatcher bugCatcher, ApplicationVersionManager versionManager) {
-        this.add(new FileMenu(abstractDiagramManager, sessionStorage, dialogHandler, bugCatcher));
+    public MainMenuBar(AbstractDiagramManager abstractDiagramManager, SessionStorage sessionStorage, ArbilWindowManager dialogHandler, ApplicationVersionManager versionManager) {
+        this.add(new FileMenu(abstractDiagramManager, sessionStorage, dialogHandler));
         this.add(new EditMenu(abstractDiagramManager));
         this.add(new DiagramOptionsMenu(abstractDiagramManager));
         this.add(new KinTermsMenu(abstractDiagramManager));
         this.add(new ArchiveMenu(abstractDiagramManager));
         this.add(new DiagramPanelsMenu(abstractDiagramManager));
         this.add(new WindowMenu(abstractDiagramManager));
-        this.add(new HelpMenu(abstractDiagramManager, bugCatcher, dialogHandler, sessionStorage, versionManager));
+        this.add(new HelpMenu(abstractDiagramManager, dialogHandler, sessionStorage, versionManager));
     }
 }
