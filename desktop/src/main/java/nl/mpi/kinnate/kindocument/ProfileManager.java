847128f1230684b1ecc076477fa69739cd671a5a
Peter Withers <Peter.Withers@mpi.nl>
2012-03-21 13:31:09 +0000

    Added code to remove relations between entities. Updated the diagram context menu. Added a functional remove relations menu item. Updated the profile selection panel to; provide progress feedback, no download any profiles until requested, when a profile is selected download and convert the required profile, give better feedback when an error occurs. Updated the add entity to provide better feedback when there is an error creating an entity and to always clear the progress bar even when there is an error. Modified the profile transformer code to delete output and intermediate files when there was an error.

--- desktop/src/main/java/nl/mpi/kinnate/kindocument/ProfileManager.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/ProfileManager.java
@@ -1,13 +1,11 @@
 package nl.mpi.kinnate.kindocument;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import javax.xml.transform.TransformerException;
 import nl.mpi.arbil.clarin.profiles.CmdiProfileReader;
 import nl.mpi.arbil.clarin.profiles.CmdiProfileReader.ProfileSelection;
 import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbil.util.BugCatcherManager;
+import nl.mpi.arbil.util.MessageDialogHandler;
 import nl.mpi.kinnate.ui.entityprofiles.CmdiProfileSelectionPanel;
 
 /**
@@ -18,12 +16,17 @@ import nl.mpi.kinnate.ui.entityprofiles.CmdiProfileSelectionPanel;
 public class ProfileManager {
 
     private SessionStorage sessionStorage;
+    private MessageDialogHandler dialogHandler;
+    private CmdiProfileSelectionPanel cmdiProfileSelectionPanel;
+    private ArrayList<String> selectedProfiles = new ArrayList<String>();
 
-    public ProfileManager(SessionStorage sessionStorage) {
+    public ProfileManager(SessionStorage sessionStorage, MessageDialogHandler dialogHandler) {
         this.sessionStorage = sessionStorage;
+        this.dialogHandler = dialogHandler;
     }
 
     public void loadProfiles(final boolean forceUpdate, final CmdiProfileSelectionPanel cmdiProfileSelectionPanel) {
+        this.cmdiProfileSelectionPanel = cmdiProfileSelectionPanel;
         CmdiProfileReader.getSingleInstance().setSelection(ProfileSelection.ALL);
         cmdiProfileSelectionPanel.setStatus(false, "Loading, please wait...", false);
         new Thread("loadProfiles") {
@@ -33,16 +36,10 @@ public class ProfileManager {
                 ArrayList<String> problemProfiles = new ArrayList<String>();
                 // load the profiles selected for use on this diagram
                 for (String profileId : new String[]{"clarin.eu:cr1:p_1320657629627"}) { // todo: this array should come from the list of selected profiles in the diagram
+                    cmdiProfileSelectionPanel.setStatus(false, "Loading: " + profileId + ", please wait...", false);
                     try {
-                        cmdiProfileSelectionPanel.setStatus(false, "Loading: " + profileId + ", please wait...", false);
-                        File xsdFile = new File(sessionStorage.getCacheDirectory(), "individual" + "-" + profileId + ".xsd");
-                        if (!xsdFile.exists() || forceUpdate) {
-                            new CmdiTransformer(sessionStorage).transformProfileXmlToXsd(xsdFile, profileId);
-                        }
-                    } catch (IOException exception) {
-                        problemProfiles.add(profileId);
-                        BugCatcherManager.getBugCatcher().logError(exception);
-                    } catch (TransformerException exception) {
+                        preloadProfile(profileId, forceUpdate);
+                    } catch (KinXsdException exception) {
                         problemProfiles.add(profileId);
                         BugCatcherManager.getBugCatcher().logError(exception);
                     }
@@ -50,9 +47,9 @@ public class ProfileManager {
                 // load the profile list from the clarin server
                 cmdiProfileSelectionPanel.setStatus(false, "Loading the profile list from the server, please wait...", false);
                 CmdiProfileReader cmdiProfileReader = CmdiProfileReader.getSingleInstance();
-                cmdiProfileReader.refreshProfilesAndUpdateCache(cmdiProfileSelectionPanel.getProfileReloadProgressBar(), forceUpdate);
+                cmdiProfileReader.refreshProfiles(forceUpdate);
 
-                cmdiProfileSelectionPanel.setCmdiProfileReader(cmdiProfileReader);
+                cmdiProfileSelectionPanel.setCmdiProfileReader(cmdiProfileReader, ProfileManager.this);
                 if (!problemProfiles.isEmpty()) {
                     // todo: show a message dialogue
                     cmdiProfileSelectionPanel.setStatus(true, "There were " + problemProfiles.size() + " selected profiles that could not be retrieved", true);
@@ -61,5 +58,41 @@ public class ProfileManager {
                 }
             }
         }.start();
+    }
+
+    private void preloadProfile(String profileId, boolean forceUpdate) throws KinXsdException {
+//        File xsdFile = new File(sessionStorage.getCacheDirectory(), "individual" + "-" + profileId + ".xsd");
+//        if (!xsdFile.exists() || forceUpdate) {
+        new CmdiTransformer(sessionStorage).getXsd(profileId, forceUpdate);
+//        }
+    }
+
+    public void addProfileSelection(final String profileId, final String displayName) {
+        new Thread("addProfileSelection") {
+
+            @Override
+            public void run() {
+                try {
+                    cmdiProfileSelectionPanel.setStatus(false, "Loading, please wait...", false);
+                    preloadProfile(profileId, false);
+                    selectedProfiles.add(profileId);
+//            return true;
+                } catch (KinXsdException exception) {
+                    BugCatcherManager.getBugCatcher().logError(exception);
+                    dialogHandler.addMessageDialogToQueue("The selected profile (" + displayName + ") could not be loaded.", "Profile Selection Error");
+//            return false;
+                }
+                cmdiProfileSelectionPanel.setStatus(true, "", false);
+            }
+        }.start();
+
+    }
+
+    public void removeProfileSelection(String profileId) {
+        selectedProfiles.remove(profileId);
+    }
+
+    public boolean profileIsSelected(String profileId) {
+        return selectedProfiles.contains(profileId);
     }
 }
