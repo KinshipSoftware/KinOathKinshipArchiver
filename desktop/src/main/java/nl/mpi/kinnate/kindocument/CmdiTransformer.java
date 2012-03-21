4e74953d87db63cdd20983e9e66d11ed49568550
Peter Withers <Peter.Withers@mpi.nl>
2012-03-21 13:29:37 +0000

    Added code to remove relations between entities. Updated the diagram context menu. Added a functional remove relations menu item. Updated the profile selection panel to; provide progress feedback, no download any profiles until requested, when a profile is selected download and convert the required profile, give better feedback when an error occurs. Updated the add entity to provide better feedback when there is an error creating an entity and to always clear the progress bar even when there is an error. Modified the profile transformer code to delete output and intermediate files when there was an error.

--- desktop/src/main/java/nl/mpi/kinnate/kindocument/CmdiTransformer.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/CmdiTransformer.java
@@ -2,21 +2,18 @@ package nl.mpi.kinnate.kindocument;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbil.util.ApplicationVersionManager;
-import nl.mpi.arbil.util.BugCatcherManager;
 import nl.mpi.kinnate.KinOathVersion;
 import nl.mpi.kinnate.userstorage.KinSessionStorage;
 
@@ -37,25 +34,15 @@ public class CmdiTransformer {
         this.sessionStorage = sessionStorage;
     }
 
-    public URI getXsdUrlString(String entityType) throws KinXsdException {
-//        String profileId = "clarin.eu:cr1:p_1320657629627";
-        String profileId = "clarin.eu:cr1:p_1320657629627";
-        File xsdFile = new File(sessionStorage.getCacheDirectory(), entityType + "-" + profileId + ".xsd");
-        try {
-            if (!xsdFile.exists()) {
-                transformProfileXmlToXsd(xsdFile, profileId);
-            }
-            return xsdFile.toURI();
-        } catch (IOException exception) {
-            System.out.println("exception: " + exception.getMessage());
-            throw new KinXsdException();
-        } catch (TransformerException exception) {
-            System.out.println("exception: " + exception.getMessage());
-            throw new KinXsdException();
+    public URI getXsd(String profileId, boolean forceUpdate) throws KinXsdException {
+        File xsdFile = new File(sessionStorage.getCacheDirectory(), profileId + "-kmdi.xsd");
+        if (forceUpdate || !xsdFile.exists()) {
+            transformProfileXmlToXsd(xsdFile, profileId);
         }
+        return xsdFile.toURI();
     }
 
-    public File transformProfileXmlToXsd(File outputFile, String profileId) throws IOException, TransformerException {
+    private File transformProfileXmlToXsd(File outputFile, String profileId) throws KinXsdException {
         String cmdiProfileXmlUrl = "http://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/profiles/" + profileId + "/xml";
         System.out.println("outputFile: " + outputFile.getAbsolutePath());
 
@@ -70,34 +57,33 @@ public class CmdiTransformer {
         File intermediateFile = new File(outputFile.getParentFile(), profileId + "-cmdi.xsd");
         System.out.println(intermediateFile);
         System.out.println(outputFile);
-        generateXsd(xlsFile, cmdiProfileXmlUrl, intermediateFile);
-        File cmdi2kmdiFile = sessionStorage.updateCache(cmdi2kmdiXsl.toExternalForm(), 1, false);
-        generateXsd(cmdi2kmdiFile, intermediateFile.toURI().toString(), outputFile);
-        return outputFile;
+        try {
+            generateXsd(xlsFile, cmdiProfileXmlUrl, intermediateFile);
+            File cmdi2kmdiFile = sessionStorage.updateCache(cmdi2kmdiXsl.toExternalForm(), 1, false);
+            generateXsd(cmdi2kmdiFile, intermediateFile.toURI().toString(), outputFile);
+            return outputFile;
+        } catch (IOException exception) {
+            System.out.println("exception: " + exception.getMessage());
+            intermediateFile.delete();
+            xlsFile.delete();
+            throw new KinXsdException("Could not read the selected profile");
+        } catch (TransformerException exception) {
+            System.out.println("exception: " + exception.getMessage());
+            intermediateFile.delete();
+            xlsFile.delete();
+            throw new KinXsdException("Could not read the selected profile");
+        }
     }
 
-    private void generateXsd(File xlsFile, String cmdiProfileXmlUrl, File outputFile) {
+    private void generateXsd(File xlsFile, String cmdiProfileXmlUrl, File outputFile) throws IOException, TransformerException {
         Templates componentToSchemaTemplates;
-        try {
-            System.setProperty("javax.xml.transform.TransformerFactory", net.sf.saxon.TransformerFactoryImpl.class.getName());
-            componentToSchemaTemplates = TransformerFactory.newInstance().newTemplates(new StreamSource(xlsFile));
-        } catch (TransformerConfigurationException e) {
-            BugCatcherManager.getBugCatcher().logError("Cannot create Template", e);
-            return;
-        }
-        try {
-            Transformer transformer = componentToSchemaTemplates.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        System.setProperty("javax.xml.transform.TransformerFactory", net.sf.saxon.TransformerFactoryImpl.class.getName());
+        componentToSchemaTemplates = TransformerFactory.newInstance().newTemplates(new StreamSource(xlsFile));
+        Transformer transformer = componentToSchemaTemplates.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 //            transformer.setOutputProperty(SaxonOutputKeys.INDENT_SPACES, "1"); //Keeps the downloads a lot smaller.
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            transformer.transform(new StreamSource(cmdiProfileXmlUrl), new StreamResult(new java.io.FileOutputStream(outputFile)));
-        } catch (TransformerConfigurationException e) {
-            BugCatcherManager.getBugCatcher().logError("Cannot create Transformer", e);
-        } catch (TransformerException e) {
-            BugCatcherManager.getBugCatcher().logError("Cannot transform xml file: ", e);
-        } catch (FileNotFoundException e) {
-            BugCatcherManager.getBugCatcher().logError(e);
-        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        transformer.transform(new StreamSource(cmdiProfileXmlUrl), new StreamResult(new java.io.FileOutputStream(outputFile)));
     }
 
     static public void main(String[] args) {
@@ -106,11 +92,8 @@ public class CmdiTransformer {
             final KinSessionStorage kinSessionStorage = new KinSessionStorage(new ApplicationVersionManager(new KinOathVersion()));
             File xsdFile = new File(kinSessionStorage.getCacheDirectory(), "individual" + "-" + profileId + ".xsd");
             new CmdiTransformer(kinSessionStorage).transformProfileXmlToXsd(xsdFile, profileId);
-        } catch (IOException exception) {
-            System.out.println("exception: " + exception.getMessage());
-        } catch (TransformerException exception) {
+        } catch (KinXsdException exception) {
             System.out.println("exception: " + exception.getMessage());
         }
-
     }
 }
