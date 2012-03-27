1f9a333517968a7c1191d7a16eae368ec80da09d
Peter Withers <Peter.Withers@mpi.nl>
2012-03-27 14:19:08 +0000

    In order to make the import results easier for users to examine: Added options to the search panel to show any selected node on the results tree in the graph and to expand that selection by kin type string. Added a diagram tree for imported entities so that the imported entities can be browsed by the user. Added the drag handler to the imported nodes tree. When an import is complete the imported entities tree is shown in a graph window. Updated the gedcom import to be more friendly in the format of the date resulting from the import, including type labels so that the user knows a submitter from a submission etc. Reduced the stress on the application when importing by providing the user with the imported entities in a tree rather than trying to graph them all at the same time. Modified the search and imported node trees to show the selected nodes in the graph and to expand that selection by user entered kintype strings.

--- desktop/src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
+++ desktop/src/main/java/nl/mpi/kinnate/entityindexer/EntityService.java
@@ -1,15 +1,16 @@
 package nl.mpi.kinnate.entityindexer;
 
 import java.net.URI;
+import java.util.ArrayList;
 import javax.swing.JProgressBar;
 import nl.mpi.kinnate.kindata.EntityData;
-import nl.mpi.kinnate.kintypestrings.ParserHighlight;
 import nl.mpi.kinnate.svg.DataStoreSvg;
+import nl.mpi.kinnate.ui.KinTypeStringProvider;
 
 /**
- *  Document   : EntityService
- *  Created on : Mar 22, 2011, 1:09:33 PM
- *  Author     : Peter Withers
+ * Document : EntityService
+ * Created on : Mar 22, 2011, 1:09:33 PM
+ * Author : Peter Withers
  */
 public interface EntityService {
 
@@ -18,5 +19,5 @@ public interface EntityService {
     public void requestAbortProcess();
 
 //    public void primeWithEntities(EntityData[] preLoadedEntities); // it would seem not to be a good idea to try and use existing entities from an svg file when their relations might not exist, so we will allow the existing entities to be used on the graph but not for database actions
-    public EntityData[] processKinTypeStrings(URI[] egoNodes, String[] kinTypeStrings, ParserHighlight[] parserHighlight, IndexerParameters indexParameters, DataStoreSvg dataStoreSvg, JProgressBar progressBar) throws EntityServiceException, ProcessAbortException;
+    public EntityData[] processKinTypeStrings(URI[] egoNodes, ArrayList<KinTypeStringProvider> kinTypeStringProviders, IndexerParameters indexParameters, DataStoreSvg dataStoreSvg, JProgressBar progressBar) throws EntityServiceException, ProcessAbortException;
 }
