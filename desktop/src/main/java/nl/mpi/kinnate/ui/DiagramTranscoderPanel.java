bc3762493e10e24667c2f14019c18099480ea0a1
Peter Withers <Peter.Withers@mpi.nl>
2012-04-19 14:01:06 +0000

    Added PNG to the options for export. Added a custom accessory option to the Arbil file select so that additional controls can be added to the dialogue. Updated the export diagram to PDF and JPG menu actions. refs #1721

--- desktop/src/main/java/nl/mpi/kinnate/ui/DiagramTranscoderPanel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/DiagramTranscoderPanel.java
@@ -1,6 +1,8 @@
 package nl.mpi.kinnate.ui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Dimension2D;
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
@@ -51,6 +53,12 @@ public class DiagramTranscoderPanel extends JPanel {
             }
         };
         dpiSpinner.addChangeListener(changeListener);
+        formatComboBox.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                updateSettings();
+            }
+        });
     }
 
     protected void updateSettings() {
