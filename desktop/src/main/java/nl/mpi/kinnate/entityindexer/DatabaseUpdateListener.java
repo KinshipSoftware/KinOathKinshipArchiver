23863d5fa5fcfe28375dd5ec458d5c2a4ae64dc1
Peter Withers <Peter.Withers@mpi.nl>
2012-04-25 15:29:15 +0000

    Added a mechanism to update the project tree and others when a database update occurs. Added the project tree to the standard diagrams. Added the drag handler to the project tree. Added a query type that returns all entities that do not have a relation type eg ancestor or descendant. This will be used to display a ancestor or descendant tree for the project. Added an end point query which returns all entities that are the farthest ancestor or most recent descendant for use in the project tree. Added a project tree, project panel and project root node. refs #1814

---
+++ desktop/src/main/java/nl/mpi/kinnate/entityindexer/DatabaseUpdateListener.java
@@ -0,0 +1,11 @@
+package nl.mpi.kinnate.entityindexer;
+
+/**
+ * Document : DatabaseUpdateListener
+ * Created on : Apr 25, 2012, 5:08:26 PM
+ * Author : Peter Withers
+ */
+public interface DatabaseUpdateListener {
+
+    void updateOccured();
+}
