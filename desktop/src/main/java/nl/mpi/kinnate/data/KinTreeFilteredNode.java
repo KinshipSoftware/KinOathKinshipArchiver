6323968977ac9f69b1fc5704eb226e5d681d02e9
Peter Withers <Peter.Withers@mpi.nl>
2012-04-26 15:29:34 +0000

    Made the tree loading more efficient. Added paging to the project tree to overcome the speed of the tree and the potential number of tree nodes. Added a mechanism to update the project tree and others when a database update occurs. Added the project tree to the standard diagrams. Added the drag handler to the project tree. Added a query type that returns all entities that do not have a relation type eg ancestor or descendant. This will be used to display a ancestor or descendant tree for the project. Added an end point query which returns all entities that are the farthest ancestor or most recent descendant for use in the project tree. Added a project tree, project panel and project root node. refs #1814 #1825

--- desktop/src/main/java/nl/mpi/kinnate/data/KinTreeFilteredNode.java
+++ desktop/src/main/java/nl/mpi/kinnate/data/KinTreeFilteredNode.java
@@ -7,7 +7,6 @@ import nl.mpi.arbil.util.MessageDialogHandler;
 import nl.mpi.kinnate.entityindexer.EntityCollection;
 import nl.mpi.kinnate.entityindexer.IndexerParameters;
 import nl.mpi.kinnate.kindata.DataTypes;
-import nl.mpi.kinnate.kindata.EntityData;
 import nl.mpi.kinnate.kindata.EntityRelation;
 
 /**
@@ -18,10 +17,24 @@ import nl.mpi.kinnate.kindata.EntityRelation;
 public class KinTreeFilteredNode extends KinTreeNode {
 
     DataTypes.RelationType subnodeFilter;
+    EntityRelation entityRelation;
 
-    public KinTreeFilteredNode(EntityData entityData, DataTypes.RelationType subnodeFilter, IndexerParameters indexerParameters, MessageDialogHandler dialogHandler, EntityCollection entityCollection, ArbilDataNodeLoader dataNodeLoader) {
-        super(entityData, indexerParameters, dialogHandler, entityCollection, dataNodeLoader);
-        this.subnodeFilter = subnodeFilter; // subnode filter will be used to filter the child nodes
+    public KinTreeFilteredNode(EntityRelation entityRelation, IndexerParameters indexerParameters, MessageDialogHandler dialogHandler, EntityCollection entityCollection, ArbilDataNodeLoader dataNodeLoader) {
+        super(entityRelation.alterUniqueIdentifier, entityRelation.getAlterNode(), indexerParameters, dialogHandler, entityCollection, dataNodeLoader);
+        this.subnodeFilter = entityRelation.getRelationType(); // subnode filter will be used to filter the child nodes
+        this.entityRelation = entityRelation;
+    }
+
+    protected void loadEntityIfNotLoaded() {
+        if (entityData == null) {
+            // todo: should these enties be cached? or will the entire tree be discarded on redraw?
+            // todo change this to return a node imediately and the node can then load itself and then request a tree resort
+            entityData = entityRelation.getAlterNode();
+            if (entityData == null) {
+                entityData = entityCollection.getEntity(entityRelation.alterUniqueIdentifier, indexerParameters);
+                entityRelation.setAlterNode(entityData);
+            }
+        }
     }
 
     @Override
@@ -35,16 +48,25 @@ public class KinTreeFilteredNode extends KinTreeNode {
         HashSet<ArbilNode> relationList = new HashSet<ArbilNode>();
         for (EntityRelation entityRelation : entityData.getAllRelations()) {
             if (subnodeFilter == entityRelation.getRelationType()) {
-                EntityData alterEntity = entityRelation.getAlterNode();
-                if (alterEntity == null) {
-                    alterEntity = entityCollection.getEntity(entityRelation.alterUniqueIdentifier, indexerParameters);
-                    entityRelation.setAlterNode(alterEntity);
-                }
-                relationList.add(new KinTreeFilteredNode(alterEntity, entityRelation.getRelationType(), indexerParameters, dialogHandler, entityCollection, dataNodeLoader));
+//                EntityData alterEntity = entityRelation.getAlterNode();
+//                if (alterEntity == null) {
+//                    alterEntity = entityCollection.getEntity(entityRelation.alterUniqueIdentifier, indexerParameters);
+//                    entityRelation.setAlterNode(alterEntity);
+//                }
+                relationList.add(new KinTreeFilteredNode(entityRelation, indexerParameters, dialogHandler, entityCollection, dataNodeLoader));
             }
         }
         getLinksMetaNode(relationList);
         childNodes = relationList.toArray(new ArbilNode[]{});
+        new Thread() {
+
+            @Override
+            public void run() {
+                for (ArbilNode childNode : childNodes) {
+                    ((KinTreeFilteredNode) childNode).loadEntityIfNotLoaded();
+                }
+            }
+        }.start();
         return childNodes;
     }
 }
