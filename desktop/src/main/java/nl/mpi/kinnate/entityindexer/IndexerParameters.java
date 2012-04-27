0fe4093d621a71f20891deb4048d02ac19fc98d4
Peter Withers <Peter.Withers@mpi.nl>
2012-04-27 15:15:19 +0000

    Reduced the superfluous fields imported with gedcom files and normalised and transformed them in the same way as the csv import. Added more symbols to the default diagram including blank for non person nodes that have been imported. Added cross symbols in the existing range of colours. Modified the import file checksum process to prevent the difference in output when creating the md5sum in the standalone application vs the webstart version. Modified the import to preserve the entity identifers on reimport, so that the diagrams maintain the same layout on different installations if the data was imported from the same file. refs #1829

--- desktop/src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
+++ desktop/src/main/java/nl/mpi/kinnate/entityindexer/IndexerParameters.java
@@ -23,8 +23,8 @@ public class IndexerParameters {
     public IndexerParam dateOfDeathField = new IndexerParam(new String[][]{{"*:Kinnate/*:CustomData/*:DateOfDeath"}});
     @XmlElement(name = "LabelFields", namespace = "http://mpi.nl/tla/kin")
     public IndexerParam labelFields = new IndexerParam(new String[][]{{"*:Kinnate/*:CustomData/*[starts-with(local-name(), 'Name')]"},
-                {"*:Kinnate/*:CustomData/*:INDI/*:NAME/*:NAME"},
-                {"*:Kinnate/*:CustomData/*:INDI/*:NAME"},
+//                {"*:Kinnate/*:CustomData/*:INDI/*:NAME/*:NAME"},
+//                {"*:Kinnate/*:CustomData/*:INDI/*:NAME"},
                 {"*:Kinnate/*:CustomData/*:Type"}
 //                , {"*:Kinnate/*:CustomData/*:Name"}
 //    , {"*:Kinnate/*:Gedcom/*:Entity/*:NAME/*:NAME"}
@@ -40,8 +40,8 @@ public class IndexerParameters {
                 , {"*:Kinnate/*:CustomData[*:Gender='Female']", "circle"},
                 {"*:Kinnate/*:CustomData[*:Gender='']", "square"},
                 {"*:Kinnate/*:CustomData[*:DateOfDeath!='']", "blackstrikethrough"}, // the use of != is correct because if there is any node value in the DateOfBirth node or nodes then the symbol should be used
-                {"*:Kinnate/*:CustomData[*:Chromosome='Xx']", "redstrikethrough"},
-                {"*:Kinnate/*:CustomData[*:Chromosome='xY']", "redstrikethrough"}
+                {"*:Kinnate/*:CustomData[*:INDI/*:chro='Xx']", "redmarker"},
+                {"*:Kinnate/*:CustomData[*:INDI/*:chro='xY']", "redmarker"}
             //                , {"*:Kinnate/*:CustomData/*:Individual[*:Gender='']", "square"},
             //                {"*:Kinnate/*:CustomData/*:Individual[*:Gender='Male']", "triangle"},
             //                {"*:Kinnate/*:CustomData/*:Individual[*:Gender='Female']", "circle"}
