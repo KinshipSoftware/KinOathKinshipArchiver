021a9bac0a7c17417490f65f55800b3ce60ee215
Peter Withers <Peter.Withers@mpi.nl>
2012-05-04 14:06:12 +0000

    Modified merge entities so that merged relations are joined correctly and no extraneous relations are left behind. Modified merge entities so that it deletes the non lead selection after merging. refs #1850 #1849 #1848

--- desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityMerger.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityMerger.java
@@ -1,5 +1,6 @@
 package nl.mpi.kinnate.kindocument;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import nl.mpi.arbil.userstorage.SessionStorage;
@@ -11,38 +12,69 @@ import nl.mpi.kinnate.kindata.EntityRelation;
 import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 
 /**
- *  Document   : EntityMerger
- *  Created on : Aug 29, 2011, 3:47:29 PM
- *  Author     : Peter Withers
+ * Document : EntityMerger
+ * Created on : Aug 29, 2011, 3:47:29 PM
+ * Author : Peter Withers
  */
 public class EntityMerger extends DocumentLoader {
+
+    private UniqueIdentifier[] deletedIdentifiersArray;
+    private UniqueIdentifier[] affectedIdentifiersArray;
 
     public EntityMerger(SessionStorage sessionStorage, MessageDialogHandler dialogHandler, EntityCollection entityCollection) {
         super(sessionStorage, dialogHandler, entityCollection);
     }
 
-    public UniqueIdentifier[] mergeEntities(UniqueIdentifier[] selectedIdentifiers) throws ImportException {
+    public UniqueIdentifier[] getAffectedIdentifiersArray() {
+        return affectedIdentifiersArray;
+    }
+
+    public UniqueIdentifier[] getDeletedIdentifiersArray() {
+        return deletedIdentifiersArray;
+    }
+
+    public void mergeEntities(UniqueIdentifier[] selectedIdentifiers) throws ImportException {
         ArrayList<EntityDocument> nonLeadEntityDocuments = new ArrayList<EntityDocument>();
+        ArrayList<UniqueIdentifier> deletedIdentifiers = new ArrayList<UniqueIdentifier>();
         try {
             EntityDocument leadEntityDocument = getEntityDocuments(selectedIdentifiers, nonLeadEntityDocuments);
             for (EntityDocument alterEntity : nonLeadEntityDocuments) {
+                deletedIdentifiers.add(alterEntity.getUniqueIdentifier());
                 for (EntityRelation entityRelation : alterEntity.entityData.getAllRelations()) {
                     EntityDocument relatedDocument = entityMap.get(entityRelation.alterUniqueIdentifier);
-                    // add the new relation
-                    leadEntityDocument.entityData.addRelatedNode(relatedDocument.entityData, entityRelation.getRelationType(), entityRelation.lineColour, entityRelation.labelString, entityRelation.dcrType, entityRelation.customType);
+                    if (!leadEntityDocument.entityData.getUniqueIdentifier().equals(relatedDocument.entityData.getUniqueIdentifier())) { // do not add a relation to itself                        
+                        // add the new relation
+                        leadEntityDocument.entityData.addRelatedNode(relatedDocument.entityData, entityRelation.getRelationType(), entityRelation.lineColour, entityRelation.labelString, entityRelation.dcrType, entityRelation.customType);
+                    }
                     // remove the old entity relation
-                    // todo: check that the correct relations are being removed from the correct entities.
                     relatedDocument.entityData.removeRelationsWithNode(alterEntity.entityData);
                     alterEntity.entityData.removeRelationsWithNode(relatedDocument.entityData);
                 }
-                alterEntity.setAsDeletedDocument();
+            }
+            // sometimes a sibling relation is created when parent relations are added so we iterate again and make sure none are left
+            for (EntityDocument alterEntity : nonLeadEntityDocuments) {
+                for (EntityRelation entityRelation : alterEntity.entityData.getAllRelations()) {
+                    EntityDocument relatedDocument = entityMap.get(entityRelation.alterUniqueIdentifier);
+                    // remove the old entity relation
+                    relatedDocument.entityData.removeRelationsWithNode(alterEntity.entityData);
+                    alterEntity.entityData.removeRelationsWithNode(relatedDocument.entityData);
+                }
             }
             saveAllDocuments();
+            for (EntityDocument alterEntity : nonLeadEntityDocuments) {
+                try {
+                    deleteFromDataBase(alterEntity);
+                } catch (IOException exception) {
+                    BugCatcherManager.getBugCatcher().logError(exception);
+                    throw new ImportException("Error: " + exception.getMessage());
+                }
+            }
         } catch (URISyntaxException exception) {
             BugCatcherManager.getBugCatcher().logError(exception);
             throw new ImportException("Error: " + exception.getMessage());
         }
-        return getAffectedIdentifiers();
+        deletedIdentifiersArray = deletedIdentifiers.toArray(new UniqueIdentifier[0]);
+        affectedIdentifiersArray = getAffectedIdentifiers();
     }
 
     public UniqueIdentifier[] duplicateEntities(UniqueIdentifier[] selectedIdentifiers) throws ImportException {
