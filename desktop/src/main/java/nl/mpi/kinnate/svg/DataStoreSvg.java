39124a8a4a2ca3ef74554b5752462900da344637
Peter Withers <Peter.Withers@mpi.nl>
2012-06-14 15:12:42 +0000

    Modified the relation type definitions table so that there can be only one row for each custom type. This has the effect of maintaining that both descendant and ancestor are shown as a single custom type. Resolved some EDT issues. refs #1803

--- desktop/src/main/java/nl/mpi/kinnate/svg/DataStoreSvg.java
+++ desktop/src/main/java/nl/mpi/kinnate/svg/DataStoreSvg.java
@@ -2,6 +2,7 @@ package nl.mpi.kinnate.svg;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -14,15 +15,16 @@ import javax.xml.bind.annotation.XmlTransient;
 import nl.mpi.arbil.util.BugCatcherManager;
 import nl.mpi.kinnate.entityindexer.IndexerParameters;
 import nl.mpi.kinnate.kindata.DataTypes;
+import nl.mpi.kinnate.kindata.DataTypes.RelationType;
 import nl.mpi.kinnate.kindata.GraphSorter;
 import nl.mpi.kinnate.kindata.RelationTypeDefinition;
 import nl.mpi.kinnate.kindata.RelationTypeDefinition.CurveLineOrientation;
 import nl.mpi.kinnate.kindata.VisiblePanelSetting;
-import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 import nl.mpi.kinnate.kintypestrings.KinTermGroup;
 import nl.mpi.kinnate.kintypestrings.KinType;
 import nl.mpi.kinnate.ui.entityprofiles.ProfileRecord;
 import nl.mpi.kinnate.uniqueidentifiers.IdentifierException;
+import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -35,7 +37,7 @@ import org.w3c.dom.svg.SVGDocument;
  */
 @XmlRootElement(name = "KinDiagramData", namespace = "http://mpi.nl/tla/kin")
 public class DataStoreSvg {
-
+    
     static protected String kinDataNameSpace = "kin";
     static protected String kinDataNameSpaceLocation = "http://mpi.nl/tla/kin";
 //    @XmlElement(name = "EgoIdList", namespace = "http://mpi.nl/tla/kin")
@@ -86,9 +88,9 @@ public class DataStoreSvg {
     public boolean showDiagramBorder = true;
     @XmlElement(name = "EntityData", namespace = "http://mpi.nl/tla/kin")
     public GraphSorter graphData;
-
+    
     public enum DiagramMode {
-
+        
         FreeForm, KinTypeQuery, /* RequiredEntities, */ Undefined
     }
     @XmlElement(name = "DiagramMode", namespace = "http://mpi.nl/tla/kin")
@@ -101,22 +103,22 @@ public class DataStoreSvg {
 //    public int windowY = 600;
 
     public class GraphRelationData {
-
+        
         public UniqueIdentifier egoNodeId;
         public UniqueIdentifier alterNodeId;
         public DataTypes.RelationType relationType;
         public CurveLineOrientation curveLineOrientation;
     }
-
+    
     public DataStoreSvg() {
     }
-
+    
     public void setDefaults() {
         // todo: it might be better not to add any kin group until the user explicitly adds one from the menu
         kinTermGroups = new KinTermGroup[]{}; //new KinTermGroup(0), new KinTermGroup(1)};
         indexParameters = new IndexerParameters();
     }
-
+    
     @XmlTransient
     public RelationTypeDefinition[] getRelationTypeDefinitions() {
         if (relationTypeDefinitions != null) {
@@ -126,11 +128,68 @@ public class DataStoreSvg {
             return new DataTypes().getReferenceRelations();
         }
     }
-
+    
     public void setRelationTypeDefinitions(RelationTypeDefinition[] relationTypeDefinitions) {
         this.relationTypeDefinitions = relationTypeDefinitions;
     }
-
+    
+    class RelationTypeHashKey {
+        
+        String displayName;
+        String dataCategory;
+        
+        public RelationTypeHashKey(String displayName, String dataCategory) {
+            this.displayName = displayName;
+            this.dataCategory = dataCategory;
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final RelationTypeHashKey other = (RelationTypeHashKey) obj;
+            if ((this.displayName == null) ? (other.displayName != null) : !this.displayName.equals(other.displayName)) {
+                return false;
+            }
+            if ((this.dataCategory == null) ? (other.dataCategory != null) : !this.dataCategory.equals(other.dataCategory)) {
+                return false;
+            }
+            return true;
+        }
+        
+        @Override
+        public int hashCode() {
+            int hash = 5;
+            hash = 53 * hash + (this.displayName != null ? this.displayName.hashCode() : 0);
+            hash = 53 * hash + (this.dataCategory != null ? this.dataCategory.hashCode() : 0);
+            return hash;
+        }        
+    }
+    
+    public void addRelationTypeDefinition(RelationTypeDefinition relationTypeDefinitionToAdd) {
+        HashMap<RelationTypeHashKey, RelationTypeDefinition> kinTypesList = new HashMap<RelationTypeHashKey, RelationTypeDefinition>();
+        kinTypesList.put(new RelationTypeHashKey(relationTypeDefinitionToAdd.getDisplayName(), relationTypeDefinitionToAdd.getDataCategory()), relationTypeDefinitionToAdd);
+        for (RelationTypeDefinition relationTypeDefinition : Arrays.asList(getRelationTypeDefinitions())) {
+            final RelationTypeHashKey relationTypeHashKey = new RelationTypeHashKey(relationTypeDefinition.getDisplayName(), relationTypeDefinition.getDataCategory());
+            final RelationTypeDefinition foundType = kinTypesList.get(relationTypeHashKey);
+            if (foundType == null) {
+                // add the type to the list
+                kinTypesList.put(relationTypeHashKey, relationTypeDefinition);
+            } else {
+                // merge found type with the existing type
+                HashSet<RelationType> relationTypes = new HashSet<RelationType>(Arrays.asList(foundType.getRelationType()));
+                relationTypes.addAll(Arrays.asList(relationTypeDefinition.getRelationType()));
+                // because added RelationTypeDefinition is added first, we will keep the properties of the existing RelationTypeDefinition here
+                kinTypesList.put(relationTypeHashKey, new RelationTypeDefinition(relationTypeDefinition.getDisplayName(), relationTypeDefinition.getDataCategory(), relationTypes.toArray(new RelationType[]{}), relationTypeDefinition.getLineColour(), relationTypeDefinition.getLineWidth(), relationTypeDefinition.getLineStye()));
+            }
+        }        
+        this.relationTypeDefinitions = kinTypesList.values().toArray(new RelationTypeDefinition[]{});
+    }
+    
     @XmlTransient
     public KinType[] getKinTypeDefinitions() {
         KinType[] returnArray;
@@ -143,25 +202,25 @@ public class DataStoreSvg {
         Collections.sort(Arrays.asList(returnArray));
         return returnArray;
     }
-
+    
     public void setKinTypeDefinitions(KinType[] kinTypeDefinitions) {
         this.kinTypeDefinitions = kinTypeDefinitions;
     }
-
+    
     public VisiblePanelSetting[] getVisiblePanels() {
         if (visiblePanels == null) {
             return null;
         }
         return visiblePanels.toArray(new VisiblePanelSetting[]{});
     }
-
+    
     public void setPanelState(VisiblePanelSetting.PanelType panelType, int panelWidth, boolean panelVisible) {
         if (visiblePanels == null) {
             visiblePanels = new HashSet<VisiblePanelSetting>();
         }
         visiblePanels.add(new VisiblePanelSetting(panelType, panelVisible, panelWidth));
     }
-
+    
     public GraphRelationData getEntitiesForRelations(Node relationGroup) throws IdentifierException {
         for (Node currentChild = relationGroup.getFirstChild(); currentChild != null; currentChild = currentChild.getNextSibling()) {
             if ("RelationEntities".equals(currentChild.getLocalName())) {
@@ -175,7 +234,7 @@ public class DataStoreSvg {
         }
         return null;
     }
-
+    
     public void storeRelationParameters(SVGDocument doc, Element relationGroup, DataTypes.RelationType relationType, CurveLineOrientation curveLineOrientation, UniqueIdentifier egoEntity, UniqueIdentifier alterEntity) {
         Element dataRecordNode = doc.createElementNS(kinDataNameSpace, "kin:RelationEntities");
         dataRecordNode.setAttributeNS(kinDataNameSpace, "kin:relationType", relationType.name());
