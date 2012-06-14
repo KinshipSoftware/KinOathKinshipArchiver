47270731b1a7f40fa2bafb138405f2da273e5590
Peter Withers <Peter.Withers@mpi.nl>
2012-06-14 11:22:41 +0000

    Modified the relation type definitions table so that there can be only one row for each custom type. This has the effect of maintaining that both descendant and ancestor are shown as a single custom type. Resolved some EDT issues. refs #1803

--- desktop/src/main/java/nl/mpi/kinnate/ui/kintypeeditor/ArrayListCellRenderer.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/kintypeeditor/ArrayListCellRenderer.java
@@ -2,32 +2,35 @@ package nl.mpi.kinnate.ui.kintypeeditor;
 
 import java.awt.Component;
 import java.util.ArrayList;
-import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 
 /**
- *  Document   : ArrayListCellRenderer
- *  Created on : Jan 12, 2012, 5:46:19 PM
- *  Author     : Peter Withers
+ * Document : ArrayListCellRenderer
+ * Created on : Jan 12, 2012, 5:46:19 PM
+ * Author : Peter Withers
  */
-public class ArrayListCellRenderer implements TableCellRenderer {
+public class ArrayListCellRenderer extends DefaultTableCellRenderer implements TableCellRenderer {
 
     static protected String anyOptionDisplayString = "<any>";
 
+    @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        JLabel labelRenderer;
+        DefaultTableCellRenderer labelRenderer = this;
         StringBuilder stringBuilder = new StringBuilder();
         if (row == table.getRowCount() - 1) {
-            labelRenderer = new JLabel();
+            labelRenderer.setText("");
         } else if (value == null) {
-            labelRenderer = new JLabel(anyOptionDisplayString);
+            labelRenderer.setText(anyOptionDisplayString);
         } else {
             for (String stringValue : (ArrayList<String>) value) {
+                if (stringBuilder.length() > 0) {
+                    stringBuilder.append(", ");
+                }
                 stringBuilder.append(stringValue);
-                stringBuilder.append(", ");
             }
-            labelRenderer = new JLabel(stringBuilder.toString());
+            labelRenderer.setText(stringBuilder.toString());
         }
         labelRenderer.setOpaque(true);
         if (isSelected) {
