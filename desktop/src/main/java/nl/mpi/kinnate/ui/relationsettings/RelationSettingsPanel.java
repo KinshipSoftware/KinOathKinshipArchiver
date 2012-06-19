937cc58d16aa72f5aed0856d4927a3b8c8d77d34
Peter Withers <Peter.Withers@mpi.nl>
2012-06-19 11:01:54 +0000

    Modified the column title 'Line Orientation' to 'Curve Line Orientation' to make it clearer how this is used. Updated the Line Orientation and Line/Dash so that it displays the result on the diagram. Modified the relation type definitions table so that there can be only one row for each custom type. This has the effect of maintaining that both descendant and ancestor are shown as a single custom type. Resolved some EDT issues. refs #1803 #1811 #1787

--- desktop/src/main/java/nl/mpi/kinnate/ui/relationsettings/RelationSettingsPanel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/relationsettings/RelationSettingsPanel.java
@@ -136,7 +136,7 @@ public class RelationSettingsPanel extends JPanel implements ActionListener {
             int initalTypeCount = dataStoreSvg.getRelationTypeDefinitions().length;
             for (EntityData entityData : dataStoreSvg.graphData.getDataNodes()) {
                 for (EntityRelation entityRelation : entityData.getAllRelations()) {
-                    dataStoreSvg.addRelationTypeDefinition(new RelationTypeDefinition(entityRelation.customType, entityRelation.dcrType, new DataTypes.RelationType[]{entityRelation.getRelationType()}, "#999999", 2, 0));
+                    dataStoreSvg.addRelationTypeDefinition(new RelationTypeDefinition(entityRelation.customType, entityRelation.dcrType, new DataTypes.RelationType[]{entityRelation.getRelationType()}, "#999999", 2, 0, RelationTypeDefinition.CurveLineOrientation.horizontal));
                 }
             }
             final int foundTypesCount = dataStoreSvg.getRelationTypeDefinitions().length - initalTypeCount;
