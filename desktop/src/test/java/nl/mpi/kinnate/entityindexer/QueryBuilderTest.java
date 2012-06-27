ed2e5a2909694647a8eb631cf1eacdc24fe7b781
Peter Withers <Peter.Withers@mpi.nl>
2012-06-27 15:13:04 +0000

    Added test file for query builder. Added tests for query strings needing quotes etc escaped. Escaped relevant chars in the xqueries. Added case insensitive near match to the search. Added search words spanning different fields. refs #1799 #2022

--- desktop/src/test/java/nl/mpi/kinnate/entityindexer/QueryBuilderTest.java
+++ desktop/src/test/java/nl/mpi/kinnate/entityindexer/QueryBuilderTest.java
@@ -1,9 +1,7 @@
 package nl.mpi.kinnate.entityindexer;
 
 import junit.framework.TestCase;
-import nl.mpi.kinnate.kindata.DataTypes.RelationType;
 import nl.mpi.kinnate.kintypestrings.KinTypeElement;
-import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 
 /**
  * Document : QueryBuilderTest
@@ -33,24 +31,24 @@ public class QueryBuilderTest extends TestCase {
         System.out.println("asSequenceString");
         String[] stringArray = new String[]{"one", "two", "one&two", "single'quote", "double\"quote"};
         QueryBuilder instance = new QueryBuilder();
-        String expResult = "(\"one\",\"two\",\"one&amp;two\",\"single'quote\",\"double&quot;quote\")";
+        String expResult = "{\"one\",\"two\",\"one&amp;two\",\"single'quote\",\"double&quot;quote\"}";
         String result = instance.asSequenceString(stringArray);
         assertEquals(expResult, result);
     }
-    
+
     /**
      * Test of asSequenceString method, of class QueryBuilder.
      */
     public void testAsSequenceString_IndexerParam() {
         System.out.println("asSequenceString");
         IndexerParam indexerParam = new IndexerParam(new String[][]{
-                {"one&two", "triangle"},
-                {"single'quote", "triangle"},
-                {"double\"quote", "triangle"},
-                {"*:Kinnate/*:CustomData[*:Gender='Male']", "triangle"},
-                {"*:Kinnate/*:CustomData[*:Gender='']", "square"},
-                {"*:Kinnate/*:CustomData[*:DateOfDeath!='']", "blackstrikethrough"}
-            });
+                    {"one&two", "triangle"},
+                    {"single'quote", "triangle"},
+                    {"double\"quote", "triangle"},
+                    {"*:Kinnate/*:CustomData[*:Gender='Male']", "triangle"},
+                    {"*:Kinnate/*:CustomData[*:Gender='']", "square"},
+                    {"*:Kinnate/*:CustomData[*:DateOfDeath!='']", "blackstrikethrough"}
+                });
         QueryBuilder instance = new QueryBuilder();
         String expResult = "(\"one&amp;two\",\"single'quote\",\"double&quot;quote\",\"*:Kinnate/*:CustomData[*:Gender='Male']\",\"*:Kinnate/*:CustomData[*:Gender='']\",\"*:Kinnate/*:CustomData[*:DateOfDeath!='']\")";
         String result = instance.asSequenceString(indexerParam);
@@ -61,160 +59,68 @@ public class QueryBuilderTest extends TestCase {
      * Test of getLabelsClause method, of class QueryBuilder.
      */
     public void testGetLabelsClause() {
-        System.out.println("getLabelsClause");
-        IndexerParameters indexParameters = null;
-        String docRootVar = "";
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getLabelsClause(indexParameters, docRootVar);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        /*
+         * todo: this test needs to allow complex xpath queries eg "[starts-with(local-name(), 'Name')]"
+         * the escapeBadChars method is not adequate because this is an unquoted xpath
+         * so all xpath characters need to be allowed, etc.
+         */
+//        System.out.println("getLabelsClause");
+//        IndexerParameters indexParameters = new IndexerParameters();
+//        indexParameters.labelFields = new IndexerParam(new String[][]{
+//                    {"*:Kinnate/*:CustomData/*:Type"},
+//                    {"*:Kinnate/*:CustomData/*[starts-with(local-name(), 'Name')]"},
+//                    {"one&two"},
+//                    {"single'quote"},
+//                    {"double\"quote"}
+//                });
+//        String docRootVar = "docrootvar";
+//        QueryBuilder instance = new QueryBuilder();
+//        String result = instance.getLabelsClause(indexParameters, docRootVar);
+//        System.out.println("result: " + result);
+//        assertEquals(result.indexOf("\""), -1);
+//        assertEquals(result.indexOf("&"), -1);
     }
 
     /**
      * Test of getDatesClause method, of class QueryBuilder.
      */
-    public void testGetDatesClause() {
-        System.out.println("getDatesClause");
-        IndexerParameters indexParameters = null;
-        String docRootVar = "";
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getDatesClause(indexParameters, docRootVar);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+//    public void testGetDatesClause() {
+//        System.out.println("getDatesClause");
+//        IndexerParameters indexParameters = null;
+//        String docRootVar = "";
+//        QueryBuilder instance = new QueryBuilder();
+//        String expResult = "";
+//        String result = instance.getDatesClause(indexParameters, docRootVar);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     /**
      * Test of getSymbolClause method, of class QueryBuilder.
      */
-    public void testGetSymbolClause() {
-        System.out.println("getSymbolClause");
-        IndexerParameters indexParameters = null;
-        String docRootVar = "";
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getSymbolClause(indexParameters, docRootVar);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getArchiveLinksClause method, of class QueryBuilder.
-     */
-    public void testGetArchiveLinksClause() {
-        System.out.println("getArchiveLinksClause");
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getArchiveLinksClause();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getRelationQuery method, of class QueryBuilder.
-     */
-    public void testGetRelationQuery() {
-        System.out.println("getRelationQuery");
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getRelationQuery();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEntityQueryReturn method, of class QueryBuilder.
-     */
-    public void testGetEntityQueryReturn() {
-        System.out.println("getEntityQueryReturn");
-        IndexerParameters indexParameters = null;
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getEntityQueryReturn(indexParameters);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEntityByEndPointQuery method, of class QueryBuilder.
-     */
-    public void testGetEntityByEndPointQuery() {
-        System.out.println("getEntityByEndPointQuery");
-        RelationType relationType = null;
-        IndexerParameters indexParameters = null;
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getEntityByEndPointQuery(relationType, indexParameters);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+//    public void testGetSymbolClause() {
+//        System.out.println("getSymbolClause");
+//        IndexerParameters indexParameters = null;
+//        String docRootVar = "";
+//        QueryBuilder instance = new QueryBuilder();
+//        String expResult = "";
+//        String result = instance.getSymbolClause(indexParameters, docRootVar);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     /**
      * Test of getEntityByKeyWordQuery method, of class QueryBuilder.
      */
     public void testGetEntityByKeyWordQuery() {
         System.out.println("getEntityByKeyWordQuery");
-        String keyWords = "";
-        IndexerParameters indexParameters = null;
+        String keyWords = "one two one&two single'quote double\"quote";
+        IndexerParameters indexParameters = new IndexerParameters();
         QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
         String result = instance.getEntityByKeyWordQuery(keyWords, indexParameters);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEntityWithRelationsQuery method, of class QueryBuilder.
-     */
-    public void testGetEntityWithRelationsQuery() {
-        System.out.println("getEntityWithRelationsQuery");
-        UniqueIdentifier uniqueIdentifier = null;
-        String[] excludeUniqueIdentifiers = null;
-        IndexerParameters indexParameters = null;
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getEntityWithRelationsQuery(uniqueIdentifier, excludeUniqueIdentifiers, indexParameters);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEntityPath method, of class QueryBuilder.
-     */
-    public void testGetEntityPath() {
-        System.out.println("getEntityPath");
-        UniqueIdentifier uniqueIdentifier = null;
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getEntityPath(uniqueIdentifier);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEntityQuery method, of class QueryBuilder.
-     */
-    public void testGetEntityQuery() {
-        System.out.println("getEntityQuery");
-        UniqueIdentifier uniqueIdentifier = null;
-        IndexerParameters indexParameters = null;
-        QueryBuilder instance = new QueryBuilder();
-        String expResult = "";
-        String result = instance.getEntityQuery(uniqueIdentifier, indexParameters);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(result.indexOf("one&two"), -1);
+//        assertEquals(result.indexOf("single'quote"), -1); // single quotes are fine because we use double quotes for the query strings
+        assertEquals(result.indexOf("double\"quote"), -1);
     }
 
     /**
