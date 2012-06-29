f61f745cf79cab3f884ec7d10f486f749f2051c8
Peter Withers <Peter.Withers@mpi.nl>
2012-06-29 15:13:32 +0000

    Added intersection detection code for the sanguine relation lines. Refactored the relation line classes. refs #1220 #1221

--- desktop/src/main/java/nl/mpi/kinnate/svg/RelationSvg.java
+++ desktop/src/main/java/nl/mpi/kinnate/svg/RelationSvg.java
@@ -1,14 +1,10 @@
 package nl.mpi.kinnate.svg;
 
-import java.awt.Point;
 import java.util.ArrayList;
 import nl.mpi.arbil.util.MessageDialogHandler;
 import nl.mpi.kinnate.kindata.DataTypes;
 import nl.mpi.kinnate.kindata.EntityData;
 import nl.mpi.kinnate.kindata.EntityRelation;
-import nl.mpi.kinnate.kindata.RelationTypeDefinition.CurveLineOrientation;
-import nl.mpi.kinnate.svg.relationlines.LineLookUpTable;
-import nl.mpi.kinnate.svg.relationlines.LineRecord;
 import nl.mpi.kinnate.svg.relationlines.RelationRecord;
 import nl.mpi.kinnate.uniqueidentifiers.IdentifierException;
 import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
@@ -61,199 +57,6 @@ public class RelationSvg {
             textParentNode.appendChild(textPath);
         }
     }
-    private boolean oldFormatWarningShown = false;
-
-    protected LineRecord setPolylinePointsAttribute(LineLookUpTable lineLookUpTable, String lineIdString, DataTypes.RelationType relationType, float vSpacing, float egoX, float egoY, float alterX, float alterY, float[] averageParentPassed) {
-        //float midY = (egoY + alterY) / 2;
-        // todo: Ticket #1064 when an entity is above one that it should be below the line should make a zigzag to indicate it        
-        ArrayList<Point> initialPointsList = new ArrayList<Point>();
-        float[] averageParent = null;
-        float midSpacing = vSpacing / 2;
-//        float parentSpacing = 10;
-        float egoYmid;
-        float alterYmid;
-        float centerX = (egoX + alterX) / 2;
-        switch (relationType) {
-            case ancestor:
-                if (averageParentPassed == null) {
-                    // if no parent location has been provided then just use the current parent
-                    averageParent = new float[]{alterX, alterY};
-                } else {
-                    // todo: this is filtering out the parent location for non ancestor relations, but it would be more efficient to no get the parent location unless required
-                    averageParent = averageParentPassed;
-                }
-                egoYmid = egoY - midSpacing;
-                alterYmid = averageParent[1] + 30;
-//                alterYmid = alterY + midSpacing;
-//                egoYmid = alterYmid + 30;
-                centerX = (egoYmid < alterYmid) ? centerX : egoX;
-                centerX = (egoY < alterY && egoX == alterX) ? centerX - midSpacing : centerX;
-                break;
-//                float tempX = egoX;
-//                float tempY = egoY;
-//                egoX = alterX;
-//                egoY = alterY;
-//                alterX = tempX;
-//                alterY = tempY;
-            case descendant:
-                if (!oldFormatWarningShown) {
-                    dialogHandler.addMessageDialogToQueue("This diagram needs to be updated, select recalculate diagram from the edit menu before continuing.", "Old or erroneous format detected");
-                    oldFormatWarningShown = true;
-                }
-//                targetNode.getParentNode().getParentNode().getParentNode().getParentNode().removeChild(targetNode.getParentNode().getParentNode().getParentNode());
-                throw new UnsupportedOperationException("in order to simplify section, the ancestor relations should be swapped so that ego is the parent");
-//                return;
-//                throw new UnsupportedOperationException("in order to simplify section, the ancestor relations should be swapped so that ego is the parent");
-//                egoYmid = egoY + midSpacing;
-//                alterYmid = alterY - midSpacing;
-//                centerX = (egoYmid < alterYmid) ? alterX : centerX;
-//                centerX = (egoY > alterY && egoX == alterX) ? centerX - midSpacing : centerX;
-//                break;
-            case sibling:
-                egoYmid = egoY - midSpacing;
-                alterYmid = alterY - midSpacing;
-                centerX = (egoY < alterY) ? alterX : egoX;
-                centerX = (egoX == alterX) ? centerX - midSpacing : centerX;
-                break;
-            case union:
-//                float unionMid = (egoY > alterY) ? egoY : alterY;
-                egoYmid = egoY + 30;
-                alterYmid = alterY + 30;
-                centerX = (egoY < alterY) ? egoX : alterX;
-                centerX = (egoX == alterX) ? centerX - midSpacing : centerX;
-                break;
-//            case affiliation:
-//            case none:
-            default:
-                egoYmid = egoY;
-                alterYmid = alterY;
-                break;
-        }
-//        if (alterY == egoY) {
-//            // make sure that union lines go below the entities and sibling lines go above
-//            if (relationType == DataTypes.RelationType.sibling) {
-//                midY = alterY - vSpacing / 2;
-//            } else if (relationType == DataTypes.RelationType.union) {
-//                midY = alterY + vSpacing / 2;
-//            }
-//        }
-
-        initialPointsList.add(new Point((int) egoX, (int) egoY));
-        initialPointsList.add(new Point((int) egoX, (int) egoYmid));
-
-        if (averageParent != null) {
-            float averageParentX = averageParent[0];
-//            float minParentY = averageParent[1];
-            initialPointsList.add(new Point((int) averageParentX, (int) egoYmid));
-            initialPointsList.add(new Point((int) averageParentX, (int) alterYmid));
-        } else {
-            initialPointsList.add(new Point((int) centerX, (int) egoYmid));
-            initialPointsList.add(new Point((int) centerX, (int) alterYmid));
-        }
-        initialPointsList.add(new Point((int) alterX, (int) alterYmid));
-        initialPointsList.add(new Point((int) alterX, (int) alterY));
-
-        if (lineLookUpTable != null) {
-            // this version is used when the relations are drawn on the diagram
-            // or when an entity is dragged before the diagram is redrawn in the case of a reloaded from disk diagram (this case is sub optimal in that on first load the loops will not be drawn)
-            return lineLookUpTable.adjustLineToObstructions(lineIdString, initialPointsList);
-        } else {
-            // this version is used when the relation drag handles are used
-//           return new LineLookUpTable.LineRecord(lineIdString, initialPointsList.toArray(new Point[]{}));
-            throw new UnsupportedOperationException("lineLookUpTable == null, this is not yet supported");
-        }
-    }
-
-    protected String setPathPointsAttribute(CurveLineOrientation curveLineOrientation, float hSpacing, float vSpacing, float egoX, float egoY, float alterX, float alterY) {
-        float fromBezX;
-        float fromBezY;
-        float toBezX;
-        float toBezY;
-        if ((egoX > alterX && egoY < alterY) || (egoX > alterX && egoY > alterY)) {
-            // prevent the label on the line from rendering upside down
-            float tempX = alterX;
-            float tempY = alterY;
-            alterX = egoX;
-            alterY = egoY;
-            egoX = tempX;
-            egoY = tempY;
-        }
-        // todo: if this line is too straight then add a curve by tweeking the handles
-        if (curveLineOrientation == CurveLineOrientation.vertical) {
-            fromBezX = egoX;
-            toBezX = alterX;
-            if (egoY > alterY) {
-                if (egoY - alterY < hSpacing / 4) {
-                    fromBezY = egoY - hSpacing / 4;
-                    toBezY = alterY - hSpacing / 4;
-                } else {
-                    fromBezY = (egoY - alterY) / 2 + alterY;
-                    toBezY = (egoY - alterY) / 2 + alterY;
-                }
-            } else {
-                if (alterY - egoY < hSpacing / 4) {
-                    fromBezY = egoY + hSpacing / 4;
-                    toBezY = alterY + hSpacing / 4;
-                } else {
-                    fromBezY = (alterY - egoY) / 2 + egoY;
-                    toBezY = (alterY - egoY) / 2 + egoY;
-                }
-            }
-//            System.out.println("egoY: " + egoY + " alterY: " + alterY);
-            final float distanceX = Math.abs(egoX - alterX);
-//            System.out.println("distanceY: " + distanceY);
-            if (distanceX < hSpacing / 4) {
-//                System.out.println("needs curve added");
-                boolean quadrantType = egoY > alterY == egoY > alterY; // top left and bottom right need to be handled differently from top right and bottom left                
-//                System.out.println("quadrantType: " + quadrantType);
-                final float curveToAdd = hSpacing / 8;
-                if (quadrantType) {
-                    fromBezX -= curveToAdd;
-                    toBezX += curveToAdd;
-                } else {
-                    fromBezX += curveToAdd;
-                    toBezX -= curveToAdd;
-                }
-            }
-        } else {
-            fromBezY = egoY;
-            toBezY = alterY;
-            if (egoX > alterX) {
-                if (egoX - alterX < hSpacing / 4) {
-                    fromBezX = egoX - hSpacing / 4;
-                    toBezX = alterX - hSpacing / 4;
-                } else {
-                    fromBezX = (egoX - alterX) / 2 + alterX;
-                    toBezX = (egoX - alterX) / 2 + alterX;
-                }
-            } else {
-                if (alterX - egoX < hSpacing / 4) {
-                    fromBezX = egoX + hSpacing / 4;
-                    toBezX = alterX + hSpacing / 4;
-                } else {
-                    fromBezX = (alterX - egoX) / 2 + egoX;
-                    toBezX = (alterX - egoX) / 2 + egoX;
-                }
-            }
-//            System.out.println("egoY: " + egoY + " alterY: " + alterY);
-            final float distanceY = Math.abs(egoY - alterY);
-//            System.out.println("distanceY: " + distanceY);
-            if (distanceY < hSpacing / 4) {
-//                System.out.println("needs curve added");
-                boolean quadrantType = egoX > alterX == egoY > alterY; // top left and bottom right need to be handled differently from top right and bottom left                
-//                System.out.println("quadrantType: " + quadrantType);
-                final float curveToAdd = hSpacing / 8;
-                if (quadrantType) {
-                    fromBezY -= curveToAdd;
-                    toBezY += curveToAdd;
-                } else {
-                    fromBezY += curveToAdd;
-                    toBezY -= curveToAdd;
-                }
-            }
-        }
-        return "M " + egoX + "," + egoY + " C " + fromBezX + "," + fromBezY + " " + toBezX + "," + toBezY + " " + alterX + "," + alterY;
-    }
 
     public boolean hasCommonParent(EntityData currentNode, EntityRelation graphLinkNode) {
         if (graphLinkNode.getRelationType() == DataTypes.RelationType.sibling) {
@@ -301,7 +104,7 @@ public class RelationSvg {
             Element groupNode = graphPanel.doc.createElementNS(graphPanel.svgNameSpace, "g");
             groupNode.setAttribute("id", relationRecord.idString);
             Element defsNode = graphPanel.doc.createElementNS(graphPanel.svgNameSpace, "defs");
-            new DataStoreSvg().storeRelationParameters(graphPanel.doc, relationGroupNode, relationRecord.directedRelation, relationRecord.curveLineOrientation, relationRecord.leftEntity.getUniqueIdentifier(), relationRecord.rightEntity.getUniqueIdentifier());
+            new DataStoreSvg().storeRelationParameters(graphPanel.doc, groupNode, relationRecord.directedRelation, relationRecord.curveLineOrientation, relationRecord.leftEntity.getUniqueIdentifier(), relationRecord.rightEntity.getUniqueIdentifier());
             boolean addedRelationLine = false;
             Element linkLine;
             if (relationRecord.curveLinePoints != null) {
@@ -406,14 +209,18 @@ public class RelationSvg {
 //                        float alterY = alterSymbolRect.getY() + alterSymbolRect.getHeight() / 2;
 
                             if ("polyline".equals(relationLineElement.getLocalName())) {
-                                //System.out.println("polyline to update: " + lineElementId);
-                                LineRecord lineRecord = setPolylinePointsAttribute(graphPanel.lineLookUpTable, lineElementId, directedRelation, vSpacing, egoX, egoY, alterX, alterY, parentPoint);
-                                relationLineElement.setAttribute("points", lineRecord.getPointsAttribute());
+                                try {
+                                    //System.out.println("polyline to update: " + lineElementId);
+                                    RelationRecord relationRecord = new RelationRecord(/* graphPanel.lineLookUpTable, */lineElementId, directedRelation, vSpacing, egoX, egoY, alterX, alterY, parentPoint);
+                                    relationLineElement.setAttribute("points", relationRecord.lineRecord.getPointsAttribute());
+                                } catch (OldFormatException exception) {
+                                    dialogHandler.addMessageDialogToQueue(exception.getMessage(), "Old or erroneous format detected");
+                                }
                             }
                             if ("path".equals(relationLineElement.getLocalName())) {
                                 //System.out.println("path to update: " + relationLineElement.getLocalName());
-                                String curveLinePoints = setPathPointsAttribute(graphRelationData.curveLineOrientation, hSpacing, vSpacing, egoX, egoY, alterX, alterY);
-                                relationLineElement.setAttribute("d", curveLinePoints);
+                                RelationRecord relationRecord = new RelationRecord(graphRelationData.curveLineOrientation, hSpacing, vSpacing, egoX, egoY, alterX, alterY);
+                                relationLineElement.setAttribute("d", relationRecord.curveLinePoints);
                             }
                             addUseNode(graphPanel.doc, graphPanel.svgNameSpace, (Element) currentChild, lineElementId);
                             updateLabelNode(graphPanel.doc, graphPanel.svgNameSpace, lineElementId, idAttrubite.getNodeValue());
