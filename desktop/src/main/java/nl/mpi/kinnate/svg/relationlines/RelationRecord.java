bccf1cbd4eb8f32c3e467fd275fc31debd6b5e65
Peter Withers <Peter.Withers@mpi.nl>
2012-06-29 15:14:11 +0000

    Added intersection detection code for the sanguine relation lines. Refactored the relation line classes. refs #1220 #1221

--- desktop/src/main/java/nl/mpi/kinnate/svg/relationlines/RelationRecord.java
+++ desktop/src/main/java/nl/mpi/kinnate/svg/relationlines/RelationRecord.java
@@ -32,6 +32,14 @@ public class RelationRecord {
     public LineRecord lineRecord;
     public String curveLinePoints = null;
 
+    public RelationRecord(String lineIdString, DataTypes.RelationType relationType, float vSpacing, float egoX, float egoY, float alterX, float alterY, float[] averageParentPassed) throws OldFormatException {
+        lineRecord = setPolylinePointsAttribute(lineIdString, relationType, vSpacing, egoX, egoY, alterX, alterY, averageParentPassed);
+    }
+
+    public RelationRecord(RelationTypeDefinition.CurveLineOrientation curveLineOrientation, float hSpacing, float vSpacing, float egoX, float egoY, float alterX, float alterY) {
+        curveLinePoints = setPathPointsAttribute(curveLineOrientation, hSpacing, vSpacing, egoX, egoY, alterX, alterY);
+    }
+
     public RelationRecord(GraphPanel graphPanel, int relationLineIndex, EntityData leftEntity, EntityData rightEntity, DataTypes.RelationType directedRelation, int lineWidth, int lineDash, RelationTypeDefinition.CurveLineOrientation curveLineOrientation, String lineColour, String lineLabel, int hSpacing, int vSpacing) throws OldFormatException {
         this.graphPanel = graphPanel;
         this.leftEntity = leftEntity;
