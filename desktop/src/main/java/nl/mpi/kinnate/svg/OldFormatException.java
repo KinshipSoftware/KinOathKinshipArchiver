a89a1ddc79b6c0e680bbcc85e7b0eb84c2464ca1
Peter Withers <Peter.Withers@mpi.nl>
2012-06-29 15:13:40 +0000

    Added intersection detection code for the sanguine relation lines. Refactored the relation line classes. refs #1220 #1221

---
+++ desktop/src/main/java/nl/mpi/kinnate/svg/OldFormatException.java
@@ -0,0 +1,13 @@
+package nl.mpi.kinnate.svg;
+
+/**
+ * Document : OldFormatException
+ * Created on : Jun 29, 2012, 3:21:12 PM
+ * Author : Peter Withers
+ */
+public class OldFormatException extends Exception {
+
+    public OldFormatException(String message) {
+        super(message);
+    }
+}
