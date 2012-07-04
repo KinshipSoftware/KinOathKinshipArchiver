a2e0daeab93af2177e5b7926ededf055de85efdd
Peter Withers <Peter.Withers@mpi.nl>
2012-07-04 15:00:13 +0000

    Created a new project for the plugin core interfaces.

---
+++ plugins/core/src/main/java/nl/mpi/kinnate/entityindexer/QueryException.java
@@ -0,0 +1,13 @@
+package nl.mpi.kinnate.entityindexer;
+
+/**
+ * Document : QueryException
+ * Created on : Jul 4, 2012, 4:23:37 PM
+ * Author : Peter Withers
+ */
+public class QueryException extends Exception {
+
+    public QueryException(String message) {
+        super(message);
+    }
+}
