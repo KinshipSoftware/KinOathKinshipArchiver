db42367a0c99bcaea6488bd5273a3d057435f9a0
Peter Withers <Peter.Withers@mpi.nl>
2012-07-10 11:35:59 +0000

    Refactored the relation line classes in preparation for the loops and line separation code. refs #1220 #1221

--- desktop/src/main/java/nl/mpi/kinnate/svg/relationlines/RelationRecordTable.java
+++ desktop/src/main/java/nl/mpi/kinnate/svg/relationlines/RelationRecordTable.java
@@ -2,6 +2,11 @@ package nl.mpi.kinnate.svg.relationlines;
 
 import java.util.Collection;
 import java.util.HashMap;
+import nl.mpi.kinnate.kindata.DataTypes;
+import nl.mpi.kinnate.kindata.EntityData;
+import nl.mpi.kinnate.kindata.RelationTypeDefinition;
+import nl.mpi.kinnate.svg.GraphPanel;
+import nl.mpi.kinnate.svg.OldFormatException;
 
 /**
  * Document : RelationRecordTable
@@ -11,8 +16,10 @@ import java.util.HashMap;
 public class RelationRecordTable {
 
     HashMap<String, RelationRecord> recordStore = new HashMap<String, RelationRecord>();
+    LineLookUpTable lineLookUpTable = new LineLookUpTable();
 
-    public void addRecord(RelationRecord relationRecord) {
+    public void addRecord(GraphPanel graphPanel, int relationLineIndex, EntityData leftEntity, EntityData rightEntity, DataTypes.RelationType directedRelation, int lineWidth, int lineDash, RelationTypeDefinition.CurveLineOrientation curveLineOrientation, String lineColour, String lineLabel, int hSpacing, int vSpacing) throws OldFormatException {
+        RelationRecord relationRecord = new RelationRecord(graphPanel, lineLookUpTable, relationLineIndex, leftEntity, rightEntity, directedRelation, lineWidth, lineDash, curveLineOrientation, lineColour, lineLabel, hSpacing, vSpacing);
         recordStore.put(relationRecord.lineIdString, relationRecord);
     }
 
@@ -26,5 +33,11 @@ public class RelationRecordTable {
 
     public int size() {
         return recordStore.size();
+    }
+
+    public void adjustLines() {
+        // todo: update all entity positions
+        
+        lineLookUpTable.addLoops();
     }
 }
