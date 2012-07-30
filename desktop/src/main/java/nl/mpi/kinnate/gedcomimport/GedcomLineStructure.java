a073a9a6397af0c194ba45ccca43ad1a8473ce33
Peter Withers <Peter.Withers@mpi.nl>
2012-07-30 11:46:05 +0000

    Started to add an importer for TIP format by making the gedcom importer more generic. refs #2193

--- desktop/src/main/java/nl/mpi/kinnate/gedcomimport/GedcomLineStructure.java
+++ desktop/src/main/java/nl/mpi/kinnate/gedcomimport/GedcomLineStructure.java
@@ -9,20 +9,68 @@ import java.util.ArrayList;
  */
 public class GedcomLineStructure extends ImportLineStructure {
 
-    public GedcomLineStructure(String lineString, ArrayList<String> gedcomLevelStrings) {
+    public GedcomLineStructure(String lineString, ArrayList<String> gedcomLevelStrings) throws ImportException {
         super(lineString, gedcomLevelStrings);
         isFileHeader = lineString.startsWith("0 HEAD");
         String[] lineParts = lineString.split(" ", 3);
         gedcomLevel = Integer.parseInt(lineParts[0]);
-        currentName = lineParts[1];
+        if (!isFileHeader && gedcomLevel == 0) {
+            currentID = lineParts[1];
+            if (lineParts.length > 2) {
+                setType(lineParts[2]);
+                if (!currentID.startsWith("@") || !currentID.endsWith("@")) {
+                    throw new ImportException("Incorrect gedcom identifier format: " + currentID);
+                }
+            }
+        } else {
+            currentID = lineParts[1];
+            currentName = lineParts[1];
+            lineContents = "";
+            if (lineParts.length > 2) {
+                lineContents = lineParts[2];
+            }
+        }
         while (gedcomLevelStrings.size() > gedcomLevel) {
             gedcomLevelStrings.remove(gedcomLevelStrings.size() - 1);
         }
-        lineContents = "";
-        if (lineParts.length > 2) {
-            lineContents = lineParts[2];
-        }
         gedcomLevelStrings.add(currentName);
+    }
+
+    private void setType(String getdomType) {
+        if (getdomType.equals("NOTE")) {
+            entityType = "Gedcom Note";
+        } else if (getdomType.equals("FAM")) {
+            entityType = "Gedcom Family Group";
+        } else if (getdomType.equals("INDI")) {
+            // do not set a type for individuals
+        } else if (getdomType.equals("OBJE")) {
+            entityType = "Resource File";
+        } else if (getdomType.equals("REPO")) {
+            entityType = "Repository";
+        } else if (getdomType.equals("SUBN")) {
+            entityType = "Submission";
+        } else if (getdomType.equals("SOUR")) {
+            entityType = "Source";
+        } else if (getdomType.equals("SUBM")) {
+            entityType = "Submitter";
+        } else {
+            entityType = getdomType;
+        }
+    }
+
+    @Override
+    public boolean isContineLine() {
+        return currentName != null && currentName.equals("CONC");
+    }
+
+    @Override
+    public boolean isContineLineBreak() {
+        return currentName != null && currentName.equals("CONT");
+    }
+
+    @Override
+    public boolean isEndOfFileMarker() {
+        return currentID != null && currentID.equals("TRLR");
     }
 
     public boolean isRelation() {
