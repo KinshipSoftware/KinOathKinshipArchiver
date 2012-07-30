ea80e7c5fdaa960050aac37e5a2ebae89a141f66
Peter Withers <Peter.Withers@mpi.nl>
2012-07-30 14:58:28 +0000

    Started to add an importer for TIP format by making the gedcom importer more generic. refs #2193

--- desktop/src/main/java/nl/mpi/kinnate/gedcomimport/TipImporter.java
+++ desktop/src/main/java/nl/mpi/kinnate/gedcomimport/TipImporter.java
@@ -1,8 +1,10 @@
 package nl.mpi.kinnate.gedcomimport;
 
+import java.util.ArrayList;
 import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
 import nl.mpi.arbil.userstorage.SessionStorage;
+import nl.mpi.kinnate.kindocument.ImportTranslator;
 
 /**
  * Document : TipImporter
@@ -13,6 +15,22 @@ public class TipImporter extends GedcomImporter {
 
     public TipImporter(JProgressBar progressBarLocal, JTextArea importTextAreaLocal, boolean overwriteExistingLocal, SessionStorage sessionStorage) {
         super(progressBarLocal, importTextAreaLocal, overwriteExistingLocal, sessionStorage);
+    }
+
+    @Override
+    protected ImportTranslator getImportTranslator() {
+        ImportTranslator importTranslator = new ImportTranslator(true);
+        // todo: add the translator values if required
+//        importTranslator.addTranslationEntry("SEX", "F", "Gender", "Female");
+//        importTranslator.addTranslationEntry("SEX", "M", "Gender", "Male");
+//        importTranslator.addTranslationEntry("NAME", null, "Name", null);
+//        importTranslator.addTranslationEntry("chro", null, "Chromosome", null);
+        return importTranslator;
+    }
+
+    @Override
+    protected ImportLineStructure getImportLineStructure(String lineString, ArrayList<String> gedcomLevelStrings) throws ImportException {
+        return new TipLineStructure(lineString, gedcomLevelStrings);
     }
 
     @Override
