33ab7d9b8845223a80eec920454489211886a44c
Peter Withers <Peter.Withers@mpi.nl>
2012-08-06 13:15:19 +0000

    Created a test project for a Arbil metadata search plugin.

--- plugins/metadatasearch/src/main/java/db/MetadataFileType.java
+++ plugins/metadatasearch/src/main/java/db/MetadataFileType.java
@@ -1,19 +1,32 @@
 package db;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Document : MetadataFileType
  * Created on : Aug 6, 2012, 1:37:47 PM
  * Author : Peter Withers
  */
+@XmlRootElement(name = "MetadataFileType")
 public class MetadataFileType {
 
+    @XmlElement(name = "rootXpath")
     final private String rootXpath;
+    @XmlElement(name = "displayString")
     final private String displayString;
+    @XmlElement(name = "recordCount")
     private int recordCount = 0;
+    @XmlElement(name = "childMetadataTypes")
+    private MetadataFileType[] childMetadataTypes = null;
 
     public MetadataFileType(String rootXpath, String displayString) {
         this.rootXpath = rootXpath;
         this.displayString = displayString;
+    }
+
+    public MetadataFileType[] getChildMetadataTypes() {
+        return childMetadataTypes;
     }
 
     @Override
