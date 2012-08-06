92b86f50b932dffc0513a14b47ff01f0e6298761
Peter Withers <Peter.Withers@mpi.nl>
2012-08-06 13:19:47 +0000

    Created a test project for a Arbil metadata search plugin.

--- plugins/metadatasearch/src/main/java/db/ArbilDatabase.java
+++ plugins/metadatasearch/src/main/java/db/ArbilDatabase.java
@@ -1,11 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package db;
 
 import java.io.File;
+import java.io.StringReader;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbil.util.BugCatcherManager;
+import nl.mpi.arbil.util.MessageDialogHandler;
 import nl.mpi.kinnate.entityindexer.QueryException;
 import org.basex.core.BaseXException;
 import org.basex.core.Context;
@@ -14,6 +17,7 @@ import org.basex.core.cmd.CreateDB;
 import org.basex.core.cmd.DropDB;
 import org.basex.core.cmd.Open;
 import org.basex.core.cmd.Set;
+import org.basex.core.cmd.XQuery;
 
 /**
  * Document : ArbilDatabase
@@ -25,8 +29,12 @@ public class ArbilDatabase {
     static Context context = new Context();
     static final Object databaseLock = new Object();
     private final String databaseName = "ArbilDatabase";
+    final private SessionStorage sessionStorage;
+    final private MessageDialogHandler dialogHandler;
 
-    public ArbilDatabase() {
+    public ArbilDatabase(SessionStorage sessionStorage, MessageDialogHandler dialogHandler) {
+        this.sessionStorage = sessionStorage;
+        this.dialogHandler = dialogHandler;
         try {
             synchronized (databaseLock) {
                 new Open(databaseName).execute(context);
@@ -44,7 +52,7 @@ public class ArbilDatabase {
     }
 
     public void createDatabase(File directoryOfInputFiles) throws QueryException {
-            String suffixFilter = "*.*mdi";
+        String suffixFilter = "*.*mdi";
         try {
             synchronized (databaseLock) {
                 new DropDB(databaseName).execute(context);
@@ -54,5 +62,44 @@ public class ArbilDatabase {
         } catch (BaseXException exception) {
             throw new QueryException(exception.getMessage());
         }
+    }
+
+    private String getQuery(){
+        return "for $xpathString in distinct-values(\n"
+                + "for $entityNode in collection('SimpleExportTemp')/*\n"
+                + "return path($entityNode)\n"
+                + ")\n"
+                + "return"
+                + "$xpathString";
+    }
+    public MetadataFileType[] getMetadataTypes(MetadataFileType metadataFileType[]) {
+        long startTime = System.currentTimeMillis();
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(MetadataFileType.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            String queryResult;
+            synchronized (databaseLock) {
+                queryResult = new XQuery(getQuery()).execute(context);
+            }
+//            System.out.println("queryResult: " + queryResult);
+            MetadataFileType foundEntities = (MetadataFileType) unmarshaller.unmarshal(new StreamSource(new StringReader(queryResult)), MetadataFileType.class).getValue();
+            long queryMils = System.currentTimeMillis() - startTime;
+            final MetadataFileType[] entityDataArray = foundEntities.getChildMetadataTypes();
+            int resultCount = 0;
+            if (entityDataArray != null) {
+                resultCount = entityDataArray.length;
+            }
+            String queryTimeString = "Query time: " + queryMils + "ms for " + resultCount + " entities";
+            System.out.println(queryTimeString);
+//            selectedEntity.appendTempLabel(queryTimeString);
+            return foundEntities.getChildMetadataTypes();
+        } catch (JAXBException exception) {
+            BugCatcherManager.getBugCatcher().logError(exception);
+            dialogHandler.addMessageDialogToQueue("Error getting search options", "Search Options");
+        } catch (BaseXException exception) {
+            BugCatcherManager.getBugCatcher().logError(exception);
+            dialogHandler.addMessageDialogToQueue("Error getting search options", "Search Options");
+        }
+        return new MetadataFileType[]{};
     }
 }
