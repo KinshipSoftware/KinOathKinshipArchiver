de6cef04554c045488a8ce387d16812d51bc319d
Peter Withers <Peter.Withers@mpi.nl>
2012-08-13 16:17:48 +0000

    Created a plugin loader project that will provide a plugin selection menu, that will be used by host applications such as Arbil and KinOath. #2248

--- desktop/src/main/java/nl/mpi/kinnate/ui/menu/MainMenuBar.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/menu/MainMenuBar.java
@@ -5,7 +5,10 @@ import javax.swing.JMenuBar;
 import nl.mpi.arbil.ui.ArbilWindowManager;
 import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbil.util.ApplicationVersionManager;
+import nl.mpi.kinnate.plugins.KinOathPluginManager;
 import nl.mpi.kinnate.ui.window.AbstractDiagramManager;
+import nl.mpi.pluginloader.PluginService;
+import nl.mpi.pluginloader.ui.PluginMenu;
 
 /**
  * Document : MainMenuBar
@@ -21,6 +24,7 @@ public class MainMenuBar extends JMenuBar {
         this.add(new KinTermsMenu(abstractDiagramManager, parentComponent));
         this.add(new ArchiveMenu(abstractDiagramManager, parentComponent));
         this.add(new DiagramPanelsMenu(abstractDiagramManager, parentComponent));
+        this.add(new PluginMenu(PluginService.getInstance(), new KinOathPluginManager(abstractDiagramManager, dialogHandler, parentComponent)));
         this.add(new WindowMenu(abstractDiagramManager, parentComponent));
         this.add(new HelpMenu(abstractDiagramManager, dialogHandler, sessionStorage, versionManager));
     }
