6964de9c6e39ee2e8b2cddd3764bbcf9573a348a
Peter Withers <Peter.Withers@mpi.nl>
2012-08-13 12:33:59 +0000

    Created a plugin loader maven project for the search plugin et al..

---
+++ plugins/pluginloader/src/main/java/nl/mpi/pluginloader/App.java
@@ -0,0 +1,13 @@
+package nl.mpi.pluginloader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
