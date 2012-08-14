89b121a6d003e69168ac0afd8c29ab3bd1ff61ab
Peter Withers <Peter.Withers@mpi.nl>
2012-08-14 11:15:21 +0000

    Created a plugin loader project that will provide a plugin selection menu, that will be used by host applications such as Arbil and KinOath. #2248

--- plugins/pluginloader/src/main/java/nl/mpi/pluginloader/PluginSettings.java
+++ plugins/pluginloader/src/main/java/nl/mpi/pluginloader/PluginSettings.java
@@ -1,12 +1,14 @@
 package nl.mpi.pluginloader;
 
 import javax.swing.table.TableModel;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Document : PluginSettings
  * Created on : Dec 30, 2011, 3:04:27 PM
  * Author : Peter Withers
  */
+@XmlRootElement(name = "PluginSettings")
 public interface PluginSettings extends TableModel {
 // This class should be used to store run time settings for the plugin and should be annotated for jaxb
 }
