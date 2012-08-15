7759eb4c56a361263c1a60c934382f69eb2f3fc2
Peter Withers <Peter.Withers@mpi.nl>
2012-08-15 12:24:48 +0000

    Created a shared "core plugin" project to hold the required interfaces so that the plugins can use Abil and KinOath components. refs #2248 #2097

---
+++ plugins/core/src/main/java/nl/mpi/arbil/plugin/PluginSessionStorage.java
@@ -0,0 +1,10 @@
+package nl.mpi.arbil.plugin;
+
+/**
+ * Document : PluginSessionStorage <br>
+ * Created on Aug 15, 2012, 2:10:56 PM <br>
+ * @author Peter Withers <br>
+ */
+public interface PluginSessionStorage {
+
+}
