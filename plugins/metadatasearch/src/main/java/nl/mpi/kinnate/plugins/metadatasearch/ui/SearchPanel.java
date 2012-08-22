826231333d2f4258f507b3aa744fe7d4011b5f7d
Peter Withers <Peter.Withers@mpi.nl>
2012-08-22 10:00:47 +0000

    Added a prototype search module providing similar features as the search in Arbil. refs #2233

--- plugins/metadatasearch/src/main/java/nl/mpi/kinnate/plugins/metadatasearch/ui/SearchPanel.java
+++ plugins/metadatasearch/src/main/java/nl/mpi/kinnate/plugins/metadatasearch/ui/SearchPanel.java
@@ -7,6 +7,9 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import nl.mpi.arbil.data.ArbilNode;
 import nl.mpi.arbil.plugin.PluginDialogHandler;
 import nl.mpi.arbil.ui.ArbilNodeSearchColumnComboBox;
@@ -27,7 +30,6 @@ public class SearchPanel extends JPanel implements ActionListener {
 
     final private ArbilDatabase arbilDatabase;
     final PluginDialogHandler arbilWindowManager;
-    final JPanel lowerPanel;
     final JFrame jFrame;
     final SearchOptionBox searchPathOptionBox;
     final SearchOptionBox searchFieldOptionBox;
@@ -38,29 +40,53 @@ public class SearchPanel extends JPanel implements ActionListener {
         arbilDatabase = new ArbilDatabase(new ArbilSessionStorage(), arbilWindowManager, BugCatcherManager.getBugCatcher());
         this.setLayout(new BorderLayout());
         this.add(new ArbilNodeSearchPanel(null, null, new ArbilNode[0]), BorderLayout.PAGE_END);
-        lowerPanel = new JPanel();
-        lowerPanel.setLayout(new FlowLayout());
+        final JPanel criterionPanel = new JPanel();
+        criterionPanel.setLayout(new FlowLayout());
+
+        final JPanel criterionOuterPanel = new JPanel(new BorderLayout());
+
+        searchPathOptionBox = new SearchOptionBox();
+        searchPathOptionBox.addActionListener(this);
+        searchPathOptionBox.setActionCommand("path");
+        criterionPanel.add(searchPathOptionBox);
+
+        searchFieldOptionBox = new SearchOptionBox();
+        searchFieldOptionBox.addActionListener(this);
+        searchFieldOptionBox.setActionCommand("field");
+        criterionPanel.add(searchFieldOptionBox);
+
+        criterionOuterPanel.add(criterionPanel, BorderLayout.LINE_START);
+
+        JTextField searchText = new JTextField();
+        criterionOuterPanel.add(searchText, BorderLayout.CENTER);
+
+        this.add(criterionOuterPanel, BorderLayout.PAGE_START);
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        JPanel progressPanel = new JPanel(new BorderLayout());
+
         final JButton createButton = new JButton("create db");
         createButton.setActionCommand("create");
         final JButton optionsButton = new JButton("get options");
         optionsButton.setActionCommand("options");
         optionsButton.addActionListener(this);
         createButton.addActionListener(this);
-        lowerPanel.add(createButton);
-        lowerPanel.add(optionsButton);
 
-        searchPathOptionBox = new SearchOptionBox();
-        searchPathOptionBox.addActionListener(this);
-        searchPathOptionBox.setActionCommand("path");
-        lowerPanel.add(searchPathOptionBox);
+        JPanel dbButtonsPanel = new JPanel();
+        dbButtonsPanel.add(createButton);
+        dbButtonsPanel.add(optionsButton);
 
-        searchFieldOptionBox = new SearchOptionBox();
-        searchFieldOptionBox.addActionListener(this);
-        searchFieldOptionBox.setActionCommand("field");
-        lowerPanel.add(searchFieldOptionBox);
+        progressPanel.add(dbButtonsPanel, BorderLayout.LINE_START);
 
+        progressPanel.add(new JProgressBar(), BorderLayout.CENTER);
 
-        this.add(lowerPanel, BorderLayout.CENTER);
+        JPanel searchButtonsPanel = new JPanel();
+        searchButtonsPanel.add(new JButton("Search"));
+        progressPanel.add(searchButtonsPanel, BorderLayout.LINE_END);
+
+        centerPanel.add(progressPanel, BorderLayout.PAGE_START);
+        centerPanel.add(new JTextArea(), BorderLayout.CENTER);
+
+        this.add(centerPanel, BorderLayout.CENTER);
     }
 
     static public void main(String[] args) {
