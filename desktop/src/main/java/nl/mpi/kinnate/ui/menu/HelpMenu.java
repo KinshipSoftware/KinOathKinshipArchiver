38dfc2190b1007293c1fae9a55e8a322b9050046
Peter Withers <Peter.Withers@mpi.nl>
2012-10-01 15:28:19 +0000

    Add the help menu item and the help window ready for the manual to be included as the internal help system. refs #2360

--- desktop/src/main/java/nl/mpi/kinnate/ui/menu/HelpMenu.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/menu/HelpMenu.java
@@ -1,5 +1,6 @@
 package nl.mpi.kinnate.ui.menu;
 
+import java.io.IOException;
 import java.net.URI;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -12,20 +13,21 @@ import nl.mpi.arbil.util.ApplicationVersion;
 import nl.mpi.arbil.util.ApplicationVersionManager;
 import nl.mpi.arbil.util.ArbilBugCatcher;
 import nl.mpi.arbil.util.BugCatcherManager;
+import nl.mpi.kinnate.ui.KinOathHelp;
 import nl.mpi.kinnate.ui.window.AbstractDiagramManager;
+import org.xml.sax.SAXException;
 
 /**
- * Document : HelpMenu
- * Created on : Dec 8, 2011, 4:25:49 PM
- * Author : Peter Withers
+ * Document : HelpMenu Created on : Dec 8, 2011, 4:25:49 PM
+ *
+ * @author Peter Withers
  */
 public class HelpMenu extends JMenu {
-    
-    public HelpMenu(AbstractDiagramManager diagramWindowManager, final ArbilWindowManager dialogHandler, final SessionStorage sessionStorage, final ApplicationVersionManager versionManager) {
+
+    public HelpMenu(final AbstractDiagramManager diagramWindowManager, final ArbilWindowManager dialogHandler, final SessionStorage sessionStorage, final ApplicationVersionManager versionManager) {
         this.setText("Help");
         JMenuItem aboutMenuItem = new JMenuItem("About");
         aboutMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 // todo: move this into the window manager
                 ApplicationVersion appVersion = versionManager.getApplicationVersion();
@@ -48,23 +50,23 @@ public class HelpMenu extends JMenu {
         JMenuItem helpMenuItem = new JMenuItem("Internal Help");
         helpMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, 0));
         helpMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 try {
-                    // todo: update this when the manual is written
-                    dialogHandler.addMessageDialogToQueue("Because this application is in beta, no manual is available as yet.\n"
-                            + "In the mean time please refer to the sample diagrams, via the file menu.", "Interim Help");
-                } catch (Exception ex) {
+                    diagramWindowManager.createDiagramWindow("Internal Help", new KinOathHelp());
+                } catch (IOException ex) {
+                    dialogHandler.addMessageDialogToQueue("Could not start the help system:\n" + ex.getMessage(), "Internal Help");
+                    BugCatcherManager.getBugCatcher().logError(ex);
+                } catch (SAXException ex) {
+                    dialogHandler.addMessageDialogToQueue("Could not start the help system:\n" + ex.getMessage(), "Internal Help");
                     BugCatcherManager.getBugCatcher().logError(ex);
                 }
             }
         });
-        helpMenuItem.setEnabled(false);
+        helpMenuItem.setEnabled(true);
         this.add(helpMenuItem);
-        
+
         JMenuItem arbilWebsiteMenuItem = new JMenuItem("KinOath Website");
         arbilWebsiteMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 try {
                     dialogHandler.openFileInExternalApplication(new URI("http://tla.mpi.nl/tools/tla-tools/kinoath"));
@@ -74,10 +76,9 @@ public class HelpMenu extends JMenu {
             }
         });
         this.add(arbilWebsiteMenuItem);
-        
+
         JMenuItem arbilOnlineManualMenuItem = new JMenuItem("KinOath Online Manual");
         arbilOnlineManualMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 try {
                     dialogHandler.openFileInExternalApplication(new URI("http://www.mpi.nl/corpus/html/kinoath/index.html"));
@@ -87,10 +88,9 @@ public class HelpMenu extends JMenu {
             }
         });
         this.add(arbilOnlineManualMenuItem);
-        
+
         JMenuItem arbilForumMenuItem = new JMenuItem("KinOath Forum (Website)");
         arbilForumMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 try {
                     dialogHandler.openFileInExternalApplication(new URI("http://tla.mpi.nl/forums/software/kinoath"));
@@ -102,7 +102,6 @@ public class HelpMenu extends JMenu {
         this.add(arbilForumMenuItem);
         final JMenuItem viewErrorLogMenuItem = new JMenuItem("View Error Log");
         viewErrorLogMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 try {
                     dialogHandler.openFileInExternalApplication(ArbilBugCatcher.getLogFile(sessionStorage, versionManager.getApplicationVersion()).toURI());
@@ -114,7 +113,6 @@ public class HelpMenu extends JMenu {
         this.add(viewErrorLogMenuItem);
         JMenuItem checkForUpdatesMenuItem = new JMenuItem("Check for Updates");
         checkForUpdatesMenuItem.addActionListener(new java.awt.event.ActionListener() {
-            
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 try {
                     if (!versionManager.forceUpdateCheck()) {
@@ -148,13 +146,12 @@ public class HelpMenu extends JMenu {
 
 //        this.add(updateKmdiProfileMenuItem);
         this.addMenuListener(new MenuListener() {
-            
             public void menuCanceled(MenuEvent evt) {
             }
-            
+
             public void menuDeselected(MenuEvent evt) {
             }
-            
+
             public void menuSelected(MenuEvent evt) {
                 viewErrorLogMenuItem.setEnabled(ArbilBugCatcher.getLogFile(sessionStorage, versionManager.getApplicationVersion()).exists());
             }
