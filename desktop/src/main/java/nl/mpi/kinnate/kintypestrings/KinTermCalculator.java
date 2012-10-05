2b650495a69b78d1a7f4cafc714b6e8c0b675cd1
Peter Withers <Peter.Withers@mpi.nl>
2012-10-05 15:35:52 +0000

    Added todo comments for things in progress.

--- desktop/src/main/java/nl/mpi/kinnate/kintypestrings/KinTermCalculator.java
+++ desktop/src/main/java/nl/mpi/kinnate/kintypestrings/KinTermCalculator.java
@@ -10,12 +10,12 @@ import nl.mpi.kinnate.kindata.EntityData;
 public class KinTermCalculator {
 
     public void insertKinTerms(EntityData[] graphNodeArray, KinTermGroup[] kinTermsArray) {
-
+// todo: finish migrating the kin terms code here
 //        for (EntityData grapgNode : graphNodeArray) {
 //            if (grapgNode.isEgo) {
 //                for (KinTermGroup kinTermGroup : kinTermsArray) {
-//                    for (KinTerm kinTerm : kinTermGroup.getKinTerms()) {
 //                        if (kinTermGroup.graphShow) {
+//                    for (KinTerm kinTerm : kinTermGroup.getKinTerms()) {
 //                            // todo: replace this with a loop over egoDataNodeList and then calculate the actual kin type strings for each one rather than the user entered ones used here
 //                            // todo: this should probably be done after all nodes have been created so that subsequent relations are taken into account when calculating the kin terms
 ////                        for (String kinTermLabel : kinTermGroup.getTermLabel(fullKinTypeString)) {
