5e4fe7f9b019f65c61f9500a170299969f2fef3d
Peter Withers <Peter.Withers@mpi.nl>
2012-10-15 12:12:50 +0000

    refs #2394 #2400 In CSV import added support for ID column in any location. Also added info text to explain the use of this in the CSV text output. Corrected some graph sorting issues found after importing a CSV from Halle. Added CSV import for the french gender words. Added support for PUCK txt tab delimited files which have new headers mid way through the file. Added further PUCK support where If the first record does not start at zero then all relations to the ID of zero will be ignored. Added an extra test case for the csv importer.

--- desktop/src/test/java/nl/mpi/kinnate/gedcomimport/CsvImporterTest.java
+++ desktop/src/test/java/nl/mpi/kinnate/gedcomimport/CsvImporterTest.java
@@ -13,8 +13,8 @@ import nl.mpi.kinnate.KinOathVersion;
 import nl.mpi.kinnate.userstorage.KinSessionStorage;
 
 /**
- *  Document   : CsvImporterTest
- *  Created on : Dec 30, 2011, 10:11:44
+ * Document : CsvImporterTest Created on : Dec 30, 2011, 10:11:44
+ *
  * @author petwit
  */
 public class CsvImporterTest extends TestCase {
@@ -23,26 +23,27 @@ public class CsvImporterTest extends TestCase {
      * Test of getFieldsForLine method, of class CsvImporter.
      */
     public void testFieldsForLine() {
-        runFieldsForLine("one,two,three", new String[]{"one", "two", "three"});
-        runFieldsForLine("one\ttwo\tthree", new String[]{"one", "two", "three"});
-        runFieldsForLine("one,two,three\n", new String[]{"one", "two", "three"});
-        runFieldsForLine("one,two,three\r", new String[]{"one", "two", "three"});
-        runFieldsForLine("one,\"two\",three\r", new String[]{"one", "two", "three"});
-        runFieldsForLine("one,\"two\",three\r", new String[]{"one", "two", "three"});
-        runFieldsForLine("one,\"two\nextra\",three\r", new String[]{"one", "two\nextra", "three"});
-        runFieldsForLine("one,\"two,extra\",three\r", new String[]{"one", "two,extra", "three"});
-        runFieldsForLine("one,\"two\textra\",three\r", new String[]{"one", "two\textra", "three"});
-        runFieldsForLine("one\t\"two,extra\"\tthree\r", new String[]{"one", "two,extra", "three"});
-        runFieldsForLine("one\t\"two\textra\"\tthree\r", new String[]{"one", "two\textra", "three"});
+        runFieldsForLine("one,two,three", new String[]{"one", "two", "three"}, ',');
+        runFieldsForLine("one\ttwo\tthree", new String[]{"one", "two", "three"}, '\t');
+        runFieldsForLine("one,two,three\n", new String[]{"one", "two", "three"}, ',');
+        runFieldsForLine("one,two,three\r", new String[]{"one", "two", "three"}, ',');
+        runFieldsForLine("one,\"two\",three\r", new String[]{"one", "two", "three"}, ',');
+        runFieldsForLine("one,\"two\",three\r", new String[]{"one", "two", "three"}, ',');
+        runFieldsForLine("one,\"two\nextra\",three\r", new String[]{"one", "two\nextra", "three"}, ',');
+        runFieldsForLine("one,\"two,extra\",three\r", new String[]{"one", "two,extra", "three"}, ',');
+        runFieldsForLine("one,\"two\textra\",three\r", new String[]{"one", "two\textra", "three"}, ',');
+        runFieldsForLine("one\t\"two,extra\"\tthree\r", new String[]{"one", "two,extra", "three"}, '\t');
+        runFieldsForLine("one\t\"two\textra\"\tthree\r", new String[]{"one", "two\textra", "three"}, '\t');
+        runFieldsForLine("* comments \n* more comments\r\r\n\n\rone\t\"two\textra\"\tthree\r", new String[]{"one", "two\textra", "three"}, '\t');
     }
 
-    private void runFieldsForLine(String csvInputString, String[] expectedResult) {
+    private void runFieldsForLine(String csvInputString, String[] expectedResult, char fieldSeparator) {
         try {
             BufferedReader bufferedReader = new BufferedReader(new StringReader(csvInputString));
             final ApplicationVersionManager applicationVersionManager = new ApplicationVersionManager(new KinOathVersion());
             final KinSessionStorage kinSessionStorage = new KinSessionStorage(applicationVersionManager);
             final ArbilBugCatcher bugCatcher = new ArbilBugCatcher(kinSessionStorage, new ApplicationVersionManager(new KinOathVersion()));
-            ArrayList<String> arrayList = new CsvImporter(new JProgressBar(), new JTextArea(), true, new KinSessionStorage(applicationVersionManager)).getFieldsForLine(bufferedReader);
+            ArrayList<String> arrayList = new CsvImporter(new JProgressBar(), new JTextArea(), true, new KinSessionStorage(applicationVersionManager)).getFieldsForLineExcludingComments(bufferedReader, fieldSeparator);
 
             assertTrue("Incorrect number of fields found", expectedResult.length == arrayList.size());
             for (int arrayCounter = 0; arrayCounter < expectedResult.length; arrayCounter++) {
