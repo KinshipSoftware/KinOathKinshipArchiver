33e8e7e062189e806c0c077e4942fa6832d5afd6
Peter Withers <Peter.Withers@mpi.nl>
2012-10-25 09:19:38 +0000

    Moved plugins from KinOath into a separate project called PluginsAndModules.

---
+++ plugins/core/src/main/java/nl/mpi/kinnate/plugin/PluginGraphSource.java
@@ -0,0 +1,17 @@
+package nl.mpi.kinnate.plugin;
+
+/**
+ * Created on Oct 20, 2012, 21:55:21 <br>
+ *
+ * @author Peter Withers <br>
+ */
+public interface PluginGraphSource {
+    /*    
+     * example usages of this plugin type would be:
+     * an R graph provider, 
+     * a prolog provider,
+     * a plugin graph provider that allows the setting of each plugins parameters and defining links between each plugin in the work flow. the entities returned from this would implement ArbilNode and the fields would each be a setting 
+     */
+
+//    public EntityData[] getEntities();
+}
