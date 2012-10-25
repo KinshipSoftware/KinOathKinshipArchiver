728903392f28b4de93c775cfce0ce49309a16e86
Peter Withers <Peter.Withers@mpi.nl>
2012-10-25 13:27:01 +0000

    Added a configuration file for Arbil that lists the URLs of the available plugins. Added a listing of plugin paths that are passed to the plugin loader.

--- desktop/src/main/java/nl/mpi/kinnate/ui/menu/MainMenuBar.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/menu/MainMenuBar.java
@@ -1,6 +1,10 @@
 package nl.mpi.kinnate.ui.menu;
 
 import java.awt.Component;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import javax.swing.JMenuBar;
 import nl.mpi.arbil.ui.ArbilWindowManager;
 import nl.mpi.arbil.userstorage.SessionStorage;
@@ -24,7 +28,31 @@ public class MainMenuBar extends JMenuBar {
         this.add(new KinTermsMenu(abstractDiagramManager, parentComponent));
         this.add(new ArchiveMenu(abstractDiagramManager, parentComponent));
         this.add(new DiagramPanelsMenu(abstractDiagramManager, parentComponent));
-        this.add(new PluginMenu(PluginService.getInstance(), new KinOathPluginManager(abstractDiagramManager, dialogHandler, parentComponent), false));
+        // get the list of available plugins
+        ArrayList<URL> pluginUlrs = new ArrayList<URL>();
+        String errorMessages = "";
+        try {
+            final String[] pluginStringArray = sessionStorage.loadStringArray("PluginList");
+            if (pluginStringArray != null) {
+                for (String pluginString : pluginStringArray) {
+                    try {
+                        pluginUlrs.add(new URL(pluginString));
+                    } catch (MalformedURLException exception) {
+                        System.out.println(exception.getMessage());
+                        errorMessages = errorMessages + "Could not load plugin: " + pluginString + "\n";
+                    }
+                }
+//            } else {
+//                sessionStorage.saveStringArray("PluginList", new String[]{"file:///<path to plugin>.jar", "file:///<path to plugin>.jar"});
+            }
+            if (!"".equals(errorMessages)) {
+                dialogHandler.addMessageDialogToQueue(errorMessages, "Plugin Error");
+            }
+        } catch (IOException ex) {
+            // if the list is not found then we need not worry at this point.
+            System.out.println("PluginList not found");
+        }
+        this.add(new PluginMenu(new PluginService(pluginUlrs.toArray(new URL[0])), new KinOathPluginManager(abstractDiagramManager, dialogHandler, parentComponent), false));
         this.add(new WindowMenu(abstractDiagramManager, parentComponent));
         this.add(new HelpMenu(abstractDiagramManager, dialogHandler, sessionStorage, versionManager));
     }
