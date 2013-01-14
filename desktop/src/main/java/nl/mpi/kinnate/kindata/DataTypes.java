521fd64e4831b2b3c255bf376a8844b6c8f65d44
Peter Withers <Peter.Withers@mpi.nl>
2013-01-14 11:40:24 +0000

    refs #2670 Added line end markers so that directed relations can be represented.

--- desktop/src/main/java/nl/mpi/kinnate/kindata/DataTypes.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindata/DataTypes.java
@@ -1,26 +1,26 @@
 /**
  * Copyright (C) 2012 The Language Archive
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.kinnate.kindata;
 
 /**
- *  Document   : RelationType
- *  Created on : Mar 25, 2011, 6:16:56 PM
- *  Author     : Peter Withers
+ * Created on : Mar 25, 2011, 6:16:56 PM
+ *
+ * @author Peter Withers
  */
 public class DataTypes {
 
@@ -29,7 +29,7 @@ public class DataTypes {
         // todo: replace affiliation, resource, collector, metadata, none  for OTHER..
         // maybe keep the: kinterm, curve1, curve2, none
         // sibling, ancestor, descendant, union, kin_term, custom_type1, custom_type2, none
-        sibling, ancestor, descendant, union, kinterm, other
+        sibling, ancestor, descendant, union, kinterm, other, undirected, from, towards
         //, horizontalCurve, gedcom,/* probably remove the items after this */ custom, affiliation, resource, collector, metadata, none // todo: should metadata relations use the same link type as jpg files?
 // modify the kin type definitions to allow multi select for symbols and relation types eg P could be squ or cir and tri
         // and add additional option after divider with <all> that toggles all selections
@@ -54,6 +54,10 @@ public class DataTypes {
                 return DataTypes.RelationType.descendant;
             case descendant:
                 return DataTypes.RelationType.ancestor;
+            case from:
+                return DataTypes.RelationType.towards;
+            case towards:
+                return DataTypes.RelationType.from;
         }
         return relationType;
     }
