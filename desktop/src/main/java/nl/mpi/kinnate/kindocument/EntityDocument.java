37834ec1da83bca8f6af20bda4fd615b638d737d
Peter Withers <Peter.Withers@mpi.nl>
2013-01-17 14:35:28 +0000

    refs #2059 Excluded empty FAM entities when importing; only FAM entities that contain data will be created when importing GEDCOM files. refs #2603 Resolved unsupported relation type on import.

--- desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityDocument.java
+++ desktop/src/main/java/nl/mpi/kinnate/kindocument/EntityDocument.java
@@ -1,19 +1,19 @@
 /**
  * Copyright (C) 2012 The Language Archive
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.kinnate.kindocument;
 
@@ -375,6 +375,13 @@ public class EntityDocument {
 
     public String getFilePath() {
         return entityFile.getAbsolutePath();
+    }
+
+    public int getAddedFieldCount() {
+        // count the feilds in the metadata node.
+        // note that this may not be the same as the number of field insertions because if the file existed before then there may be pre-existing fields 
+        // also note that for the sake of simplicity this only counts the first level not any child nodes
+        return metadataNode.getChildNodes().getLength();
     }
 
     public void saveDocument() throws ImportException {
