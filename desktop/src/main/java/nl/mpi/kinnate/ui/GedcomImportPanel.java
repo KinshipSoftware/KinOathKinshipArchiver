3174ce3812ab6cbbcc6e5fd1a4e2421906a7a33d
Peter Withers <Peter.Withers@mpi.nl>
2013-01-17 14:06:50 +0000

    refs #2322 When an import is done from a freeform diagram the imported result are be shown in a new diagram.

--- desktop/src/main/java/nl/mpi/kinnate/ui/GedcomImportPanel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/GedcomImportPanel.java
@@ -1,19 +1,19 @@
 /**
  * Copyright (C) 2012 The Language Archive
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.kinnate.ui;
 
@@ -99,11 +99,15 @@ public class GedcomImportPanel extends JPanel {
             closeButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     final SavePanel currentSavePanel = abstractDiagramManager.getCurrentSavePanel(parentComponent);
-                    KinDiagramPanel kinDiagramPanel;
+                    KinDiagramPanel kinDiagramPanel = null;
                     if (currentSavePanel instanceof KinDiagramPanel) {
                         kinDiagramPanel = (KinDiagramPanel) currentSavePanel;
+                        if (kinDiagramPanel.getGraphPanel().dataStoreSvg.diagramMode != DataStoreSvg.DiagramMode.KinTypeQuery) {
+                            kinDiagramPanel = null;
+                        }
 //                        kinDiagramPanel.updateGraph(); // there is no point updating the graph at this point
-                    } else {
+                    }
+                    if (kinDiagramPanel == null) {
                         kinDiagramPanel = new KinDiagramPanel(DocumentNewMenu.DocumentType.Simple, sessionStorage, dialogHandler, dataNodeLoader, treeHelper, entityCollection, abstractDiagramManager);
                         kinDiagramPanel.setName("Imported Entities");
                         abstractDiagramManager.createDiagramContainer(kinDiagramPanel, null);
