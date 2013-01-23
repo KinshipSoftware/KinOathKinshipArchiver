f5e580e56e2a64eebf2cf6df94b48818ef8c4aaa
Peter Withers <Peter.Withers@mpi.nl>
2013-01-23 12:47:45 +0000

    refs #2712 Corrected an issue where the "Export for R/SpSS" via the file menu failed when dates of birth/death were not found.

--- desktop/src/main/java/nl/mpi/kinnate/export/PedigreePackageExport.java
+++ desktop/src/main/java/nl/mpi/kinnate/export/PedigreePackageExport.java
@@ -1,19 +1,19 @@
 /**
  * Copyright (C) 2012 The Language Archive
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.kinnate.export;
 
@@ -29,9 +29,9 @@ import nl.mpi.kinnate.ui.KinTypeStringProvider;
 import nl.mpi.kinnate.uniqueidentifiers.UniqueIdentifier;
 
 /**
- * Document : PedigreePackageExport
  * Created on : Jun 27, 2011, 12:06:30 PM
- * Author : Peter Withers
+ *
+ * @author Peter Withers
  */
 public class PedigreePackageExport {
 
@@ -131,9 +131,13 @@ public class PedigreePackageExport {
                 stringBuilder.append("0");
             }
             stringBuilder.append("\t");
-            stringBuilder.append(entityData.getDateOfBirth().getDateString());
+            if (entityData.getDateOfBirth() != null) {
+                stringBuilder.append(entityData.getDateOfBirth().getDateString());
+            }
             stringBuilder.append("\t");
-            stringBuilder.append(entityData.getDateOfDeath().getDateString());
+            if (entityData.getDateOfDeath() != null) {
+                stringBuilder.append(entityData.getDateOfDeath().getDateString());
+            }
             stringBuilder.append("\t");
             String[] currentLabels = entityData.getLabel();
 //            String[] labelArray = new String[labelCounter];
@@ -164,7 +168,6 @@ public class PedigreePackageExport {
         // // todo: an addition to KinTypeStringConverter: EmB does not have any parents but it could however just take the parnents of Em: String kinTypes = "EmB|EmZ|EmM|EmF|EmS|EmD";
         //String kinTypes = "EmM|EmF|EmS|EmD";        
         KinTypeStringProvider kinTypeStringProvider = new KinTypeStringProvider() {
-
             public String[] getCurrentStrings() {
                 return "EmB,EmZ,EmM,EmF,EmS,EmD".split(",");
             }
