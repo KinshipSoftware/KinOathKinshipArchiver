109016dc6e8de5a5c4c4afc3dce48cbdbace52cf
Peter Withers <Peter.Withers@mpi.nl>
2013-01-24 16:37:42 +0000

    Replaced the path stored in the entity by get path query for an identifier, which allows reindexing and would make the query a little faster.

--- desktop/src/main/java/nl/mpi/kinnate/ui/KinDiagramPanel.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/KinDiagramPanel.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2012 The Language Archive
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 package nl.mpi.kinnate.ui;
 
 import java.awt.BorderLayout;
@@ -220,7 +237,7 @@ public class KinDiagramPanel extends JPanel implements SavePanel, KinTermSavePan
         kinTermHidePane = new HidePane(HidePane.HidePanePosition.right, 0);
 
         TableCellDragHandler tableCellDragHandler = new TableCellDragHandler();
-        graphPanel.setArbilTableModel(new MetadataPanel(graphPanel, tableHidePane, tableCellDragHandler, dataNodeLoader, null)); // todo: pass a ImageBoxRenderer here if you want thumbnails
+        graphPanel.setArbilTableModel(new MetadataPanel(graphPanel, entityCollection, tableHidePane, tableCellDragHandler, dataNodeLoader, null)); // todo: pass a ImageBoxRenderer here if you want thumbnails
 
         // in some older files and non kinoath files these VisiblePanelSettings would not be set, so we make sure that they are here
         final ProfileManager profileManager = new ProfileManager(sessionStorage, dialogHandler);
