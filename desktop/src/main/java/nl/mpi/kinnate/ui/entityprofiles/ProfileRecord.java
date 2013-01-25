c96764adbe8c76265ed3e7fdb2f75ac9eaef51fb
Peter Withers <Peter.Withers@mpi.nl>
2013-01-25 15:23:47 +0000

    Added additional profiles into the default diagram.

--- desktop/src/main/java/nl/mpi/kinnate/ui/entityprofiles/ProfileRecord.java
+++ desktop/src/main/java/nl/mpi/kinnate/ui/entityprofiles/ProfileRecord.java
@@ -1,28 +1,28 @@
 /**
  * Copyright (C) 2012 The Language Archive
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.kinnate.ui.entityprofiles;
 
 import javax.xml.bind.annotation.XmlAttribute;
 
 /**
- * Document : ProfileRecord
  * Created on : Mar 21, 2012, 2:45:30 PM
- * Author : Peter Withers
+ *
+ * @author Peter Withers
  */
 public class ProfileRecord {
 
@@ -49,6 +49,7 @@ public class ProfileRecord {
     }
 
     static public ProfileRecord[] getDefaultProfiles() {
-        return new ProfileRecord[]{new ProfileRecord("Individual", "clarin.eu:cr1:p_1320657629627"), new ProfileRecord("Chromosome_Example", "clarin.eu:cr1:p_1332345811038"), new ProfileRecord("Clan_Example", "clarin.eu:cr1:p_1337778924934")};
+        return new ProfileRecord[]{new ProfileRecord("Individual", "clarin.eu:cr1:p_1320657629627"), new ProfileRecord("Chromosome_Example", "clarin.eu:cr1:p_1332345811038"), new ProfileRecord("Clan_Example", "clarin.eu:cr1:p_1337778924934"), new ProfileRecord("KinTerm", "clarin.eu:cr1:p_1337778924930"), new ProfileRecord("Social_Event", "clarin.eu:cr1:p_1357720977500")};
+//         <kin:Profile kin:ProfileName="Chromosome_Example" kin:ProfileId="clarin.eu:cr1:p_1337778924932"/>
     }
 }
