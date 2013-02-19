292e94fc2f197379db9c34642ce344df9efde6ec
Peter Withers <Peter.Withers@mpi.nl>
2013-02-19 13:43:09 +0000

    Suppressed the the get type methods to the core data node.

--- desktop/src/main/java/nl/mpi/kinnate/data/KinTreeNode.java
+++ desktop/src/main/java/nl/mpi/kinnate/data/KinTreeNode.java
@@ -29,8 +29,6 @@ import nl.mpi.arbil.data.ArbilDataNodeLoader;
 import nl.mpi.arbil.data.ArbilNode;
 import nl.mpi.arbil.data.ContainerNode;
 import nl.mpi.arbil.util.MessageDialogHandler;
-import nl.mpi.flap.model.DataNodeType;
-import nl.mpi.flap.model.PluginDataNodeType;
 import nl.mpi.flap.model.FieldGroup;
 import nl.mpi.kinnate.entityindexer.EntityCollection;
 import nl.mpi.kinnate.entityindexer.IndexerParameters;
@@ -300,16 +298,6 @@ public class KinTreeNode extends ArbilNode implements Comparable {
 
     @Override
     public List<String> getChildIds() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setType(DataNodeType dataNodeType) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public DataNodeType getType() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
