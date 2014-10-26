3d92f653393b1cfd43b8ab903e0206680b6d6ca5
Peter Withers <peter-gthb@bambooradical.com>
2014-10-26 12:00:49 +0100

    Created a maven project in which to move graph sorting code into. #408

---
+++ graph-sorter/src/test/java/nl/mpi/kinoath/graphsorter/VersionTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2014 Peter Withers
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+package nl.mpi.kinoath.graphsorter;
+
+import junit.framework.TestCase;
+import nl.mpi.flap.module.AbstractBaseModule;
+import nl.mpi.flap.plugin.PluginException;
+
+/**
+ * Created on : Oct 12, 2014, 10:02
+ *
+ * @author Peter Withers
+ */
+public class VersionTest extends TestCase {
+
+    /**
+     * Test of getArtifactVersion method, of class AbstractBasePlugin.
+     */
+    public void testGetArtifactVersion() {
+        System.out.println("getArtifactVersion");
+        try {
+            AbstractBaseModule abstractBasePlugin = new AbstractBaseModule("KinOathgGaphSorter", "Kinship Graph Sorter.", "nl.mpi.kinoath.graphsorter") {
+            };
+            assertTrue(abstractBasePlugin.isMavenVersionCorrect());
+        } catch (PluginException exception) {
+            fail(exception.getMessage());
+        }
+    }
+}
