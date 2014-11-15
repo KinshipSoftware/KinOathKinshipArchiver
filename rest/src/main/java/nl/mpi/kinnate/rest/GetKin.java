5bc2982e710ab10f8170a7ec7debc4c5da1d0765
Peter Withers <peter-gthb@bambooradical.com>
2014-11-15 19:08:27 +0100

    Resurrected the KinOath REST web service so that it can be used to test the non swing diagram generation process.

--- rest/src/main/java/nl/mpi/kinnate/rest/GetKin.java
+++ rest/src/main/java/nl/mpi/kinnate/rest/GetKin.java
@@ -1,19 +1,20 @@
 /**
- * Copyright (C) 2013 The Language Archive, Max Planck Institute for Psycholinguistics
+ * Copyright (C) 2013 The Language Archive, Max Planck Institute for
+ * Psycholinguistics
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.kinnate.rest;
 
@@ -22,37 +23,55 @@ package nl.mpi.kinnate.rest;
  *  Created on : Jun 21, 2011, 11:55:37 AM
  *  Author     : Peter Withers
  */
-//import javax.ejb.EJB;
-//import javax.ejb.Stateless;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import nl.mpi.kinnate.export.PedigreePackageExport;
 import nl.mpi.kinnate.kindata.EntityData;
-import nl.mpi.kinnate.kintypestrings.KinTermGroup;
+import nl.mpi.kinnate.kintypestrings.KinType;
 import nl.mpi.kinnate.kintypestrings.KinTypeStringConverter;
 import nl.mpi.kinnate.kintypestrings.ParserHighlight;
-import nl.mpi.kinnate.svg.DataStoreSvg;
+import nl.mpi.kinnate.ui.KinTypeStringProvider;
 
 //@Stateless
 @Path("/getkin")
 public class GetKin {
 
+    private static final String RHOMBUS = "rhombus";
+
 //    @EJB
 //    private EntityStorageBean entityStorage;
 //    @Path("/kintypes/{kintypeStrings: [a-zA-Z0-9]}")
-    private EntityData[] getEntityNodes(List<String> kintypeStrings) {
-        KinTypeStringConverter graphData = new KinTypeStringConverter(new DataStoreSvg());
+    private EntityData[] getEntityNodes(final List<String> kintypeStrings) {
+        KinTypeStringConverter graphData = new KinTypeStringConverter(RHOMBUS, KinType.getReferenceKinTypes());
         HashSet<String> kinTypeAllStrings = new HashSet<String>();
         // loop each and split any pipe chars | into lines
         for (String currentKinType : kintypeStrings) {
             kinTypeAllStrings.addAll(Arrays.asList(currentKinType.split("\\|")));
         }
-        graphData.readKinTypes(kinTypeAllStrings.toArray(new String[]{}), /*graphPanel.getkinTermGroups()*/ new KinTermGroup[]{}, new DataStoreSvg(), new ParserHighlight[kintypeStrings.size()]);
+        final ArrayList<KinTypeStringProvider> arrayList = new ArrayList<KinTypeStringProvider>();
+        arrayList.add(new KinTypeStringProvider() {
+
+            @Override
+            public String[] getCurrentStrings() {
+                return kintypeStrings.toArray(new String[kintypeStrings.size()]);
+            }
+
+            @Override
+            public int getTotalLength() {
+                return kintypeStrings.size();
+            }
+
+            @Override
+            public void highlightKinTypeStrings(ParserHighlight[] parserHighlight, String[] kinTypeStrings) {
+            }
+        });
+        graphData.readKinTypes(arrayList);
         return graphData.getDataNodes();
     }
 
@@ -65,9 +84,11 @@ public class GetKin {
         stringBuilder.append("<table>");
         for (EntityData entityData : entiryData) {
             stringBuilder.append("<tr><td>");
-            stringBuilder.append(entityData.getUniqueIdentifier());
+            stringBuilder.append(entityData.getUniqueIdentifier().getAttributeIdentifier());
             stringBuilder.append("</td><td>");
-            stringBuilder.append(entityData.getSymbolType());
+            for (String symbol : entityData.getSymbolNames(RHOMBUS)) {
+                stringBuilder.append(symbol);
+            }
             stringBuilder.append("</td><td>");
             for (String labelString : entityData.getLabel()) {
                 stringBuilder.append(labelString);
@@ -82,7 +103,7 @@ public class GetKin {
         }
         stringBuilder.append("</table>");
         stringBuilder.append("<pre>");
-        PedigreePackageExport packageExport = new PedigreePackageExport();
+        PedigreePackageExport packageExport = new PedigreePackageExport(RHOMBUS);
         stringBuilder.append(packageExport.createCsvContents(entiryData));
         stringBuilder.append("</pre>");
         return "<html><body>" + kintypeStrings + "<p>" + stringBuilder + "</p></body></html>";
@@ -92,7 +113,7 @@ public class GetKin {
     @Produces("text/csv")
     @Path("/csv")
     public String getXml(@QueryParam("kts") List<String> kintypeStrings) {
-        PedigreePackageExport packageExport = new PedigreePackageExport();
+        PedigreePackageExport packageExport = new PedigreePackageExport(RHOMBUS);
         return packageExport.createCsvContents(getEntityNodes(kintypeStrings));
     }
     // todo: Ticket #1088 Enable kin type string queries to be done via the rest interface (currently only transient kin type strings are enabled).
